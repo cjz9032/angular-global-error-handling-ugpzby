3eca5d845a8f1d1d34d528820eb9f3c60a041332
unknown <agordon2@lenovo.com>
2019-01-20 13:08:05 -0500

    cleanup of initial code commit & added bootstrap

--- vtr-ui/src/app/app.component.ts
+++ vtr-ui/src/app/app.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 import { Router, ActivatedRoute } from '@angular/router';
-import { ElectronService } from 'ngx-electron';
 import { UserService } from './services/user/user.service';
 import { ContainerService } from './services/container/container.service';
 import { DevService } from './services/dev/dev.service';
@@ -19,7 +18,6 @@ export class AppComponent implements OnInit {
 
 	constructor(
 		private route: ActivatedRoute,
-		private electronService: ElectronService,
 		private userService: UserService,
 		private containerService: ContainerService,
 		private devService: DevService,
@@ -37,31 +35,7 @@ export class AppComponent implements OnInit {
 
 		const urlParams = new URLSearchParams(window.location.search);
 		this.devService.writeLog('GOT PARAMS', urlParams.toString());
-		const token = urlParams.get('lenovoid.wust');
-		this.devService.writeLog('GOT TOKEN', token);
-		if (token !== undefined && token !== null && token !== '') {
-			this.userService.setToken(token);
-		} else {
-			this.userService.checkCookies();
-		}
-
-		if (this.electronService.ipcRenderer) {
-			this.electronService.ipcRenderer.on('GetGuidFromMain', function (event, data) {
-				self.devService.writeLog('GUID FROM ELECTRON', data);
-				self.containerService.setAppId(data);
-				self.userService.loadSettings();
-			});
-
-			this.containerService.loadGUID();
-		} else {
-			// TEST
-			self.containerService.setAppId('5f74e879-7241-ff6d-27db-8ddeed9424de');
-			self.userService.loadSettings();
-		}
 
 	}
-
-
-
 
 }
