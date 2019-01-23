7fa5f843f10c496be597f3a9cc2c6865f2fb368d
unknown <agordon2@lenovo.com>
2019-01-22 22:57:16 -0500

    VAN-24 added initial template for common header main menu

--- vtr-ui/src/app/components/menu-main/menu-main.component.ts
+++ vtr-ui/src/app/components/menu-main/menu-main.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { ConfigService } from '../../services/config/config.service';
+import { DeviceService } from '../../services/device/device.service';
 
 @Component({
 	selector: 'vtr-menu-main',
@@ -13,32 +15,87 @@ export class MenuMainComponent implements OnInit {
 			id: 'dashboard',
 			label: 'Dashboard',
 			path: 'dashboard',
-			icon: 'dashboard'
+			icon: 'dashboard',
+			subitems: []
 		}, {
 			id: 'device',
 			label: 'Device',
 			path: 'device',
-			icon: 'device'
+			icon: 'device',
+			subitems: [{
+				id: 'device',
+				label: 'My Device',
+				path: 'device',
+				icon: '',
+				subitems: []
+			}, {
+				id: 'device-settings',
+				label: 'My Device Settings',
+				path: 'device-settings',
+				icon: '',
+				subitems: []
+			}, {
+				id: 'hardware-scan',
+				label: 'Hardware Scan',
+				path: 'hardware-scan',
+				icon: '',
+				subitems: []
+			}]
 		}, {
 			id: 'security',
 			label: 'Security',
 			path: 'security',
-			icon: 'security'
+			icon: 'security',
+			subitems: [{
+				id: 'security',
+				label: 'My Security',
+				path: 'security',
+				icon: '',
+				subitems: []
+			}, {
+				id: 'anti-virus',
+				label: 'Anti-Virus',
+				path: 'anti-virus',
+				icon: '',
+				subitems: []
+			}, {
+				id: 'wifi-security',
+				label: 'Wifi Security',
+				path: 'wifi-security',
+				icon: '',
+				subitems: []
+			}, {
+				id: 'password-protection',
+				label: 'Password Protection',
+				path: 'password-protection',
+				icon: '',
+				subitems: []
+			}, {
+				id: 'internet-protection',
+				label: 'Internet Protection',
+				path: 'internet-protection',
+				icon: '',
+				subitems: []
+			}]
 		}, {
 			id: 'support',
 			label: 'Support',
 			path: 'support',
-			icon: 'support'
+			icon: 'support',
+			subitems: []
 		}, {
 			id: 'user',
 			label: 'User',
 			path: 'user',
-			icon: 'user'
+			icon: 'user',
+			subitems: []
 		}
 	];
 
 	constructor(
-		private router: Router
+		private router: Router,
+		public configService: ConfigService,
+		public deviceService: DeviceService
 	) { }
 
 	ngOnInit() {
