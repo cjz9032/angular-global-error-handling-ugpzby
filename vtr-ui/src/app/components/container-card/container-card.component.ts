bece4f5f30a617d58d7687bac22850043adfc830
Adam Gordon <agordon2@lenovo.com>
2019-01-28 15:04:15 -0500

    fixes to adhere to coding standards

--- vtr-ui/src/app/components/container-card/container-card.component.ts
+++ vtr-ui/src/app/components/container-card/container-card.component.ts
@@ -1,48 +1,48 @@
-import { Component, OnInit,Input } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
-  selector: 'vtr-container-card',
-  templateUrl: './container-card.component.html',
-  styleUrls: ['./container-card.component.scss']
+	selector: 'vtr-container-card',
+	templateUrl: './container-card.component.html',
+	styleUrls: ['./container-card.component.scss']
 })
 export class ContainerCardComponent implements OnInit {
 
-  @Input() img: string = this.img || '';
-  @Input() caption: string = this.caption || '';
-  @Input() title: string = this.title || '';
-  @Input() logo: string = this.logo || '';
-  @Input() logoText: string = this.logoText || '';
-  @Input() readMore: string = this.readMore || '';
-  @Input() type: string = this.type || '';
+	@Input() img: string = this.img || '';
+	@Input() caption: string = this.caption || '';
+	@Input() title: string = this.title || '';
+	@Input() logo: string = this.logo || '';
+	@Input() logoText: string = this.logoText || '';
+	@Input() readMore: string = this.readMore || '';
+	@Input() type: string = this.type || '';
 
-  constructor() { }
+	constructor() { }
 
-  getType() {
-    let cardType = 1;
-    switch (this.type) {
-      case '4X4': {
-        cardType = 1;
-         break;
-      }
-      case '8X4': {
-         cardType = 2;
-         break;
-	  }
-	  case '4X4': {
-		cardType = 3;
-		break;
-	 }
-      default: {
-        cardType = 1;
-         break;
-      }
-   }
+	getType() {
+		let cardType = 1;
+		switch (this.type) {
+			case '4X4': {
+				cardType = 1;
+				break;
+			}
+			case '8X4': {
+				cardType = 2;
+				break;
+			}
+			case '4X4': {
+				cardType = 3;
+				break;
+			}
+			default: {
+				cardType = 1;
+				break;
+			}
+		}
 
-    return cardType;
+		return cardType;
 
-  }
+	}
 
-  ngOnInit() {
-  }
+	ngOnInit() {
+	}
 
 }
