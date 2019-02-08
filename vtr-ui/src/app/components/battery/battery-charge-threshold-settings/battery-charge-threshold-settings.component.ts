b6496d37aaa35b4b66cceace880472a89afca9ee
Santosh udnur <sudnur@lenovo.com>
2019-02-08 20:00:56 +0530

    santosh-van-176: added input param to component to hide or show only note

--- vtr-ui/src/app/components/battery/battery-charge-threshold-settings/battery-charge-threshold-settings.component.ts
+++ vtr-ui/src/app/components/battery/battery-charge-threshold-settings/battery-charge-threshold-settings.component.ts
@@ -9,12 +9,13 @@ import { NgForm } from '@angular/forms';
 export class BatteryChargeThresholdSettingsComponent implements OnInit {
 	@Input() title: string = this.title || '';
 	@Input() type: string = this.type || 'primary';
+	@Input() displayNoteOnly: boolean = this.displayNoteOnly || false;
 	@ViewChild('batterySettingsForm') batterySettingsForm: NgForm;
 	isCheckedAuto: boolean = this.isCheckedAuto || false;
+
 	chargeOptions = [40, 45, 50, 55, 60, 65, 70, 75, 80, 85, 90, 95, 100];
 	selMaxCharge = this.selMaxCharge || 75;
 	selMinCharge = this.selMinCharge || 40;
-
 	minChargeOptions = this.chargeOptions.slice(0, this.chargeOptions.length - 1); //
 	maxChargeOptions = this.chargeOptions.slice(1, this.chargeOptions.length);
 
