ce534277af669608be99651912fe425299a241b1
Adam Gordon <agordon2@lenovo.com>
2019-02-12 23:22:02 -0500

    removed demo code

--- vtr-ui/src/app/components/pages/page-device-settings/children/subpage-device-settings-power/subpage-device-settings-power.component.ts
+++ vtr-ui/src/app/components/pages/page-device-settings/children/subpage-device-settings-power/subpage-device-settings-power.component.ts
@@ -8,22 +8,7 @@ import { Component, OnInit } from '@angular/core';
 export class SubpageDeviceSettingsPowerComponent implements OnInit {
 
 	title = 'Power Settings';
-	dummyCollapsibleContainerItemArray = [
-		{
-			'rightIcon': ['far', 'question-circle'],
-			'leftIcon': ['fas', 'power-off'],
-			'title': 'Always on USB',
-			'caption': 'Charge USB devices through the Always on USB connector on the computer orem ipsum dolor sit amet del Lorem ipsum dolor sit amet delorem ipsum dolor sit amet del Lorem ipsum dolor sit amet del',
-			'isSwitchVisible': false
-		},
-		{
-			'rightIcon': ['far', 'question-circle'],
-			'leftIcon': ['fas', 'plug'],
-			'title': 'Easy Resume',
-			'caption': 'orem ipsum dolor sit amet del Lorem ipsum dolor sit amet del',
-			'isSwitchVisible': false
-		}		
-	];
+
 	constructor() { }
 
 	ngOnInit() {
