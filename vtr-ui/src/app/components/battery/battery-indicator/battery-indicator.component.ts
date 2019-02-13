a4b8c585a7c428cc8cd05eade2d6b6d0a35f1573
raj <raj.kishor09@gmail.com>
2019-02-13 17:31:54 +0530

    VAN-237 made changes based on code review feedback. added color code for battery indicator component

--- vtr-ui/src/app/components/battery/battery-indicator/battery-indicator.component.ts
+++ vtr-ui/src/app/components/battery/battery-indicator/battery-indicator.component.ts
@@ -6,53 +6,115 @@ import { Component, OnInit, ViewChild, ElementRef, Input } from '@angular/core';
 	styleUrls: ['./battery-indicator.component.scss']
 })
 export class BatteryIndicatorComponent implements OnInit {
-	@ViewChild('battery') battery: ElementRef;
-	@ViewChild('batteryShadow') batteryShadow: ElementRef;
-	@ViewChild('acidLevel') acidLevel: ElementRef;
+	private cssStyleDeclaration: CSSStyleDeclaration;
 
-	@Input() percentage = 0;
-	@Input() remainingHour = 0;
-	@Input() remainingMinutes = 0;
+	@ViewChild('battery') battery: ElementRef;
+	@ViewChild('batteryIndicator') batteryIndicator: ElementRef;
+
+	@Input() percentage = 0; // number without % symbol
+	@Input() remainingHour = 0; // number of hours remaining
+	@Input() remainingMinutes = 0; // number of minutes remaining
 
 	constructor() {}
 
 	ngOnInit() {
-		this.refreshLevel(0.5);
-
-		this.battery.nativeElement.style.setProperty(
-			'--background-color',
-			'#f17f14'
-		);
-		this.batteryShadow.nativeElement.style.setProperty(
-			'--background-color',
-			'#f17f14'
-		);
-		this.battery.nativeElement.style.setProperty(
-			'--border-color',
-			'rgba(241, 127, 20, 0.4)'
-		);
-		this.batteryShadow.nativeElement.style.setProperty(
-			'--border-color',
-			'rgba(241, 127, 20, 0.4)'
-		);
-		this.acidLevel.nativeElement.style.setProperty(
-			'--acid-fill-gradient',
-			'linear-gradient(302.26deg, #f17f14 0%, #ed4e04 100%)'
-		);
+		this.getCssDeclaration();
+		this.refreshLevel();
 	}
 
 	/**
 	 * based on decimal value its fills battery acid level
 	 * @param level decimal value ranging from 0.0 to 1.0
 	 */
-	refreshLevel(level: number) {
-		this.acidLevel.nativeElement.style.setProperty(
-			'--acid-height',
-			level * this.battery.nativeElement.clientWidth + 'px'
-		);
+	refreshLevel() {
+		if (this.percentage > 0 && this.percentage <= 100) {
+			const level = this.percentage / 100;
+			const {
+				borderColor,
+				backgroundColor,
+				fillColor
+			} = this.getLevelCssValues(level);
+
+			console.log(level, backgroundColor, borderColor, fillColor);
+
+			this.batteryIndicator.nativeElement.style.setProperty(
+				'--background-color',
+				backgroundColor
+			);
+
+			this.batteryIndicator.nativeElement.style.setProperty(
+				'--border-color',
+				borderColor
+			);
+			this.batteryIndicator.nativeElement.style.setProperty(
+				'--acid-fill-gradient',
+				fillColor
+			);
+
+			this.batteryIndicator.nativeElement.style.setProperty(
+				'--acid-width',
+				level * this.battery.nativeElement.clientWidth + 'px'
+			);
+		}
 	}
 
-	getTimeRemaining() {
+	getLevelCssValues(level: number): any {
+		let borderColor = '';
+		let backgroundColor = '';
+		let fillColor = '';
+
+		switch (true) {
+			case level < 0.2:
+				borderColor = this.getCssPropertyValue('--border-color-0-20');
+				backgroundColor = this.getCssPropertyValue(
+					'--background-color-0-20'
+				);
+				fillColor = this.getCssPropertyValue(
+					'--acid-fill-gradient-0-20'
+				);
+				break;
+			case level < 0.4:
+				borderColor = this.getCssPropertyValue('--border-color-21-40');
+				backgroundColor = this.getCssPropertyValue(
+					'--background-color-21-40'
+				);
+				fillColor = this.getCssPropertyValue(
+					'--acid-fill-gradient-21-40'
+				);
+				break;
+			case level < 0.6:
+				borderColor = this.getCssPropertyValue('--border-color-41-60');
+				backgroundColor = this.getCssPropertyValue(
+					'--background-color-41-60'
+				);
+				fillColor = this.getCssPropertyValue(
+					'--acid-fill-gradient-41-60'
+				);
+				break;
+			case level < 0.8:
+				borderColor = this.getCssPropertyValue('--border-color-61-80');
+				backgroundColor = this.getCssPropertyValue(
+					'--background-color-61-80'
+				);
+				fillColor = this.getCssPropertyValue(
+					'--acid-fill-gradient-61-80'
+				);
+				break;
+			case level < 1:
+				borderColor = this.getCssPropertyValue('--border-color-81-100');
+				backgroundColor = this.getCssPropertyValue(
+					'--background-color-81-100'
+				);
+				fillColor = this.getCssPropertyValue(
+					'--acid-fill-gradient-81-100'
+				);
+				break;
+		}
+
+		return { borderColor, backgroundColor, fillColor };
+	}
+
+	getTimeRemaining(): string {
 		const hours =
 			this.remainingHour > 0 && this.remainingHour < 2 ? 'hour' : 'hours';
 		const minutes =
@@ -62,5 +124,16 @@ export class BatteryIndicatorComponent implements OnInit {
 		return `${this.remainingHour} ${hours} ${
 			this.remainingMinutes
 		} ${minutes}`;
+	}
+
+	// returns windows object
+	getCssPropertyValue(propertyName: string): string {
+		return this.cssStyleDeclaration.getPropertyValue(propertyName);
+	}
+
+	getCssDeclaration() {
+		this.cssStyleDeclaration = window.getComputedStyle(
+			this.batteryIndicator.nativeElement
+		);
 	}
 }
