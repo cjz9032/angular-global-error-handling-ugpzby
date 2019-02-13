246324fffb560e34e6f8f6150886b36caa163be5
Santosh udnur <sudnur@lenovo.com>
2019-02-13 11:21:43 +0530

    santosh-VAN-125 : merging develop code to current branch

--- vtr-ui/src/app/app.module.ts
+++ vtr-ui/src/app/app.module.ts
@@ -20,14 +20,11 @@ import { ClockComponent } from './components/clock/clock.component';
 
 // APPLICATION REUSABLE COMPONENTS
 import { ContainerCardComponent } from './components/container-card/container-card.component';
-import { UiRowSwitchComponent } from './components/ui/ui-row-switch/ui-row-switch.component';
-import { BatteryDetailComponent } from './components/battery/battery-detail/battery-detail.component';
+import { ContainerCollapsibleComponent } from './components/container-collapsible/container-collapsible.component';
 
 // APPLICATION UI COMPONENTS
 import { UiSwitchOnoffComponent } from './components/ui/ui-switch-onoff/ui-switch-onoff.component';
-import { UiSwitchTristateComponent } from './components/ui/ui-switch-tristate/ui-switch-tristate.component';
-import { UiRectangleRadioComponent } from './components/ui/ui-rectangle-radio/ui-rectangle-radio.component';
-import { ContainerCollapsibleComponent } from './components/container-collapsible/container-collapsible.component';
+import { BatteryDetailComponent } from './components/battery/battery-detail/battery-detail.component';
 
 // APPLICATION PAGE COMPONENTS
 import { PageDashboardComponent } from './components/pages/page-dashboard/page-dashboard.component';
@@ -74,11 +71,11 @@ import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { library } from '@fortawesome/fontawesome-svg-core';
 import { fas } from '@fortawesome/free-solid-svg-icons';
 import { fab } from '@fortawesome/free-brands-svg-icons';
-import { far } from '@fortawesome/free-regular-svg-icons';
 
 library.add(fas);
 library.add(fab);
-library.add(far);
+
+
 
 @NgModule({
 	declarations: [
@@ -113,17 +110,16 @@ library.add(far);
 		ModalWelcomeComponent,
 		ContainerCollapsibleComponent,
 		UiSwitchOnoffComponent,
-		UiRectangleRadioComponent,
-		UiSwitchTristateComponent,
 		BatteryDetailComponent,
-		UiRowSwitchComponent,
 	],
 	imports: [
 		BrowserModule,
 		AppRoutingModule,
 		FormsModule,
 		HttpClientModule,
+
 		FontAwesomeModule,
+
 		NgbModule
 	],
 	providers: [
