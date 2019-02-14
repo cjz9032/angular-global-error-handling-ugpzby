1ca1cee7f28a3059eb79b90a31d6f73b52f14eb0
raj <raj.kishor09@gmail.com>
2019-02-14 19:16:17 +0530

    VAN-237 : color code for battery updated based on mail from Scott. Added charging variable in indicator component

--- vtr-ui/src/app/components/battery/battery-indicator/battery-indicator.component.ts
+++ vtr-ui/src/app/components/battery/battery-indicator/battery-indicator.component.ts
@@ -1,16 +1,26 @@
-import { Component, OnInit, ViewChild, ElementRef, Input } from '@angular/core';
+import {
+	Component,
+	OnInit,
+	ViewChild,
+	ElementRef,
+	Input,
+	HostListener,
+	SimpleChanges,
+	OnChanges
+} from '@angular/core';
 
 @Component({
 	selector: 'vtr-battery-indicator',
 	templateUrl: './battery-indicator.component.html',
 	styleUrls: ['./battery-indicator.component.scss']
 })
-export class BatteryIndicatorComponent implements OnInit {
+export class BatteryIndicatorComponent implements OnInit, OnChanges {
 	private cssStyleDeclaration: CSSStyleDeclaration;
 
 	@ViewChild('battery') battery: ElementRef;
 	@ViewChild('batteryIndicator') batteryIndicator: ElementRef;
 
+	@Input() isCharging = true; // boolean indicator if its changing or not
 	@Input() percentage = 0; // number without % symbol
 	@Input() remainingHour = 0; // number of hours remaining
 	@Input() remainingMinutes = 0; // number of minutes remaining
@@ -22,91 +32,104 @@ export class BatteryIndicatorComponent implements OnInit {
 		this.refreshLevel();
 	}
 
+	ngOnChanges(changes: SimpleChanges): void {
+		console.log(changes);
+		if (changes['percentage'] && !changes['percentage'].firstChange) {
+			this.refreshLevel();
+		}
+	}
+
+	// Note : when page is resized, battery fill is not showing correctly.
+	// need to recalculate width based on page size
+	@HostListener('window:resize', ['$event'])
+	onResize(event) {
+		this.refreshLevel();
+	}
+
 	/**
 	 * based on decimal value its fills battery acid level
 	 * @param level decimal value ranging from 0.0 to 1.0
 	 */
 	refreshLevel() {
+		let level = 1,
+			fillWidth = 100;
 		if (this.percentage > 0 && this.percentage <= 100) {
-			const level = this.percentage / 100;
-			const {
-				borderColor,
-				backgroundColor,
-				fillColor
-			} = this.getLevelCssValues(level);
-
-			console.log(level, backgroundColor, borderColor, fillColor);
-
-			this.batteryIndicator.nativeElement.style.setProperty(
-				'--background-color',
-				backgroundColor
-			);
-
-			this.batteryIndicator.nativeElement.style.setProperty(
-				'--border-color',
-				borderColor
-			);
-			this.batteryIndicator.nativeElement.style.setProperty(
-				'--acid-fill-gradient',
-				fillColor
-			);
-
-			this.batteryIndicator.nativeElement.style.setProperty(
-				'--acid-width',
-				level * this.battery.nativeElement.clientWidth + 'px'
-			);
+			level = this.percentage / 100;
+			fillWidth = this.percentage;
 		}
+
+		const {
+			borderColor,
+			backgroundColor,
+			fillColor
+		} = this.getLevelCssValues(this.percentage);
+
+		this.batteryIndicator.nativeElement.style.setProperty(
+			'--background-color',
+			backgroundColor
+		);
+
+		this.batteryIndicator.nativeElement.style.setProperty(
+			'--border-color',
+			borderColor
+		);
+		this.batteryIndicator.nativeElement.style.setProperty(
+			'--acid-fill-gradient',
+			fillColor
+		);
+
+		this.batteryIndicator.nativeElement.style.setProperty(
+			'--acid-width',
+			`calc(${fillWidth}% - 0.85rem)`
+		);
 	}
 
 	getLevelCssValues(level: number): any {
+		// Green:	RemainTimePercent >= 25%
+		// Yellow:	RemainTimePercent  in [15%, 24%]
+		// Red:		RemainTimePercent < 15%
+		// RedCross with a black background: Battery is error.
+
 		let borderColor = '';
 		let backgroundColor = '';
 		let fillColor = '';
 
 		switch (true) {
-			case level < 0.2:
-				borderColor = this.getCssPropertyValue('--border-color-0-20');
+			case level >= 0 && level < 15: // red status
+				borderColor = this.getCssPropertyValue('--border-color-0-14');
 				backgroundColor = this.getCssPropertyValue(
-					'--background-color-0-20'
+					'--background-color-0-14'
 				);
 				fillColor = this.getCssPropertyValue(
-					'--acid-fill-gradient-0-20'
+					'--acid-fill-gradient-0-14'
 				);
 				break;
-			case level < 0.4:
-				borderColor = this.getCssPropertyValue('--border-color-21-40');
+			case level >= 15 && level < 25: // Yellow
+				borderColor = this.getCssPropertyValue('--border-color-15-24');
 				backgroundColor = this.getCssPropertyValue(
-					'--background-color-21-40'
+					'--background-color-15-24'
 				);
 				fillColor = this.getCssPropertyValue(
-					'--acid-fill-gradient-21-40'
+					'--acid-fill-gradient-15-24'
 				);
 				break;
-			case level < 0.6:
-				borderColor = this.getCssPropertyValue('--border-color-41-60');
+			case level >= 25: // green
+				borderColor = this.getCssPropertyValue('--border-color-25-100');
 				backgroundColor = this.getCssPropertyValue(
-					'--background-color-41-60'
+					'--background-color-25-100'
 				);
 				fillColor = this.getCssPropertyValue(
-					'--acid-fill-gradient-41-60'
+					'--acid-fill-gradient-25-100'
 				);
 				break;
-			case level < 0.8:
-				borderColor = this.getCssPropertyValue('--border-color-61-80');
+			default:
+				// -1 for battery error
+				borderColor = this.getCssPropertyValue('--border-color-error');
 				backgroundColor = this.getCssPropertyValue(
-					'--background-color-61-80'
+					'--background-color-error'
 				);
 				fillColor = this.getCssPropertyValue(
-					'--acid-fill-gradient-61-80'
-				);
-				break;
-			case level < 1:
-				borderColor = this.getCssPropertyValue('--border-color-81-100');
-				backgroundColor = this.getCssPropertyValue(
-					'--background-color-81-100'
-				);
-				fillColor = this.getCssPropertyValue(
-					'--acid-fill-gradient-81-100'
+					'--acid-fill-gradient-error'
 				);
 				break;
 		}
@@ -128,7 +151,10 @@ export class BatteryIndicatorComponent implements OnInit {
 
 	// returns windows object
 	getCssPropertyValue(propertyName: string): string {
-		return this.cssStyleDeclaration.getPropertyValue(propertyName);
+		if (this.cssStyleDeclaration) {
+			return this.cssStyleDeclaration.getPropertyValue(propertyName);
+		}
+		return '';
 	}
 
 	getCssDeclaration() {
