e8e283fe4f0704cc43c44e186d6e076fb764c9ef
raj <rsahu2@lenovo.com>
2019-02-25 12:48:31 +0530

    removed unused code

--- vtr-ui/src/app/components/pages/page-dashboard/page-dashboard.component.ts
+++ vtr-ui/src/app/components/pages/page-dashboard/page-dashboard.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 import { MockService } from '../../../services/mock/mock.service';
-import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
 	selector: 'vtr-page-dashboard',
@@ -15,26 +14,9 @@ export class PageDashboardComponent implements OnInit {
 		label: 'Customize Dashboard'
 	};
 
-	constructor(
-		public mockService: MockService,
-		private modalService: NgbModal
-	) {}
+	constructor(public mockService: MockService) {}
 
 	ngOnInit() {}
 
-	public onFeedBackClick(content: any): void {
-		this.modalService
-			.open(content, {
-				size: 'lg',
-				windowClass: 'battery-modal-size1'
-			})
-			.result.then(
-				result => {
-					// on open
-				},
-				reason => {
-					// on close
-				}
-			);
-	}
+	public onFeedBackClick(content: any): void {}
 }
