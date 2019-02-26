763484568bad752e54eb34820c43b6967a6377f7
meycry <meycry@zeoalliance.com>
2019-02-26 17:47:50 +0200

    sidebar-benefits-widget

--- vtr-ui/src/app/components/pages/page-privacy/main-layout/sidebar/sidebar-benefits-widget/sidebar-benefits-widget.component.ts
+++ vtr-ui/src/app/components/pages/page-privacy/main-layout/sidebar/sidebar-benefits-widget/sidebar-benefits-widget.component.ts
@@ -1,15 +1,50 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'vtr-sidebar-benefits-widget',
-  templateUrl: './sidebar-benefits-widget.component.html',
-  styleUrls: ['./sidebar-benefits-widget.component.scss']
+	selector: 'vtr-sidebar-benefits-widget',
+	templateUrl: './sidebar-benefits-widget.component.html',
+	styleUrls: ['./sidebar-benefits-widget.component.scss']
 })
 export class SidebarBenefitsWidgetComponent implements OnInit {
+	mainImageAlt = 'Stop trackers!';
+	mainImportantText = 'Stop trackers in their tracks';
+	mainText = 'Get the app that lets you choose who sees your info';
+	primaryButtonText = 'Try Lenovo Privacy';
 
-  constructor() { }
+	isOpen = false;
+	linkButtonText = this.isOpen ? 'Hide details' : 'Tell me more';
 
-  ngOnInit() {
-  }
+	benefitsItems = [
+		{
+			image: '/assets/images/privacy-tab/default.svg',
+			subtitle: 'Scan for breaches',
+			text: 'Start by finding out if any of your accounts have been part of a data breach.',
+		},
+		{
+			image: '/assets/images/privacy-tab/default.svg',
+			subtitle: 'Monitor future breaches',
+			text: 'If any of your accounts stored in Lenovo Privacy by FigLeaf are part of a breach, you’ll know about it',
+		},
+		{
+			image: '/assets/images/privacy-tab/default.svg',
+			subtitle: 'Blocks online trackers',
+			text: 'We’ll keep you private from online trackers, by blocking them on website you visit.',
+		},
+		{
+			image: '/assets/images/privacy-tab/default.svg',
+			subtitle: 'Mask your email',
+			text: 'Sign up at new sites without giving out your real email address.',
+		},
+	];
 
+	constructor() {
+	}
+
+	ngOnInit() {
+	}
+
+	handleClick(ev) {
+		this.isOpen = !this.isOpen;
+		this.linkButtonText = this.isOpen ? 'Hide details' : 'Tell me more';
+	}
 }
