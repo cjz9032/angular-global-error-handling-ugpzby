f526466271bcc8514700df6e6ee40e8aa091604c
zhara <zhara@zeoalliance.com>
2019-02-26 13:18:57 +0200

    add vtr-article-promo comp

---
+++ vtr-ui/src/app/components/pages/page-privacy/common-ui/article-promo/article-promo.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'vtr-article-promo',
+  templateUrl: './article-promo.component.html',
+  styleUrls: ['./article-promo.component.scss']
+})
+export class ArticlePromoComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
