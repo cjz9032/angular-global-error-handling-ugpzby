b3f2321d007670818c6d11df6bc4cba72272eb53
zhara <zhara@zeoalliance.com>
2019-02-26 20:28:08 +0200

    add promo block to breached-accounts page

--- vtr-ui/src/app/components/pages/page-privacy/pages/breached-accounts/breached-accounts.component.ts
+++ vtr-ui/src/app/components/pages/page-privacy/pages/breached-accounts/breached-accounts.component.ts
@@ -14,6 +14,27 @@ export class BreachedAccountsComponent {
 		title: 'Fix breaches and watch for future ones',
         text: 'Get the app that puts you back in control of your privacy'
 	};
+	public pageBannerData = {
+		title: 'Lenovo Privacy by FigLeaf — free for 14 days',
+		text: 'Lenovo Privacy by FigLeaf lets you share what you want or keep things private for each site you visit or transact with. Your email. Payment and billing info. Your location. Even your personal interests. No matter what you do er where you go, you decide your level of privacy.',
+		image_url: '/assets/images/privacy-tab/banner.png',
+		read_more_link: 'https://figleafapp.com/',
+	};
+	public promoFeaturesData = [
+		{
+			title: 'Scan foor breaches',
+			text: 'Start by finding out if any of your accounts have been part of data breach',
+		}, {
+			title: 'Block online trackers',
+			text: 'Do what you love online without being tracked by advertisers and others',
+		}, {
+			title: 'Monitor for future breaches',
+			text: 'If any of your accounts stored in Lenovo Privacy by FigLeaf are part of a breach, You’ll know about it.',
+		}, {
+			title: 'Mask your email',
+			text: 'Sign up at new sites without giving out your real email address',
+		},
+	];
 
 	constructor(private _location: Location, private serverCommunicationService: ServerCommunicationService) {
 	}
