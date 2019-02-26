06484b7d7f1136bf86123f6a17ee4a34b39dae04
raj <rsahu2@lenovo.com>
2019-02-26 16:52:15 +0530

    fixed issue - camera feed was disconnecting when we minimize Vantage App

--- vtr-ui/src/app/components/camera-control/camera-control.component.ts
+++ vtr-ui/src/app/components/camera-control/camera-control.component.ts
@@ -11,7 +11,14 @@ import { Subscription } from 'rxjs/internal/Subscription';
 })
 
 export class CameraControlComponent implements OnInit, OnDestroy {
+	public cameraDetail: CameraDetail;
+
 	private cameraPreview: ElementRef;
+	private _video: HTMLVideoElement;
+	private cameraDetailSubscription: Subscription;
+	private systemMediaControls: any;
+	private media: any;
+
 	@ViewChild('cameraPreview') set content(content: ElementRef) {
 		// when camera preview video element is visible then start camera feed
 		this.cameraPreview = content;
@@ -22,21 +29,32 @@ export class CameraControlComponent implements OnInit, OnDestroy {
 		}
 	}
 
-	@Input() cameraDetail: CameraDetail;
-
-	public dataSource: CameraDetail;
-	private _video: HTMLVideoElement;
-	private cameraDetailSubscription: Subscription;
-
 	constructor(
 		public cameraFeedService: CameraFeedService,
 		public baseCameraDetail: BaseCameraDetail
-	) {}
+	) {
+		this.cameraDetail = new CameraDetail();
+
+		//#region below logic required to re-enable camera feed when window is maximized from minimized state
+
+		this.media = this.getMedia();
+		if (this.media) {
+			this.systemMediaControls = this.media.SystemMediaTransportControls.getForCurrentView();
+			this.systemMediaControls.addEventListener(
+				'propertychanged',
+				(args: any) => {
+					this.systemMediaControls_propertyChanged(args);
+				}
+			);
+		}
+
+		//#endregion
+	}
 
 	ngOnInit() {
 		this.cameraDetailSubscription = this.baseCameraDetail.cameraDetailObservable.subscribe(
 			(cameraDetail: CameraDetail) => {
-				this.dataSource = cameraDetail;
+				this.cameraDetail = cameraDetail;
 			},
 			error => {
 				console.log(error);
@@ -47,6 +65,12 @@ export class CameraControlComponent implements OnInit, OnDestroy {
 	ngOnDestroy() {
 		this.deactivateCamera();
 		this.cameraDetailSubscription.unsubscribe();
+		if (this.systemMediaControls) {
+			this.systemMediaControls.removeEventListener(
+				'propertychanged',
+				this.systemMediaControls_propertyChanged
+			);
+		}
 	}
 
 	public onAutoExposureChange($event: any) {
@@ -70,5 +94,32 @@ export class CameraControlComponent implements OnInit, OnDestroy {
 		if (this._video) {
 			this._video.pause();
 		}
+	}
+
+	// return Media object from WinJS library
+	private getMedia(): any {
+		const win: any = window;
+		if (win.Windows) {
+			return win.Windows.Media;
+		}
+		return null;
+	}
+
+	/**
+	 * Check to see if the app is being muted. If so, it is being minimized.
+     * Otherwise if it is not initialized, it is being brought into focus.
+	 * @param args argument received from WinJS propertyChanged event
+	 */
+	private systemMediaControls_propertyChanged(args: any) {
+		console.log('systemMediaControls_propertyChanged', args);
+		const win: any = window;
+		if (win.Windows) {
+			if (args.target.soundLevel === win.Windows.Media.SoundLevel.muted) {
+				this.deactivateCamera();
+			} else if (!this.cameraDetail.isPrivacyModeEnabled) {
+				this.activateCamera();
+			}
+		}
+
 	}
 }
