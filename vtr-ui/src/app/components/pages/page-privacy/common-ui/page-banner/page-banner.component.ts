8764ea86436e5b74d2c6cb5e24f80493ad50136f
zhara <zhara@zeoalliance.com>
2019-02-26 15:01:55 +0200

    add styles and logic for vtr-article-promo comp

--- vtr-ui/src/app/components/pages/page-privacy/common-ui/page-banner/page-banner.component.ts
+++ vtr-ui/src/app/components/pages/page-privacy/common-ui/page-banner/page-banner.component.ts
@@ -6,6 +6,5 @@ import { Component, Input } from '@angular/core';
 	styleUrls: ['./page-banner.component.scss'],
 })
 export class PageBannerComponent {
-	@Input() title: string;
-	@Input() text: string;
+	@Input() data: {title: string, text: string, image_url: string, read_more_link?: string};
 }
--- vtr-ui/src/app/components/pages/page-privacy/pages/browser-accounts/browser-accounts.component.ts
+++ vtr-ui/src/app/components/pages/page-privacy/pages/browser-accounts/browser-accounts.component.ts
@@ -9,6 +9,12 @@ import {Location} from '@angular/common';
 export class BrowserAccountsComponent {
 	// static Data for html
 	public browserStoredAccountsData = {showDetailAction: 'expand', showBanner: true};
+	public pageBannerData = {
+		title: 'Lenovo Privacy by FigLeaf — free for 14 days',
+		text: 'Lenovo Privacy by FigLeaf lets you share what you want or keep things private for each site you visit or transact with. Your email. Payment and billing info. Your location. Even your personal interests. No matter what you do er where you go, you decide your level of privacy.',
+		image_url: '/assets/images/privacy-tab/banner.png',
+        read_more_link: '',
+    };
 
     constructor(private _location: Location) {
     }
