3e0ec25817a84ba7508270f311ce3c8dce369feb
zhara <zhara@zeoalliance.com>
2019-02-27 16:14:30 +0200

    add vtr-scan-steps component

---
+++ vtr-ui/src/app/components/pages/page-privacy/common-ui/scan-steps/scan-steps.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'vtr-scan-steps',
+  templateUrl: './scan-steps.component.html',
+  styleUrls: ['./scan-steps.component.scss']
+})
+export class ScanStepsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
