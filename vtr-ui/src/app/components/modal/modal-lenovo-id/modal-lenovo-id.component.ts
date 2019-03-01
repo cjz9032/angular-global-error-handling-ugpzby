69a875c6fb5a0be29fff50084f91b50825e56520
kevin <yhkuang@lenovo.com>
2019-03-01 18:05:44 +0800

    VAN-362 Lenovo ID logon Ui can use Lenovo ID SDK start logon process.

--- vtr-ui/src/app/components/modal/modal-lenovo-id/modal-lenovo-id.component.ts
+++ vtr-ui/src/app/components/modal/modal-lenovo-id/modal-lenovo-id.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit, HostListener } from '@angular/core';
 import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
 import { UserService } from '../../../services/user/user.service';
-//import { Phoenix } from '@lenovo/tan-client-bridge';
+//import { VantageShellService } from '../../../services/vantage-shell/vantage-shell.service'
 import { Subscription, timer } from "rxjs";
 
 //TODO: Create facebook new account within UWP WebView control will increase memory rapidly and crash app fianlly,
@@ -19,7 +19,8 @@ export class ModalLenovoIdComponent implements OnInit {
   private detectConnectionStatusTimer = timer(5000, 5000);
   constructor(
     public activeModal: NgbActiveModal,
-    private userService: UserService
+    private userService: UserService,
+    //private vantageShellService: VantageShellService
   ) {
     this.isOnline = false;
     this.cacheCleared = false;
@@ -71,20 +72,20 @@ export class ModalLenovoIdComponent implements OnInit {
       invokeScriptAsync?: Function
     }
 
+    var self = this;
     var webView = document.querySelector("#lid-webview") as MsWebView;
     // TODO: call JS bridge to get logon url and navigate to it
-    //var logonUrl = Phoenix.getLoginUrl();
+    //var loginUrl = self.vantageShellService.getLoginUrl();
     //var logonUrl = 'https://sso.lenovo.com/ssoserver/authorizeclient/v1/login?aid=e4af23f9161ef931df61ef4a1af95fa16e91640af2bdd3dabee0f74309e5123b&did=6435aa64a36a7877afd654952a78cbabafa24bb3a0dd42f35d11484f514b80ab0&sid=5c13238222d5c3a0a4ff930ea5de6c5a24875dbb63ef6d6837be1dc972a03591&sign=d7b7806c03fc5594f01c8ee77d73b494c1f99070f9e508013666b463e3a75a3b';
-    var logonUrl = 'https://passport.lenovo.com/wauthen5/userLogout?lenovoid.action=uilogout&lenovoid.display=null';
+    var loginUrl = 'https://passport.lenovo.com/wauthen5/userLogout?lenovoid.action=uilogout&lenovoid.display=null';
     //Uncomment when use logon url from JS bridge
-    //if (logonUrl.indexOf("sso.lenovo.com") === -1) {
+    //if (loginUrl.indexOf("sso.lenovo.com") === -1) {
       // TODO: Set success return code and close this logon dialog
     //  return;
     //} else {
       // TODO: call JS bridge to get current system local and set to url
     //}
-    webView.src = logonUrl;
-    var self = this;
+    webView.src = loginUrl;
     webView.addEventListener("MSWebViewNavigationCompleted", function (EventArgs) {
       var webViewEvent = EventArgs as WebViewEvent;
       if (webViewEvent.isSuccess) {
@@ -96,7 +97,7 @@ export class ModalLenovoIdComponent implements OnInit {
         if (EventArgs.srcElement.documentTitle.startsWith("Login success")) {
           self.captureWebViewContent(webView).then((htmlContent: any) => {
             try {
-              // Parse html content to get username, useruad, userid, userguid, firstname and lastname
+              // Parse html content to get user info
               var parser = new DOMParser();
               var el = parser.parseFromString(htmlContent, "text/html").documentElement;
               var username = (el.querySelector('#username') as HTMLInputElement).value;
---
+++ vtr-ui/src/app/services/vantage-shell/vantage-shell.service.ts
@@ -0,0 +1,40 @@
+/// <reference path='../../../../node_modules/@lenovo/tan-client-bridge/src/index.js' />
+
+import { Injectable } from '@angular/core';
+import * as inversify from 'inversify';
+
+import bootstrap from '@lenovo/tan-client-bridge/src/index';
+
+
+@Injectable({
+    providedIn: 'root'
+})
+export class VantageShellService {
+
+    private phoenix: any;
+    constructor() {
+        console.log('VantageShellService');
+
+        const shell = this.getVantageShell();
+        if (shell) {
+            this.phoenix = bootstrap(
+                new inversify.Container(),
+                new shell.VantageRpcClient()
+            );
+            console.log('VantageRpcClient', JSON.stringify(this.phoenix));
+        }
+    }
+
+    private getVantageShell(): any {
+        const win: any = window;
+        return win.VantageShellExtension;
+    }
+
+    public getLoginUrl(): any {
+        if (this.phoenix && this.phoenix.LID) {
+			this.phoenix.LID
+            return this.phoenix.LID.getLoginUrl();
+        }
+        return undefined;
+    }
+}
