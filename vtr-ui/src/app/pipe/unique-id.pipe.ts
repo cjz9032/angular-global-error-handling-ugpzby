235e949cd3f07b7d8f5bca5fef2b5f1a5554e651
Santosh udnur <sudnur@lenovo.com>
2019-03-05 18:14:47 +0530

    fix space issue battery threshold settings,syntax issue in page-device ,widget device,qa service, created unique-id to generate unique id for input field for fixing VAN-920, Fixed VAN-920 for not getting the optimise mic phone settings radio buttons

---
+++ vtr-ui/src/app/pipe/unique-id.pipe.ts
@@ -0,0 +1,15 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+	name: 'uniqueId'
+})
+export class UniqueIdPipe implements PipeTransform {
+
+	transform(value: any, args?: any): any {
+		// Random number is used to have unique id of each input field
+		let randomNumber = Math.floor(new Date().valueOf() * Math.random());
+
+		return value + randomNumber;
+	}
+
+}
