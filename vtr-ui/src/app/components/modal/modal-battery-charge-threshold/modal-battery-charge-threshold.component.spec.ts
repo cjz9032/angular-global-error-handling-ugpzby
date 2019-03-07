9616c7df4da3c15b0328d43c9284ba85e43df613
sateesh nagarjuna <snagarjuna4@lenovo.com>
2019-03-07 16:48:19 +0530

    sateesh-defect-fixes fixed issues in password internet windows hello pages

---
+++ vtr-ui/src/app/components/modal/modal-battery-charge-threshold/modal-battery-charge-threshold.component.spec.ts
@@ -0,0 +1,25 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { ModalBatteryChargeThresholdComponent } from './modal-battery-charge-threshold.component';
+
+describe('ModalBatteryChargeThresholdComponent', () => {
+  let component: ModalBatteryChargeThresholdComponent;
+  let fixture: ComponentFixture<ModalBatteryChargeThresholdComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ ModalBatteryChargeThresholdComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(ModalBatteryChargeThresholdComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
