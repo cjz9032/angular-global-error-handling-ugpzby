cb194c75d99ce8d493ded856230ab31c2aadfd64
meycry <meycry@zeoalliance.com>
2019-03-07 12:08:40 +0200

    improve scan steps

--- vtr-ui/src/app/components/pages/page-privacy/common-ui/scan-steps/scan-steps.component.ts
+++ vtr-ui/src/app/components/pages/page-privacy/common-ui/scan-steps/scan-steps.component.ts
@@ -5,12 +5,32 @@ import { Component, OnInit, Input } from '@angular/core';
   templateUrl: './scan-steps.component.html',
   styleUrls: ['./scan-steps.component.scss']
 })
+
+
 export class ScanStepsComponent implements OnInit {
   @Input() innerIndent: boolean;
-  @Input() steps: Array<{text: string, subtext: string}>;
+  @Input() steps: 'scanSteps' | 'verifyPopUpSteps';
+
   constructor() { }
 
+  private scanSteps = [
+    {text: 'Enter your email', subtext: 'We’ll search the internet and the dark web for your info'},
+    {text: 'Verify it’s you', subtext: 'Check your email for a verification code'},
+    {text: 'See your privacy level', subtext: 'Find out if you have control of your privacy online.'}
+  ];
+  private verifyPopUpSteps = [
+    {text: 'email', subtext: 'text'},
+    {text: 'text2', subtext: 'text2'},
+    {text: 'text 3', subtext: 'text3'}
+  ];
+  private _steps = [];
+
   ngOnInit() {
+    if (this[this.steps]) {
+      this._steps = this[this.steps];
+    } else {
+      console.error('invalid param steps in ScanStepsComponent');
+    }
   }
 
 }
--- vtr-ui/src/app/components/pages/page-privacy/pages/scan/scan.component.ts
+++ vtr-ui/src/app/components/pages/page-privacy/pages/scan/scan.component.ts
@@ -11,9 +11,4 @@ export class ScanComponent {
 		text: 'A simple comprehensive app that lets you share what you want or keep things private for each site you visit or transact with.',
 		image_url: '/assets/images/privacy-tab/banner.png',
 	};
-	public scanSteps = [
-		{text: 'Enter your email', subtext: 'We’ll search the internet and the dark web for your info'},
-		{text: 'Verify it’s you', subtext: 'Check your email for a verification code'},
-		{text: 'See your privacy level', subtext: 'Find out if you have control of your privacy online.'}
-	]
 }
