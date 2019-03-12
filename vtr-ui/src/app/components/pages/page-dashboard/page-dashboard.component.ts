2746878730df6157f5b30ffb4e8e40b257b16f06
Santosh udnur <sudnur@lenovo.com>
2019-03-12 19:02:41 +0530

    Fixes for defects post system status widget integration : VAN-1140 , VAN-1217 , VAN-1238, VAN-1166 , VAN-1157 , VAN-1234 , VAN-1233 ,VAN-1229

--- vtr-ui/src/app/components/pages/page-dashboard/page-dashboard.component.ts
+++ vtr-ui/src/app/components/pages/page-dashboard/page-dashboard.component.ts
@@ -135,9 +135,11 @@ export class PageDashboardComponent implements OnInit {
 			warranty.id = 'warranty';
 			warranty.title = 'Warranty';
 			warranty.detail = 'Warranty not found';
-			warranty.path = 'ms-settings:storagesense';
+			/* warranty.path = 'ms-settings:storagesense'; */
+			warranty.path = '/support';
 			warranty.asLink = false;
-			warranty.isSystemLink = true;
+			/* warranty.isSystemLink = true; */
+			warranty.isSystemLink = false;
 
 			if (response.warranty) {
 				// const status = response.warranty.status;
@@ -166,6 +168,8 @@ export class PageDashboardComponent implements OnInit {
 			}
 			systemStatus.push(systemUpdate);
 		}
+
+		console.log('systemStatus ' + JSON.stringify(systemStatus));
 		return systemStatus;
 	}
 }
