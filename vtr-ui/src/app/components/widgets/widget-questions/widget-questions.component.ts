1084a9b2c2c343d052ce410f2d55c8f230bc71a7
vikas bansal <vbansal@lenovo.com>
2019-03-26 19:01:20 +0530

    add more changes to qa

--- vtr-ui/src/app/components/widgets/widget-questions/widget-questions.component.ts
+++ vtr-ui/src/app/components/widgets/widget-questions/widget-questions.component.ts
@@ -1,17 +1,19 @@
 import { Component, OnInit, Input } from '@angular/core';
+import {BaseComponent} from "../../base/base.component";
 
 @Component({
 	selector: 'vtr-widget-questions',
 	templateUrl: './widget-questions.component.html',
 	styleUrls: ['./widget-questions.component.scss']
 })
-export class WidgetQuestionsComponent implements OnInit {
+export class WidgetQuestionsComponent extends BaseComponent implements OnInit {
 
 	@Input() title: string;
 	@Input() description: string;
 	@Input() items: any[];
 
 	constructor() {
+	  super();
 	}
 
 	ngOnInit() {
