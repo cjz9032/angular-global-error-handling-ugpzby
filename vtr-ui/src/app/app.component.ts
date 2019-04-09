560ec78140296619b1c456b3baf461362367fc6b
raj <rsahu2@lenovo.com>
2019-04-09 15:49:10 +0530

    build error fixed

--- vtr-ui/src/app/app.component.ts
+++ vtr-ui/src/app/app.component.ts
@@ -35,25 +35,25 @@ export class AppComponent implements OnInit {
 		this.translate.setDefaultLang('en');
 		const tutorial: WelcomeTutorial = commonService.getLocalStorageValue(LocalStorageKey.WelcomeTutorial);
 
-		const modalRef = this.modalService.open(ModalWelcomeComponent,
-			{
-				backdrop: 'static'
-				, windowClass: 'welcome-modal-size'
-			});
-		modalRef.result.then(
-			(result: WelcomeTutorial) => {
-				// on open
-				console.log('welcome-modal-size', result);
-				commonService.setLocalStorageValue(LocalStorageKey.WelcomeTutorial, result);
-			},
-			(reason: WelcomeTutorial) => {
-				// on close
-				console.log('welcome-modal-size', reason);
-				commonService.setLocalStorageValue(LocalStorageKey.WelcomeTutorial, reason);
-			}
-		);
+		// const modalRef = this.modalService.open(ModalWelcomeComponent,
+		// 	{
+		// 		backdrop: 'static'
+		// 		, windowClass: 'welcome-modal-size'
+		// 	});
+		// modalRef.result.then(
+		// 	(result: WelcomeTutorial) => {
+		// 		// on open
+		// 		console.log('welcome-modal-size', result);
+		// 		commonService.setLocalStorageValue(LocalStorageKey.WelcomeTutorial, result);
+		// 	},
+		// 	(reason: WelcomeTutorial) => {
+		// 		// on close
+		// 		console.log('welcome-modal-size', reason);
+		// 		commonService.setLocalStorageValue(LocalStorageKey.WelcomeTutorial, reason);
+		// 	}
+		// );
 
-		/*if (tutorial === undefined && navigator.onLine) {
+		if (tutorial === undefined && navigator.onLine) {
 			const modalRef = this.modalService.open(ModalWelcomeComponent,
 				{
 					backdrop: 'static'
