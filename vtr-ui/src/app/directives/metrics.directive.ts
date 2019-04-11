e8a58d2a9c2c3858044693a70f6fec761c7650c0
vikas bansal <vbansal@lenovo.com>
2019-04-11 06:34:24 +0530

    implement item parent

--- vtr-ui/src/app/directives/metrics.directive.ts
+++ vtr-ui/src/app/directives/metrics.directive.ts
@@ -103,58 +103,5 @@ export class MetricsDirective {
 		}
 	}
 
-	getParents(elem, selector?) {
-
-		var parents = [];
-		var firstChar;
-		if (selector) {
-			firstChar = selector.charAt(0);
-		}
-
-		// Get matches
-		for (; elem && elem !== document; elem = elem.parentNode) {
-			if (selector) {
-
-				// If selector is a class
-				if (firstChar === '.') {
-					if (elem.classList.contains(selector.substr(1))) {
-						parents.push(elem);
-					}
-				}
-
-				// If selector is an ID
-				if (firstChar === '#') {
-					if (elem.id === selector.substr(1)) {
-						parents.push(elem);
-					}
-				}
-
-				// If selector is a data attribute
-				if (firstChar === '[') {
-
-					if (elem.hasAttribute(selector.substr(1, selector.length - 2))) {
-						parents.push(elem.getAttribute(selector.substr(1, selector.length - 2)));
-					}
-				}
-
-				// If selector is a tag
-				if (elem.tagName.toLowerCase() === selector) {
-					parents.push(elem);
-				}
-
-			} else {
-				parents.push(elem);
-			}
-
-		}
-
-		// Return parents if any exist
-		if (parents.length === 0) {
-			return null;
-		} else {
-			return parents.reverse();
-		}
-
-	}
 
 }
