2cd129f89dd15724e3bf56b554f442dbe3316d84
raj <rsahu2@lenovo.com>
2019-04-11 15:04:30 +0530

    VAN-2048 fixed app crash issue due to camera API

--- vtr-ui/src/app/components/camera-control/camera-control.component.ts
+++ vtr-ui/src/app/components/camera-control/camera-control.component.ts
@@ -24,25 +24,13 @@ export class CameraControlComponent implements OnInit, OnDestroy {
 	private cameraPreview: ElementRef;
 	private _video: HTMLVideoElement;
 	private cameraDetailSubscription: Subscription;
-	private systemMediaControls: any;
-	private media: any;
 	private logger: any;
 	private Windows: any;
 	private Capture: any;
 	private DeviceInformation: any;
 	private DeviceClass: any;
-
-	// private Capture = Windows.Media.Capture;
-	// private DeviceInformation = Windows.Devices.Enumeration.DeviceInformation;
-	// private DeviceClass = Windows.Devices.Enumeration.DeviceClass;
-	// private DisplayOrientations = Windows.Graphics.Display.DisplayOrientations;
-	// private Imaging = Windows.Graphics.Imaging;
-	// private Media = Windows.Media;
-	// private StorageLibrary = Windows.Storage.StorageLibrary;
-	// private KnownLibraryId = Windows.Storage.KnownLibraryId;
-	// private ApplicationData = Windows.Storage.ApplicationData;
-	
 	private oMediaCapture: any;
+	private visibilityChange: any;
 
 	@ViewChild('cameraPreview') set content(content: ElementRef) {
 		// when camera preview video element is visible then start camera feed
@@ -50,7 +38,6 @@ export class CameraControlComponent implements OnInit, OnDestroy {
 		if (content && !this.cameraDetail.isPrivacyModeEnabled) {
 			this.initializeCameraAsync();
 		} else {
-			
 			this.cleanupCameraAsync();
 		}
 	}
@@ -60,22 +47,39 @@ export class CameraControlComponent implements OnInit, OnDestroy {
 		public baseCameraDetail: BaseCameraDetail,
 		private vantageShellService: VantageShellService
 	) {
-		//debugger;
 		this.Windows = vantageShellService.getWindows();
 		this.Capture = this.Windows.Media.Capture;
 		this.DeviceInformation = this.Windows.Devices.Enumeration.DeviceInformation;
 		this.DeviceClass = this.Windows.Devices.Enumeration.DeviceClass;
-		// this.cameraDetail = new CameraDetail();
 
 		//#region below logic required to re-enable camera feed when window is maximized from minimized state
 		this.logger = this.vantageShellService.getLogger();
 		this.logger.info('constructor camera');
-		
 		this.oMediaCapture = new this.Capture.MediaCapture();
-
-		document.addEventListener('visibilitychange', this.onVisibilityChanged.bind(this));
-	
+		this.visibilityChange = this.onVisibilityChanged.bind(this);
+		document.addEventListener('visibilitychange', this.visibilityChange);
 		//#endregion
+	}
+
+	ngOnInit() {
+		this.cameraDetailSubscription = this.baseCameraDetail.cameraDetailObservable.subscribe(
+			(cameraDetail: CameraDetail) => {
+				this.cameraDetail = cameraDetail;
+			},
+			error => {
+				console.log(error);
+			}
+		);
+	}
+
+	ngOnDestroy() {
+		if (this.baseCameraDetail) {
+			this.cameraDetailSubscription.unsubscribe();
+		}
+		if (this._video.src !== '') {
+			this.cleanupCameraAsync();
+		}
+		document.removeEventListener('visibilitychange', this.visibilityChange);
 	}
 
 	findCameraDeviceByPanelAsync(panel) {
@@ -94,7 +98,6 @@ export class CameraControlComponent implements OnInit, OnDestroy {
 				if (!deviceInfo && devices.length > 0) {
 					deviceInfo = devices.getAt(0);
 				}
-
 				return deviceInfo;
 			});
 	}
@@ -110,18 +113,17 @@ export class CameraControlComponent implements OnInit, OnDestroy {
 					return;
 				}
 				self.oMediaCapture = new self.Windows.Media.Capture.MediaCapture();
-			
 
 				// Register for a notification when something goes wrong
-				// TODO: define the fail handle callback and show errormessage maybe... there's a chance another app is previewing camera, that's when failed happen.
-				self.oMediaCapture.addEventListener('failed', () => { 
-					console.log("failed to capture camera");
+				// TODO: define the fail handle callback and show error message maybe... there's a chance another app is previewing camera, that's when failed happen.
+				self.oMediaCapture.addEventListener('failed', () => {
+					console.log('failed to capture camera');
 					self.cleanupCameraAsync();
 				});
 
 				const settings = new self.Capture.MediaCaptureInitializationSettings();
 				settings.videoDeviceId = camera.id;
-				
+
 				// Initialize media capture and start the preview
 				return self.oMediaCapture.initializeAsync(settings);
 
@@ -147,7 +149,6 @@ export class CameraControlComponent implements OnInit, OnDestroy {
 
 	cleanupCameraAsync() {
 		console.log('cleanupCameraAsync');
-		const promiseList = {};
 		this.stopPreview();
 
 		if (this.oMediaCapture != null) {
@@ -164,28 +165,6 @@ export class CameraControlComponent implements OnInit, OnDestroy {
 		}
 	}
 
-	ngOnInit() {
-		console.log('camera control onInit');
-		this.cameraDetailSubscription = this.baseCameraDetail.cameraDetailObservable.subscribe(
-			(cameraDetail: CameraDetail) => {
-				console.log('camera detail observable', cameraDetail);
-				this.cameraDetail = cameraDetail;
-			},
-			error => {
-				console.log(error);
-			}
-		);
-	}
-
-	ngOnDestroy() {
-		if (this.baseCameraDetail) {
-			this.cameraDetailSubscription.unsubscribe();
-		}
-		if (this._video.src !== '') {
-			this.cleanupCameraAsync();
-		}
-	}
-
 	public onAutoExposureChange($event: any) {
 		try {
 			this.showAutoExposureSlider = !$event.switchValue;
@@ -196,41 +175,16 @@ export class CameraControlComponent implements OnInit, OnDestroy {
 		}
 	}
 
-	// return Media object from WinJS library
-	private getMedia(): any {
-		const win: any = window;
-		if (win.Windows) {
-			return win.Windows.Media;
-		}
-		return null;
-	}
-
-	/**
-	 * Check to see if the app is being muted. If so, it is being minimized.
-     * Otherwise if it is not initialized, it is being brought into focus.
-	 * @param args argument received from WinJS propertyChanged event
-	 */
-	private systemMediaControls_propertyChanged(args: any) {
-		console.log('systemMediaControls_propertyChanged', args);
-		const win: any = window;
-		if (win.Windows) {
-			if (args.target.soundLevel === win.Windows.Media.SoundLevel.muted) {
-				this.cleanupCameraAsync();
-			} else if (this._video.paused) {//only initialize when it's already paused
-				this.initializeCameraAsync();
-			}
-		}
-	}
 	public onBrightnessSliderChange($event: ChangeContext) {
-		console.log('Brightness changed', event);
+		console.log('Brightness changed', $event);
 		this.brightnessChange.emit($event);
 	}
 	public onContrastSliderChange($event: ChangeContext) {
-		console.log('Contrast changed', event);
+		console.log('Contrast changed', $event);
 		this.contrastChange.emit($event);
 	}
 	public onExposureSliderChange($event: ChangeContext) {
-		console.log('exposure changed', event);
+		console.log('exposure changed', $event);
 		this.exposureChange.emit($event);
 	}
 }
