1216af0985675ee8acea33ec9705629f66de4220
Kidd Liu <kiddliu@lenovo.com>
2019-04-12 17:45:06 +0800

    add a directive for NLS string

--- vtr-ui/src/app/app.module.ts
+++ vtr-ui/src/app/app.module.ts
@@ -133,7 +133,7 @@ import { WidgetSecurityStatusComponent } from './components/widgets/widget-secur
 
 
 import { MetricsDirective } from './directives/metrics.directive';
-import { DynamicDirective } from './directives/dynamic.directive';
+import { TranslateDirective } from './directives/translate.directive';
 import { InstallationHistoryComponent } from './components/pages/page-device-updates/children/installation-history/installation-history.component';
 import { SeparatePascalCasePipe } from './pipe/separate-pascal-case.pipe';
 import { ModalCommonConfirmationComponent } from './components/modal/modal-common-confirmation/modal-common-confirmation.component';
@@ -161,11 +161,6 @@ import { SubTransformPipe } from './pipe/security-antivirus/sub-transform.pipe';
 import { DateClassPipe } from './pipe/security-antivirus/date-class.pipe';
 import { ModalAboutComponent } from './components/modal/modal-about/modal-about.component';
 import { WifiClassPipe } from './pipe/security-landing/wifi-class.pipe';
-import { TagPickerPipe } from './pipe/translation/tag-picker.pipe';
-import { TextPickerPipe } from './pipe/translation/text-picker.pipe';
-import { WidgetTranslatePickerComponent } from './components/widgets/widget-translate-picker/widget-translate-picker.component';
-import { TranslatePickerBaseComponent } from './components/widgets/widget-translate-picker/base/translate-picker-base/translate-picker-base.component';
-import { TranslatePickerTailComponent } from './components/widgets/widget-translate-picker/base//translate-picker-tail/translate-picker-tail.component';
 import { JoinclassPipe } from './pipe/security-wifi/join-class.pipe';
 import { SuccessClassPipe } from './pipe/security-wifi/success-class.pipe';
 
@@ -255,7 +250,7 @@ library.add(fal);
 		StatusTextPipe,
 		ModalBatteryChargeThresholdComponent,
 		MetricsDirective,
-		DynamicDirective,
+		TranslateDirective,
 		InstallationHistoryComponent,
 		SeparatePascalCasePipe,
 		ModalCommonConfirmationComponent,
@@ -289,11 +284,6 @@ library.add(fal);
 		DateClassPipe,
 		ModalAboutComponent,
 		WifiClassPipe,
-		TagPickerPipe,
-		TextPickerPipe,
-		WidgetTranslatePickerComponent,
-		TranslatePickerBaseComponent,
-		TranslatePickerTailComponent,
 		JoinclassPipe,
 		SuccessClassPipe
 	],
