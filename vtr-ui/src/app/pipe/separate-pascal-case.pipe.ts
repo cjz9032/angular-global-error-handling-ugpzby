4f72a9f25c7d0a217933bddd71fad76de8fd3cc3
vikas bansal <vbansal@lenovo.com>
2019-04-15 16:53:30 +0530

    add uppercase pipe

--- vtr-ui/src/app/pipe/separate-pascal-case.pipe.ts
+++ vtr-ui/src/app/pipe/separate-pascal-case.pipe.ts
@@ -7,7 +7,7 @@ export class SeparatePascalCasePipe implements PipeTransform {
 
   transform(value: any, args?: any): any {
     let pascalCaseString = value.match(/[A-Z][a-z]+|[0-9]+/g).join(' ');
-    return pascalCaseString.toUpperCase();
+    return pascalCaseString.toLowerCase();
   }
 
 }
