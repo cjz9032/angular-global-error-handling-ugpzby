d04c2ac7afe81f07dba79e9bc7e6429023b1e7fd
gamberkar <gamberkar@gmail.com>
2019-04-24 13:38:51 +0530

    VAN-419 tutorial page new layout

--- vtr-ui/src/app/components/modal/modal-welcome/modal-welcome.component.ts
+++ vtr-ui/src/app/components/modal/modal-welcome/modal-welcome.component.ts
@@ -8,67 +8,18 @@ import { WelcomeTutorial } from 'src/app/data-models/common/welcome-tutorial.mod
 	styleUrls: ['./modal-welcome.component.scss']
 })
 export class ModalWelcomeComponent implements OnInit {
-
 	page = 1;
-
 	checkedArray: string[] = [];
-
 	data: any = {
-		page1: {
-			title: 'Almost There',
-			subtitle: 'Just a few more buttons to press before we can \n have liftoff. Before we can give you the most optimal experience there are still a few things to setup'
-		},
 		page2: {
 			title: 'How will you use it?',
 			subtitle: 'Click on one of these uses to tell is how you will use this machine?',
-			radioValue: null
-		},
-		// page3: {
-		// 	title: 'What are your interests?',
-		// 	subtitle: 'Click on one of these uses to tell is how you will use this machine?',
-		// 	buttons: [
-		// 		[
-		// 			{
-		// 				label: 'GAMES',
-		// 				value: 'games'
-		// 			},
-		// 			{
-		// 				label: 'NEWS',
-		// 				value: 'news'
-		// 			},
-		// 			{
-		// 				label: 'ENTERTAINMENT',
-		// 				value: 'entertainment'
-		// 			},
-		// 			{
-		// 				label: 'ARTS',
-		// 				value: 'arts'
-		// 			}
-		// 		],
-		// 		[
-		// 			{
-		// 				label: 'TECHNOLOGY',
-		// 				value: 'technology'
-		// 			},
-		// 			{
-		// 				label: 'POLITICS',
-		// 				value: 'politics'
-		// 			}, {
-		// 				label: 'SPORTS',
-		// 				value: 'sports'
-		// 			}
-		// 		],
-		// 		[
-		// 			{
-		// 				label: 'REGIONAL NEWS',
-		// 				value: 'regional news'
-		// 			}
-		// 		]
-		// 	]
-		// }
+			radioValue: null,
+		}
 	};
 
-	constructor(public activeModal: NgbActiveModal) {
+	constructor(public activeModal: NgbActiveModal, public welcomeDatamodel: WelcomeTutorial) {
+
 	}
 
 	ngOnInit() {
@@ -85,21 +36,34 @@ export class ModalWelcomeComponent implements OnInit {
 
 	toggle($event, value) {
 		if ($event.target.checked) {
-			this.checkedArray.push(value);
+			// this.checkedArray.push(value);
+			this.welcomeDatamodel.interests.push(value);
 		} else {
-			this.checkedArray.splice(this.checkedArray.indexOf(value), 1);
+			// this.checkedArray.splice(this.checkedArray.indexOf(value), 1);
+			this.welcomeDatamodel.interests.splice(this.welcomeDatamodel.interests.indexOf(value), 1);
 		}
+		console.log(this.welcomeDatamodel.interests.length);
 	}
 
-	public onTutorialClose() {
+	saveUsageType($event, value){
+		if ($event.target.checked) {
+			this.welcomeDatamodel.usageType = value;
+			console.log(this.welcomeDatamodel.usageType);
+		}
+
+	}
+	onTutorialClose() {
 		this.activeModal.dismiss(new WelcomeTutorial(true));
 	}
 
-	public onTutorialMaximise() {
-		this.activeModal.dismiss(new WelcomeTutorial(true));
+	onTutorialDone() {
+		this.welcomeDatamodel.isTutorialCompleted = true;
+		this.onTutorialClose();
 	}
 
-	public onTutorialMinimise() {
-		this.activeModal.dismiss(new WelcomeTutorial(true));
+	savePrivacy($event, value) {
+		if ($event.target.checked) {
+			this.welcomeDatamodel.isPrivacyPolicy = value;
+		}
 	}
 }
