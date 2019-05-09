d89378fb069fe4614f03f125e8323ca71e36a2fc
raghu <rtenneti@lenovo.com>
2019-05-09 19:22:08 +0530

    gaming dashboard routing and design

---
+++ vtr-ui/src/app/components/pages/page-hardwarescan/page-hardwarescan.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'vtr-page-hardwarescan',
+  templateUrl: './page-hardwarescan.component.html',
+  styleUrls: ['./page-hardwarescan.component.scss']
+})
+export class PageHardwarescanComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
