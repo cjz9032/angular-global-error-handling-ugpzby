8515fc649529132f1eefa547c74e157baf48fc23
Santosh udnur <sudnur@lenovo.com>
2019-05-10 17:38:14 +0530

    fix for van-2618, van-3921

--- vtr-ui/src/app/components/pages/page-device/page-device.component.ts
+++ vtr-ui/src/app/components/pages/page-device/page-device.component.ts
@@ -27,13 +27,6 @@ export class PageDeviceComponent implements OnInit {
 		qaService.qas.forEach(qa => {
 			try {
 				qa.title = this.translate.instant(qa.title);
-				qa.description = this.translate.instant(qa.description);
-				//console.log(qa.description);
-				this.translate.get(qa.keys).subscribe((translation: [string]) => {
-					//console.log(JSON.stringify(translation));
-					qa.keys = translation;
-					//console.log(JSON.stringify(qa.keys));
-				});
 			}
 			catch (e) {
 				console.log("already translated");
