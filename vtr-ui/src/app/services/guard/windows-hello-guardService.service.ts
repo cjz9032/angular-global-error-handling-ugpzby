4db1b1a0e75436296ecd842047844af4164e27da
Kidd Liu <kiddliu@lenovo.com>
2019-05-13 20:17:27 +0800

    VAN-3452 hide windows hello page if IMC is unavailable

--- vtr-ui/src/app/services/guard/windows-hello-guardService.service.ts
+++ vtr-ui/src/app/services/guard/windows-hello-guardService.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { CanActivate } from '@angular/router';
 import { CommonService } from '../common/common.service';
 import { VantageShellService } from '../vantage-shell/vantage-shell.service';
+import { LocalStorageKey } from 'src/app/enums/local-storage-key.enum';
 
 @Injectable({
 	providedIn: 'root',
@@ -11,8 +12,10 @@ export class WindowsHelloGuardService implements CanActivate {
 
 	canActivate(): boolean {
 		const windowsHello = this.vantageShellService.getSecurityAdvisor().windowsHello;
+		const showWhPage = this.commonService.getLocalStorageValue(LocalStorageKey.SecurityShowWindowsHello);
 		return this.commonService.isRS5OrLater()
 			&& (typeof windowsHello.facialIdStatus === 'string'
-				|| typeof windowsHello.fingerPrintStatus === 'string');
+				|| typeof windowsHello.fingerPrintStatus === 'string' || showWhPage);
+
 	}
 }
