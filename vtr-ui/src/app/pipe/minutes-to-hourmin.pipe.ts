d5881b6ea23ff7b1c4087aa8d3e7a34f7baa5a71
Santosh udnur <sudnur@lenovo.com>
2019-05-17 18:38:14 +0530

    fix for van-2791,VAN-2788

--- vtr-ui/src/app/pipe/minutes-to-hourmin.pipe.ts
+++ vtr-ui/src/app/pipe/minutes-to-hourmin.pipe.ts
@@ -1,26 +1,44 @@
 import { Pipe, PipeTransform } from '@angular/core';
+import { TranslateService } from '@ngx-translate/core';
 
 @Pipe({
-  name: 'minutesToHourmin'
+	name: 'minutesToHourmin'
 })
 export class MinutesToHourminPipe implements PipeTransform {
 
-  transform(value: any, args?: any): any {
-    const totalMin = value;
-    const hours = Math.trunc(totalMin/60);
-    const minutes = Math.trunc(totalMin%60);
-    if(value == undefined) {
-      return undefined;
-    }
-    if (Number.isNaN(minutes)) {
-      return "0 minutes";
-    }
+	constructor(private translate: TranslateService) {
+	}
+
+	transform(value: any, args?: any): any {
+		const totalMin = value;
+		const hours = Math.trunc(totalMin / 60);
+		const minutes = Math.trunc(totalMin % 60);
+		if (value === undefined) {
+			return undefined;
+		}
+		/* if (Number.isNaN(minutes)) {
+			return "0 minutes";
+		}
+
 		const hoursStr = hours > 0 && hours < 2 ? 'hour' : 'hours';
-		const minutesStr = minutes > 0 && minutes < 2 ? 'minute': 'minutes';
-		if(hours == 0) {
+		const minutesStr = minutes > 0 && minutes < 2 ? 'minute' : 'minutes'; */
+		if (Number.isNaN(minutes)) {
+			return '0 ' + this.translate.instant('device.deviceSettings.batteryGauge.minutes');
+		}
+
+		const hoursStr = hours > 0 && hours < 2 ?
+			this.translate.instant('device.deviceSettings.batteryGauge.hour') :
+			this.translate.instant('device.deviceSettings.batteryGauge.hours');
+
+		const minutesStr = minutes > 0 && minutes < 2 ?
+			this.translate.instant('device.deviceSettings.batteryGauge.minute') :
+			this.translate.instant('device.deviceSettings.batteryGauge.minutes');
+
+
+		if (hours === 0) {
 			return `${minutes} ${minutesStr}`;
 		}
 		return `${hours} ${hoursStr} ${minutes} ${minutesStr}`;
-  }
+	}
 
 }
