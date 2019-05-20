e1c4cf3c0dc93a4990f0767465c8d87cd6af16ff
sjain8 <sjain8@lenovo.com>
2019-05-20 19:24:11 +0530

    VAN-3462 added model and local storage key

--- vtr-ui/src/app/components/ui/ui-gaming-collapsible-container/ui-gaming-collapsible-container.component.ts
+++ vtr-ui/src/app/components/ui/ui-gaming-collapsible-container/ui-gaming-collapsible-container.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit, Input } from '@angular/core';
 import * as $ from 'jquery';
+import { VantageShellService } from 'src/app/services/vantage-shell/vantage-shell.service';
+import { CommonService } from 'src/app/services/common/common.service';
+import { LocalStorageKey } from 'src/app/enums/local-storage-key.enum';
 
 @Component({
   selector: 'vtr-ui-gaming-collapsible-container',
@@ -14,6 +17,21 @@ export class UiGamingCollapsibleContainerComponent implements OnInit {
 	public currentOption: string;
 	public currentDescription: string;
 
+	constructor(
+		shellService: VantageShellService,
+		commonSercice: CommonService
+	) {
+		this.currentOption = commonSercice.getLocalStorageValue(LocalStorageKey.CpuOCStatus);
+		this.currentOption = shellService.getCPUOverClockStatus();
+	}
+
+	ngOnInit() {
+		this.options.forEach(option => {
+			this.currentOption = option.defaultOption ? option.name : this.currentOption;
+			this.currentDescription = option.defaultOption ? option.description : this.currentOption;
+		});
+	}
+
 	public toggleOptions() {
 		this.showOptions = !this.showOptions;
 
@@ -24,23 +42,14 @@ export class UiGamingCollapsibleContainerComponent implements OnInit {
 			this.buttonName = 'Show';
 		}
 	}
-	constructor() { }
-
-	ngOnInit() {
-	this.options.forEach(option => {
-		this.currentOption = option.defaultOption ? option.name : this.currentOption;
-		this.currentDescription = option.defaultOption ? option.description : this.currentOption;
-	});
-	}
-
 
 	public optionSelected(option) {
-	this.currentOption = option.name;
-	this.showOptions = false;
-}
+		this.currentOption = option.name;
+		this.showOptions = false;
+	}
 
-public showDescription(option) {
-	this.currentDescription = option.description;
-}
+	public showDescription(option) {
+		this.currentDescription = option.description;
+	}
 
 }
