18416a54fba2896442f36ffc72bdb483f8e78b1e
zhara <zhara@zeoalliance.com>
2019-05-22 01:36:07 +0300

    clean code

--- vtr-ui/src/app/components/pages/page-privacy/pages/articles/article-preview/article-preview.component.ts
+++ vtr-ui/src/app/components/pages/page-privacy/pages/articles/article-preview/article-preview.component.ts
@@ -34,78 +34,28 @@ export class ArticlePreviewComponent implements OnInit, AfterViewInit {
 	}
 
 	ngAfterViewInit() {
-		const myText = this.article.title.split(' ');
-		const el = this.articleTitle.nativeElement;
-
+		const textToAdd = this.article.title.split(' ');
+		const cutHtmlElement = this.articleTitle.nativeElement;
 
 		let lineHeight = 0;
 		let currHeight = 0;
 		let lastAddedWordIndex = 0;
-		myText.forEach((word, index) => {
-			if (currHeight <= lineHeight * 2) {
-				el.innerHTML += `${word} `;
-				currHeight = el.offsetHeight;
+		const allowedLines = 2;
+		textToAdd.forEach((word, index) => {
+			if (currHeight <= lineHeight * allowedLines) {
+				cutHtmlElement.innerHTML += `${word} `;
+				currHeight = cutHtmlElement.offsetHeight;
 				if (index === 0) {
 					lineHeight = currHeight;
 				}
 				lastAddedWordIndex = index;
 			}
 		});
-		const hasMoreWords = lastAddedWordIndex <= myText.length - 1;
-		const higherThenTwoLines = currHeight > lineHeight * 2;
-		if (hasMoreWords && higherThenTwoLines) {
-			el.innerHTML = el.innerHTML.replace(` ${myText[lastAddedWordIndex]}`, '...');
+		const hasMoreWords = lastAddedWordIndex <= textToAdd.length - 1;
+		const higherThenAllowed = currHeight > lineHeight * allowedLines;
+		if (hasMoreWords && higherThenAllowed) {
+			cutHtmlElement.innerHTML = cutHtmlElement.innerHTML.replace(` ${textToAdd[lastAddedWordIndex]}`, '...');
 		}
-
-		// const n = 2;
-		// let lastHeight, hJumps = 0;
-		// myText.forEach((w, idx) => {
-		// 	const h = el.offsetHeight;
-		// 	console.log(h, lastHeight)
-		// 	if (h !== lastHeight && idx !== 0) {
-		// 		hJumps++;
-		// 	}
-		// 	if (hJumps < n) {
-		// 		el.innerHTML += `${w} `;
-		// 		lastHeight = h;
-		// 	} else {
-		// 		console.log('elWidth', el.offsetWidth);
-		// 		console.log('parentWidth', parentWidth);
-		// 	}
-		// });
-
-
-		// const hiddenEl = document.createElement('h2');
-		// hiddenEl.style.fontSize = '2rem';
-		// // const hiddenEl = el.cloneNode(false);
-		// console.log(hiddenEl.style);
-		// hiddenEl.innerHTML = el.innerHtml;
-		// // console.log('hiddenEl', hiddenEl);
-		// console.log('hiddenEl.offsetHeight', hiddenEl.offsetHeight);
-		// console.log('el.offsetHeight', el.offsetHeight);
-		//
-		// myText.forEach(word => {
-		// 	this.cuttedArticleTitle += `${word} `;
-		// 	// console.log('++++++++++++ el', el.offsetHeight);
-		// });
-		//
-		// let widthOfNewString = 0;
-		// const str = this.articleTitle.nativeElement.innerHTML.split(' ');
-		// const elementWidth = this.articleTitle.nativeElement.offsetWidth;
-		// const lineHeight = document.defaultView.getComputedStyle(el, null).getPropertyValue('lineHeight');
-		// // console.log('lineHeight', lineHeight);
-		// // const elementHeight = this.articleTitle.nativeElement.offsetHeight;
-		// // hiddenEl.classList.add('article-preview__title');
-		// // document.body.appendChild(hiddenEl);
-		// // var newStr = '';
-		// // str.forEach((letter) => {
-		// // 	// hiddenEl.innerText = letter + ' ';
-		// // 	widthOfNewString = hiddenEl.offsetWidth;
-		// // 	if (widthOfNewString < (elementWidth * 2)) {
-		// // 		hiddenEl.innerText += letter + ' ';
-		// // 		newStr += letter + ' ';
-		// // 	}
-		// // });
 	}
 
 	openSingleArticle(id) {
