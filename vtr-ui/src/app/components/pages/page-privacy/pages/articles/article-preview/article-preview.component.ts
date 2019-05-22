97b352e5eb50e4765b836ef9651c2584cb83a577
zhara <zhara@zeoalliance.com>
2019-05-22 22:14:22 +0300

    add vtrCutMultilineTextWithDots as directive

--- vtr-ui/src/app/components/pages/page-privacy/pages/articles/article-preview/article-preview.component.ts
+++ vtr-ui/src/app/components/pages/page-privacy/pages/articles/article-preview/article-preview.component.ts
@@ -1,27 +1,17 @@
-import {
-	Component,
-	ElementRef,
-	Input,
-	OnInit,
-	AfterViewInit,
-	ViewChild,
-	HostListener,
-} from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { Article } from '../articles.service';
 import { Router } from '@angular/router';
 import { CommonPopupService } from '../../../common/services/popups/common-popup.service';
 import { RouterChangeHandlerService } from '../../../common/services/router-change-handler.service';
 import { RoutersName } from '../../../privacy-routing-name';
 
-@HostListener('window:resize', ['$event'])
 @Component({
 	selector: 'vtr-article-preview',
 	templateUrl: './article-preview.component.html',
 	styleUrls: ['./article-preview.component.scss'],
 })
-export class ArticlePreviewComponent implements OnInit, AfterViewInit {
+export class ArticlePreviewComponent implements OnInit {
 	@Input() article: Article;
-	@ViewChild('articleTitle') articleTitle: ElementRef;
 
 	articlePopupId = 'articlePopupId';
 
@@ -33,43 +23,6 @@ export class ArticlePreviewComponent implements OnInit, AfterViewInit {
 	}
 
 	ngOnInit() {
-	}
-
-	ngAfterViewInit() {
-		this.addCutText();
-	}
-
-	onResizeFn() {
-		this.addCutText();
-	}
-
-	addCutText() {
-		const textToAdd = this.article.title.split(' ');
-		const cutHtmlElement = this.articleTitle.nativeElement;
-		cutHtmlElement.innerHTML = '';
-
-		let allowedHeight = 0;
-		let currHeight = 0;
-		let lastAddedWordIndex = 0;
-		const allowedLines = 2;
-		textToAdd.forEach((word, index) => {
-			if (currHeight <= allowedHeight) {
-				cutHtmlElement.innerHTML += `${word} `;
-				currHeight = cutHtmlElement.offsetHeight;
-				if (index === 0) {
-					allowedHeight = currHeight * allowedLines + (allowedLines / 2); // (allowedLines / 2) - for case if browser will round height
-				}
-				lastAddedWordIndex = index;
-			}
-		});
-		const hasMoreWords = lastAddedWordIndex <= textToAdd.length - 1;
-		if (hasMoreWords && currHeight > allowedHeight) {
-			cutHtmlElement.innerHTML = cutHtmlElement.innerHTML.replace(` ${textToAdd[lastAddedWordIndex]}`, '...');
-			currHeight = cutHtmlElement.offsetHeight;
-		}
-		if (currHeight > allowedHeight) { // if last word moved to next line after adding '...'
-			cutHtmlElement.innerHTML = cutHtmlElement.innerHTML.replace(` ${textToAdd[lastAddedWordIndex - 1]}...`, '...');
-		}
 	}
 
 	openSingleArticle(id) {
