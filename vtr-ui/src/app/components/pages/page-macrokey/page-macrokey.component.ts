5e08b4eb4a2a1ce5a8d09e7e9f1b0c25580f24fb
sjain8 <sjain8@lenovo.com>
2019-05-29 13:20:53 +0530

    VAN-678 Added macro key page containers

--- vtr-ui/src/app/components/pages/page-macrokey/page-macrokey.component.ts
+++ vtr-ui/src/app/components/pages/page-macrokey/page-macrokey.component.ts
@@ -1,15 +1,45 @@
 import { Component, OnInit } from '@angular/core';
+import { CMSService } from 'src/app/services/cms/cms.service';
 
 @Component({
-  selector: 'vtr-page-macrokey',
-  templateUrl: './page-macrokey.component.html',
-  styleUrls: ['./page-macrokey.component.scss']
+	selector: 'vtr-page-macrokey',
+	templateUrl: './page-macrokey.component.html',
+	styleUrls: [ './page-macrokey.component.scss' ]
 })
 export class PageMacrokeyComponent implements OnInit {
 
-  constructor() { }
+	cardContentPositionA: any = {};
+	cardContentPositionB: any = {};
 
-  ngOnInit() {
-  }
+	constructor(
+		private cmsService: CMSService,
+	) {}
 
+	ngOnInit() {
+		// TODO: Change the query params for macrokey subpage
+		const queryOptions = {
+			'Page': 'dashboard',
+			'Lang': 'EN',
+			'GEO': 'US',
+			'OEM': 'Lenovo',
+			'OS': 'Windows',
+			'Segment': 'SMB',
+			'Brand': 'Lenovo'
+		};
+
+		this.cmsService.fetchCMSContent(queryOptions).then((response: any) => {
+			const cardContentPositionA = this.cmsService.getOneCMSContent(response, 'half-width-top-image-title-link', 'position-F')[0];
+			if (cardContentPositionA) {
+				this.cardContentPositionA = cardContentPositionA;
+			}
+
+			const cardContentPositionB = this.cmsService.getOneCMSContent(response, 'half-width-title-description-link-image', 'position-B')[0];
+			if (cardContentPositionB) {
+				this.cardContentPositionB = cardContentPositionB;
+				if (this.cardContentPositionB.BrandName) {
+					this.cardContentPositionB.BrandName = this.cardContentPositionB.BrandName.split('|')[0];
+				}
+			}
+		});
+	}
 }
