9462c3f6a7057b75dfad12e8061a6ab9da6d2296
Koch <tkoch1@lenovo.com>
2019-05-31 19:43:39 +0530

    Added sidebar cms content

--- vtr-ui/src/app/components/pages/page-lightingcustomize/page-lightingcustomize.component.ts
+++ vtr-ui/src/app/components/pages/page-lightingcustomize/page-lightingcustomize.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { CMSService } from 'src/app/services/cms/cms.service';
+
 
 @Component({
   selector: 'vtr-page-lightingcustomize',
@@ -6,10 +8,37 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./page-lightingcustomize.component.scss']
 })
 export class PageLightingcustomizeComponent implements OnInit {
+  [x: string]: any;
 
-  constructor() { }
+  constructor(
+    private cmsService: CMSService
+  ) { }
 
   ngOnInit() {
+    const queryOptions = {
+			'Page': 'dashboard',
+			'Lang': 'EN',
+			'GEO': 'US',
+			'OEM': 'Lenovo',
+			'OS': 'Windows',
+			'Segment': 'SMB',
+			'Brand': 'Lenovo'
+		};
+
+		this.cmsService.fetchCMSContent(queryOptions).then((response: any) => {
+			const cardContentPositionA = this.cmsService.getOneCMSContent(response, 'half-width-top-image-title-link', 'position-F')[0];
+			if (cardContentPositionA) {
+				this.cardContentPositionA = cardContentPositionA;
+			}
+
+			const cardContentPositionB = this.cmsService.getOneCMSContent(response, 'half-width-title-description-link-image', 'position-B')[0];
+			if (cardContentPositionB) {
+				this.cardContentPositionB = cardContentPositionB;
+				if (this.cardContentPositionB.BrandName) {
+					this.cardContentPositionB.BrandName = this.cardContentPositionB.BrandName.split('|')[0];
+				}
+			}
+		});
   }
 
 }
