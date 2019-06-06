e38c124c1a9fee84ee89db09f06492b14c319f33
Kidd Liu <kiddliu@lenovo.com>
2019-06-06 13:05:45 +0800

    add npm script to remove all console log call

---
+++ vtr-ui/prebuild/no-console.js
@@ -0,0 +1,33 @@
+const { exec } = require('child_process');
+const fs = require('fs');
+const path = require('path');
+
+function parseError(error) {
+  const result = [];
+  const regex = new RegExp('(\\S+)\\[(\\d+), (\\d+)\\]');
+  error.split('\n').forEach((line) => {
+    const r = regex.exec(line);
+    if (Array.isArray(r) && r.length > 0) {
+	  if (result.length == 0 || r[1] != result[result.length - 1]) {
+        result.push(r[1]);
+	  }
+    }
+  });
+  return result;
+}
+
+function removeConsoleLine(file) {
+  fs.writeFileSync(file,
+    fs.readFileSync(file, 'utf-8')
+		  .split('\r\n')
+		  .filter(line => !line.includes('console.'))
+		  .join('\r\n'),
+    'utf-8');
+}
+
+exec('tslint --config prebuild/no-console.json --project .', {
+  cwd: path.resolve(__dirname, '..'),
+}, (error, stdout, stderr) => {
+  console.log(stdout);
+  parseError(stdout).forEach(file => removeConsoleLine(file));
+});
