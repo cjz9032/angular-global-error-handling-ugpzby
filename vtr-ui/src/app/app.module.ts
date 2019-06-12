1df8efba17f59b4f1bf158772c9dd824c2ac14de
vishnucr <vishnucr@ymail.com>
2019-06-12 16:37:12 +0530

    new branch creted 3370

--- vtr-ui/src/app/app.module.ts
+++ vtr-ui/src/app/app.module.ts
@@ -218,6 +218,7 @@ import { UiApsSliderComponent } from './components/ui/ui-aps-slider/ui-aps-slide
 import { UiCheckboxComponent } from './components/ui/ui-checkbox/ui-checkbox.component';
 import { UiCircleRadioWithCheckboxComponent } from './components/ui/ui-circle-radio-with-checkbox/ui-circle-radio-with-checkbox.component';
 import { UiChsStatusbarComponent } from './components/ui/ui-chs-statusbar/ui-chs-statusbar.component';
+import { ActiveProtectionSystemAdvancedComponent } from './components/pages/page-smart-assist/active-protection-system-advanced/active-protection-system-advanced.component';
 
 library.add(fas);
 library.add(fab);
@@ -397,7 +398,8 @@ library.add(fal);
 		UiApsSliderComponent,
 		UiCheckboxComponent,
 		UiCircleRadioWithCheckboxComponent,
-		UiChsStatusbarComponent
+		UiChsStatusbarComponent,
+		ActiveProtectionSystemAdvancedComponent
 	],
 	imports: [
 		BrowserModule,
---
+++ vtr-ui/src/app/components/pages/page-smart-assist/active-protection-system-advanced/active-protection-system-advanced.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'vtr-active-protection-system-advanced',
+  templateUrl: './active-protection-system-advanced.component.html',
+  styleUrls: ['./active-protection-system-advanced.component.scss']
+})
+export class ActiveProtectionSystemAdvancedComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
