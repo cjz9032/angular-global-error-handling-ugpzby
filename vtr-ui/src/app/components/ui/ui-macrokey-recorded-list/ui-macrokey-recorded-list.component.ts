ea0589fc3e9bb6771db1cba236aabac6020cc2bd
sjain8 <sjain8@lenovo.com>
2019-06-14 17:01:39 +0530

    VAN-1838 Finished pop up integrations

--- vtr-ui/src/app/components/ui/ui-macrokey-recorded-list/ui-macrokey-recorded-list.component.ts
+++ vtr-ui/src/app/components/ui/ui-macrokey-recorded-list/ui-macrokey-recorded-list.component.ts
@@ -158,7 +158,7 @@ export class UiMacrokeyRecordedListComponent implements OnInit, OnChanges, DoChe
 	}
 
 	ngDoCheck() {
-		if (this.recordsList !== this.recordsData.inputs) {
+		if (!isUndefined(this.recordsData) && this.recordsList !== this.recordsData.inputs) {
 			this.recordsList = this.recordsData.inputs;
 		}
 	}
