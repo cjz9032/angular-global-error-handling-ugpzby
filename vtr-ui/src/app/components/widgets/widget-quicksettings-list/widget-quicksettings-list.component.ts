461db0dcc3f15938e8f21033b1637ae6486d8884
sdhanekula <sdhanekula@lenovo.com>
2019-06-19 15:51:12 +0530

    thermalmode event unregister implemented

--- vtr-ui/src/app/components/widgets/widget-quicksettings-list/widget-quicksettings-list.component.ts
+++ vtr-ui/src/app/components/widgets/widget-quicksettings-list/widget-quicksettings-list.component.ts
@@ -1,5 +1,5 @@
 import { AudioService } from 'src/app/services/audio/audio.service';
-import { Component, OnInit, Input, AfterViewInit } from '@angular/core';
+import { Component, OnInit, Input, AfterViewInit, OnDestroy } from '@angular/core';
 import { ThermalModeStatus } from 'src/app/data-models/gaming/thermal-mode-status.model';
 import { GamingThermalModeService } from 'src/app/services/gaming/gaming-thermal-mode/gaming-thermal-mode.service';
 import { CommonService } from 'src/app/services/common/common.service';
@@ -15,7 +15,8 @@ import { VantageShellService } from 'src/app/services/vantage-shell/vantage-shel
 	templateUrl: './widget-quicksettings-list.component.html',
 	styleUrls: ['./widget-quicksettings-list.component.scss']
 })
-export class WidgetQuicksettingsListComponent implements OnInit, AfterViewInit {
+export class WidgetQuicksettingsListComponent implements OnInit, AfterViewInit, OnDestroy {
+
 
 	@Input() title = '';
 
@@ -202,7 +203,9 @@ export class WidgetQuicksettingsListComponent implements OnInit, AfterViewInit {
 
 	ngAfterViewInit() {
 	}
-
+	public unRegisterThermalModeEvent() {
+		this.shellServices.unRegisterEvent(EventTypes.gamingThermalModeChangeEvent, this.onRegThermalModeEvent.bind(this));
+	}
 	public onRegThermalModeEvent(status: any) {
 		if (status !== undefined) {
 			const regThermalModeStatusObj = new ThermalModeStatus();
@@ -345,5 +348,8 @@ export class WidgetQuicksettingsListComponent implements OnInit, AfterViewInit {
 		const { available, status } = this.commonService.getLocalStorageValue(LocalStorageKey.DolbyModeCache);
 		this.quickSettings[3].isVisible = available;
 		this.quickSettings[3].isChecked = status;
+	}
+	ngOnDestroy(): void {
+		this.unRegisterThermalModeEvent();
 	}
 }
