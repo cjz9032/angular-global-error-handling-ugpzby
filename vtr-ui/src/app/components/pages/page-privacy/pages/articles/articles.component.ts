ad10a48f1cd63c373fa9be07dd9c48e5945625e3
Dmitriy Didyk <creo@zeoalliance.com>
2019-06-26 15:06:37 +0300

    privacy/creo/created-article-component

--- vtr-ui/src/app/components/pages/page-privacy/pages/articles/articles.component.ts
+++ vtr-ui/src/app/components/pages/page-privacy/pages/articles/articles.component.ts
@@ -1,6 +1,6 @@
-import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Component } from '@angular/core';
 import { ArticlesService } from './articles.service';
-import { ActivatedRoute, Router } from '@angular/router';
+import { Router } from '@angular/router';
 import { RoutersName } from '../../privacy-routing-name';
 
 @Component({
@@ -8,33 +8,15 @@ import { RoutersName } from '../../privacy-routing-name';
 	templateUrl: './articles.component.html',
 	styleUrls: ['./articles.component.scss']
 })
-export class ArticlesComponent implements OnInit, OnDestroy {
+export class ArticlesComponent {
 
 	articles = this.articlesService.articles;
 	articles$ = this.articlesService.getListOfArticles();
-	openArticleId = null;
-	recommendationsArticles = null;
 
 	constructor(
-		private activatedRoute: ActivatedRoute,
 		private articlesService: ArticlesService,
 		private router: Router
 	) {
-	}
-
-	ngOnInit() {
-
-		const getFilteredArticlesByCategory = (category) => {
-			return this.articlesService.filterArticlesByCategory(category).filter((article) => article.id !== this.openArticleId);
-		};
-		this.activatedRoute.queryParams.subscribe((val) => {
-			this.openArticleId = val.id || null;
-			const openArticle = this.articles[this.openArticleId];
-			this.recommendationsArticles = openArticle ? getFilteredArticlesByCategory(openArticle.category) : null;
-		});
-	}
-
-	ngOnDestroy() {
 	}
 
 	openArticle(articleId: string) {
