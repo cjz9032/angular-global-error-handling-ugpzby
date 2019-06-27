a10a73a8cba0e3a231dc623ae81c4f09635cfd4d
Dmitriy Didyk <creo@zeoalliance.com>
2019-06-27 10:53:24 +0300

    privacy/creo/LVPT-403/added popup articles

--- vtr-ui/src/app/components/pages/page-privacy/pages/articles/articles.service.ts
+++ vtr-ui/src/app/components/pages/page-privacy/pages/articles/articles.service.ts
@@ -1,18 +1,9 @@
 import { Injectable } from '@angular/core';
-import * as page1 from './pages/article1a.html';
-import * as page2 from './pages/article1b.html';
-import * as page3 from './pages/article1c.html';
-import * as page4 from './pages/article2a.html';
-import * as page5 from './pages/article2b.html';
-import * as page6 from './pages/article3a.html';
-import * as page7 from './pages/article3b.html';
 import { RoutersName } from '../../privacy-routing-name';
 import { UserDataGetStateService } from '../../common/services/user-data-get-state.service';
-import { HttpClient } from '@angular/common/http';
 import { CommsService } from '../../../../../services/comms/comms.service';
-import { map, shareReplay, tap } from 'rxjs/operators';
+import { map, shareReplay } from 'rxjs/operators';
 import { Observable } from 'rxjs';
-import { TrackersInfo } from '../../feature/tracking-map/services/tracking-map.interface';
 
 export interface Article {
 	id: string;
@@ -73,17 +64,7 @@ export class ArticlesService {
 
 	getListOfArticles() {
 		if (!this.articlesCache$) {
-			const queryOptions = {
-				'Page': 'privacy',
-				'Lang': 'en',
-				'GEO': 'US',
-				'OEM': 'Lenovo',
-				'OS': 'Windows',
-				'Segment': 'SMB',
-				'Brand': 'idea',
-			};
-
-			this.articlesCache$ = this.commsService.endpointGetCall('/api/v1/features', queryOptions).pipe(
+			this.articlesCache$ = this.commsService.endpointGetCall('/api/v1/features', this.getOptions()).pipe(
 				map((response) => response['Results']),
 				shareReplay(1)
 			);
@@ -94,7 +75,13 @@ export class ArticlesService {
 	}
 
 	getArticle(id) {
-		const queryOptions = {
+		return this.commsService.endpointGetCall(`/api/v1/articles/${id}`, this.getOptions()).pipe(
+			map((response) => response['Results'])
+		);
+	}
+
+	private getOptions() {
+		return {
 			'Page': 'privacy',
 			'Lang': 'en',
 			'GEO': 'US',
@@ -103,9 +90,5 @@ export class ArticlesService {
 			'Segment': 'SMB',
 			'Brand': 'idea',
 		};
-
-		return this.commsService.endpointGetCall(`/api/v1/articles/${id}`, queryOptions).pipe(
-			map((response) => response['Results'])
-		);
 	}
 }
