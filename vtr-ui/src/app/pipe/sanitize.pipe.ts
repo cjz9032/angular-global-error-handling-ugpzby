e58c8967189af16f670c6d9c768418590c9dadcd
NancyDeng <nancydeng@lenovo.com>
2019-07-04 09:31:37 +0800

    Stash for readmore

--- vtr-ui/src/app/pipe/sanitize.pipe.ts
+++ vtr-ui/src/app/pipe/sanitize.pipe.ts
@@ -1,5 +1,5 @@
 import { Pipe, PipeTransform, SecurityContext } from '@angular/core';
-import { DomSanitizer, SafeHtml } from '@angular/platform-browser'; //sahinul-25Jun2019 for VAN-5719
+import { DomSanitizer, SafeHtml, SafeStyle, SafeScript, SafeUrl, SafeResourceUrl } from '@angular/platform-browser';
 
 @Pipe({
 	name: 'sanitize'
@@ -8,24 +8,26 @@ export class SanitizePipe implements PipeTransform {
 
 	constructor(protected sanitizer: DomSanitizer) { }
 
-	public transform(value: any, type: string): string | SafeHtml {
+	public transform(value: any, type: string): string | SafeHtml | SafeStyle | SafeScript | SafeUrl | SafeResourceUrl {
 		switch (type) {
 			case 'html':
-				return this.sanitizer.sanitize(SecurityContext.HTML, value);
+				return this.sanitizer.bypassSecurityTrustHtml(
+					this.sanitizer.sanitize(SecurityContext.HTML, value)
+				);
 			case 'style':
-				return this.sanitizer.sanitize(SecurityContext.STYLE, value);
+				return this.sanitizer.bypassSecurityTrustStyle(
+					this.sanitizer.sanitize(SecurityContext.STYLE, value)
+				);
 			case 'script':
-				return this.sanitizer.sanitize(SecurityContext.SCRIPT, value);
+				return this.sanitizer.bypassSecurityTrustScript(value);
 			case 'url':
-				return this.sanitizer.sanitize(SecurityContext.URL, value);
+				return this.sanitizer.bypassSecurityTrustUrl(
+					this.sanitizer.sanitize(SecurityContext.URL, value)
+				);
 			case 'resourceUrl':
-				return this.sanitizer.sanitize(SecurityContext.RESOURCE_URL, value);
-			case 'bypasshtml':	
-				//sahinul-25Jun2019 for VAN-5719
-				return this.sanitizer.bypassSecurityTrustHtml(value);
+				return this.sanitizer.bypassSecurityTrustResourceUrl(value);
 			default:
 				throw new Error(`Invalid safe type specified: ${type}`);
 		}
 	}
 }
-
