d19a2f98d9afb0f8611705091cf7179fe6a9b7fc
Dmitriy Didyk <creo@zeoalliance.com>
2019-07-23 12:36:46 +0300

    privacy/creo/change environment

--- vtr-ui/src/app/components/pages/page-privacy/environment.ts
+++ vtr-ui/src/app/components/pages/page-privacy/environment.ts
@@ -1,7 +1,7 @@
 import { environment } from '../../../../environments/environment';
 
 const productionEnvironment = {
-	backendUrl: 'https://api.figleafapp.com'
+	backendUrl: 'https://api.sz.figleafapp.com'
 };
 
 const developEnvironment = {
