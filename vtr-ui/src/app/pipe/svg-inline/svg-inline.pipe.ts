c982ba791e65bbea688438c49c6ac70c942e2337
lixd19 <lixd19@lenovo.com>
2019-07-29 17:26:26 +0800

    fix infinate call  of svg-inline-pipe

--- vtr-ui/src/app/pipe/svg-inline/svg-inline.pipe.ts
+++ vtr-ui/src/app/pipe/svg-inline/svg-inline.pipe.ts
@@ -1,30 +1,17 @@
 import { Pipe, PipeTransform, OnDestroy } from '@angular/core';
 import { DomSanitizer } from '@angular/platform-browser';
 import { Observable, Subscription } from 'rxjs';
-import { AppNotification } from 'src/app/data-models/common/app-notification.model';
-import { NetworkStatus } from 'src/app/enums/network-status.enum';
 import { CommonService } from 'src/app/services/common/common.service';
 
 @Pipe({
 	name: 'svgInline',
-	pure: false
 })
 export class SvgInlinePipe implements PipeTransform, OnDestroy {
 
-	isOnline = true;
-	notificationSubscription: Subscription;
-
 	constructor(private sanitizer: DomSanitizer, private commonService: CommonService) {
-		this.isOnline = this.commonService.isOnline;
-		this.notificationSubscription = this.commonService.notification.subscribe((notification: AppNotification) => {
-			this.onNotification(notification);
-		});
 	}
 
 	ngOnDestroy(): void {
-		if (this.notificationSubscription) {
-			this.notificationSubscription.unsubscribe();
-		}
 	}
 
 	getContent(url) {
@@ -40,24 +27,11 @@ export class SvgInlinePipe implements PipeTransform, OnDestroy {
 		});
 	}
 
-	private onNotification(notification: AppNotification) {
-		if (notification) {
-			switch (notification.type) {
-				case NetworkStatus.Online:
-				case NetworkStatus.Offline:
-					this.isOnline = notification.payload.isOnline;
-					break;
-				default:
-					break;
-			}
-		}
-	}
-
 	transform(value: any, args?: any): any {
 		if (typeof (value) !== 'undefined') {
 			return new Observable(observer => {
 				observer.next('');
-				if (value.substring(value.lastIndexOf('.')) === '.svg' && !this.isOnline) {
+				if (value.substring(value.lastIndexOf('.')) === '.svg' && !this.commonService.isOnline) {
 					this.getContent(value).then(val => {
 						val = `data:image/svg+xml;base64,${btoa(val + '')}`;
 						val = this.sanitizer.bypassSecurityTrustResourceUrl(val + '');
