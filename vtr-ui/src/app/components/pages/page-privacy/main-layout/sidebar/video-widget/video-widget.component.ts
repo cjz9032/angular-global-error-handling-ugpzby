e3db10a80cd5d6a4ab1063154d80f1378800df5c
Dmitriy Didyk <creo@zeoalliance.com>
2019-07-29 10:15:04 +0300

    privacy/creo/merge develop

---
+++ vtr-ui/src/app/components/pages/page-privacy/main-layout/sidebar/video-widget/video-widget.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+	selector: 'vtr-video-widget',
+	templateUrl: './video-widget.component.html',
+	styleUrls: ['./video-widget.component.scss']
+})
+export class VideoWidgetComponent implements OnInit {
+
+	constructor() {
+	}
+
+	ngOnInit() {
+	}
+
+}
