a43d2ba0015e7ce4ab6080b278bce4e825fa7956
Raviraj Walke <rwalke@lenovo.com>
2019-07-31 12:45:29 +0530

    fixed issues in powrmanager driver missing case

--- vtr-ui/src/app/components/battery/battery-card/battery-card.component.ts
+++ vtr-ui/src/app/components/battery/battery-card/battery-card.component.ts
@@ -15,7 +15,7 @@ import { LocalStorageKey } from 'src/app/enums/local-storage-key.enum';
 import { PowerService } from 'src/app/services/power/power.service';
 import { Subscription } from 'rxjs/internal/Subscription';
 import { AppNotification } from 'src/app/data-models/common/app-notification.model';
-import { ActivatedRoute, ParamMap} from '@angular/router';
+import { ActivatedRoute, ParamMap } from '@angular/router';
 
 @Component({
 	selector: 'vtr-battery-card',
@@ -24,7 +24,7 @@ import { ActivatedRoute, ParamMap} from '@angular/router';
 })
 export class BatteryCardComponent implements OnInit, OnDestroy {
 
-	@ViewChild('batteryDetail', {static:false}) batteryModal: ElementRef<HTMLElement>;
+	@ViewChild('batteryDetail', { static: false }) batteryModal: ElementRef<HTMLElement>;
 	batteryInfo: BatteryDetail[];
 	batteryGauge: BatteryGaugeDetail;
 	batteryCardTimer: any;
@@ -64,7 +64,7 @@ export class BatteryCardComponent implements OnInit, OnDestroy {
 
 	ngOnInit() {
 		this.isLoading = true;
-		this.activatedRoute.queryParamMap.subscribe((params: ParamMap)=>{
+		this.activatedRoute.queryParamMap.subscribe((params: ParamMap) => {
 			console.log(params);
 			if (params.has('batterydetail') && this.isModalShown) {
 				this.isModalShown = false;
@@ -139,7 +139,6 @@ export class BatteryCardComponent implements OnInit, OnDestroy {
 				this.batteryInfo = response;
 				this.batteryInfo = response.batteryInformation;
 				this.batteryGauge = response.batteryIndicatorInfo;
-				this.initBatteryInformation();
 				this.updateBatteryDetails();
 
 				let showBatteryDetail = this.activatedRoute.snapshot.queryParams.batterydetail;
@@ -170,13 +169,16 @@ export class BatteryCardComponent implements OnInit, OnDestroy {
 		this.batteryGauge.isExpressCharging = this.batteryGauge.isExpressCharging || false;
 		this.batteryGauge.percentage = this.batteryGauge.percentage || 0;
 		this.batteryInfo[0].batteryHealth = this.batteryInfo[0].batteryHealth || 0;
-		this.batteryInfo[0].batteryCondition = this.batteryInfo[0].batteryCondition || ['Normal'];
+		if (!(this.batteryInfo[0].batteryCondition && this.batteryInfo[0].batteryCondition.length > 0)) {
+			this.batteryInfo[0].batteryCondition = ['Normal'];
+		}
 		this.batteryInfo[0].fullChargeCapacity = this.batteryInfo[0].fullChargeCapacity || 0;
 		this.batteryInfo[0].designCapacity = this.batteryInfo[0].designCapacity || 0;
 	}
 
 	public updateBatteryDetails() {
 		if (this.batteryInfo && this.batteryInfo.length > 0) {
+			this.initBatteryInformation();
 			const remainingPercentages = [];
 			this.batteryInfo.forEach((info) => {
 				remainingPercentages.push(info.remainingPercent);
@@ -185,6 +187,8 @@ export class BatteryCardComponent implements OnInit, OnDestroy {
 			this.sendThresholdWarning();
 			this.batteryHealth = this.batteryInfo[0].batteryHealth;
 			this.batteryIndicator.batteryNotDetected = this.batteryHealth === 4;
+		} else {
+			this.batteryIndicator.batteryNotDetected = false;
 		}
 
 		this.isBatteryDetailsBtnDisabled = this.batteryGauge.isPowerDriverMissing;
