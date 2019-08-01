33f74b75001fa8c07a43c8b21f095fec9232b1a6
raj <rsahu2@lenovo.com>
2019-08-01 19:52:47 +0530

    updated CMS url to QA CMS

--- vtr-ui/src/environments/environment.prod.ts
+++ vtr-ui/src/environments/environment.prod.ts
@@ -2,6 +2,6 @@ import packageFile from '../../package.json';
 
 export const environment = {
 	production: true,
-	cmsApiRoot: 'https://vantage.csw.lenovo.com',
+	cmsApiRoot: 'https://vantage-qa.csw.lenovo.com',
 	appVersion: packageFile.version
 };
