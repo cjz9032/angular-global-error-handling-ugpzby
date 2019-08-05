ab443743a06bb6d9f5d67f2488279cf05c0a8440
sdhanekula <sdhanekula@lenovo.com>
2019-08-05 13:36:35 +0530

    added the network boost components

---
+++ vtr-ui/src/app/components/pages/page-networkboost/page-networkboost.component.spec.ts
@@ -0,0 +1,25 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { PageNetworkboostComponent } from './page-networkboost.component';
+
+describe('PageNetworkboostComponent', () => {
+  let component: PageNetworkboostComponent;
+  let fixture: ComponentFixture<PageNetworkboostComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ PageNetworkboostComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(PageNetworkboostComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
