84663e07eedb650ddb64f12b26108c5fd643dd29
Gian Zheng <zhengxp3@lenovo.com>
2019-08-08 17:16:07 +0800

    VAN-7626 add translation of CHS notification

--- vtr-ui/src/app/data-models/home-security/home-security-notification-item.model.ts
+++ vtr-ui/src/app/data-models/home-security/home-security-notification-item.model.ts
@@ -1,3 +1,5 @@
+import { TranslateService } from '@ngx-translate/core';
+
 export class NotificationItem {
 	iconName: string;
 	color: string;
@@ -6,11 +8,14 @@ export class NotificationItem {
 	date: string;
 	path = 'home-security';
 
-	constructor(arg: any = {}) {
+	constructor(arg: any = {}, private translateService: TranslateService) {
 		this.iconName = arg.iconName;
 		this.color = arg.color;
 		this.title = arg.title;
 		this.subText = arg.notificationDetail;
 		this.date = arg.date;
+		this.translateService.stream(this.title).subscribe((result: string) => {
+			this.title = result;
+		});
 	}
 }
