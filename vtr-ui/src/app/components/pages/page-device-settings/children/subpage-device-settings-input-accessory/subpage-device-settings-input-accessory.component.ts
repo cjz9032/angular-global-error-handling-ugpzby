e6c10ccfb26c7a40e3ffc3302653debc38aeb7a6
raj <rsahu2@lenovo.com>
2019-08-23 12:52:14 +0530

    VAN-6927 integration code completed

--- vtr-ui/src/app/components/pages/page-device-settings/children/subpage-device-settings-input-accessory/subpage-device-settings-input-accessory.component.ts
+++ vtr-ui/src/app/components/pages/page-device-settings/children/subpage-device-settings-input-accessory/subpage-device-settings-input-accessory.component.ts
@@ -4,6 +4,7 @@ import { CommonService } from 'src/app/services/common/common.service';
 import { LocalStorageKey } from 'src/app/enums/local-storage-key.enum';
 import { InputAccessoriesCapability } from 'src/app/data-models/input-accessories/input-accessories-capability.model';
 import WinRT from '@lenovo/tan-client-bridge/src/util/winrt';
+import { LoggerService } from 'src/app/services/logger/logger.service';
 
 @Component({
 	selector: 'vtr-subpage-device-settings-input-accessory',
@@ -25,8 +26,14 @@ export class SubpageDeviceSettingsInputAccessoryComponent implements OnInit {
 	public keyboardCompatibility: boolean;
 	public switchValue = false;
 	public stickyFunStatus = false;
+	public isTouchPadVisible = false;
+	public isMouseVisible = false;
 
-	constructor(private keyboardService: InputAccessoriesService, private commonService: CommonService, ) { }
+	constructor(
+		private keyboardService: InputAccessoriesService,
+		private commonService: CommonService,
+		private logger: LoggerService
+	) { }
 
 	ngOnInit() {
 		this.machineType = this.commonService.getLocalStorageValue(LocalStorageKey.MachineType);
@@ -37,6 +44,7 @@ export class SubpageDeviceSettingsInputAccessoryComponent implements OnInit {
 				this.getKBDLayoutName();
 			}
 		}
+		this.getMouseAndTouchPadCapability();
 	}
 
 	// To get Keyboard Layout Name
@@ -201,9 +209,24 @@ export class SubpageDeviceSettingsInputAccessoryComponent implements OnInit {
 		this.switchValue = event.switchValue;
 	}
 
-	launchProtocol(protocol: string) {
+	private launchProtocol(protocol: string) {
 		if (this.keyboardService.isShellAvailable && protocol && protocol.length > 0) {
 			WinRT.launchUri(protocol);
+		}
+	}
+
+	private getMouseAndTouchPadCapability() {
+		if (this.keyboardService.isShellAvailable) {
+			Promise.all([
+				this.keyboardService.getMouseCapability(),
+				this.keyboardService.getTouchPadCapability()
+			]).then((responses: any[]) => {
+				this.logger.info('SubpageDeviceSettingsInputAccessoryComponent: getMouseAndTouchPadCapability.response', responses);
+				this.isMouseVisible = responses[0];
+				this.isTouchPadVisible = responses[1];
+			}).catch((error) => {
+				this.logger.error('SubpageDeviceSettingsInputAccessoryComponent: error in getMouseAndTouchPadCapability.Promise.all()', error);
+			});
 		}
 	}
 }
