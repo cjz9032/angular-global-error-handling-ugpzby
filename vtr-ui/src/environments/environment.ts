e88a52ee9e2c26d82d3a0df86d076def77e2a8e3
Jack Huang <huangbf1@lenovo.com>
2019-08-26 17:27:30 +0800

    VAN-8094 fix SSRB issue, commit to vantage3.0

--- vtr-ui/src/environments/environment.ts
+++ vtr-ui/src/environments/environment.ts
@@ -3,7 +3,7 @@ import packageFile from '../../package.json';
 export const environment = {
 	production: false,
 	cmsApiRoot: 'https://vantage.csw.lenovo.com',
-	upeApiRoot: 'https://api.uds-dev.lenovo.com',
+	upeApiRoot: 'https://api.uds-stage.lenovo.com',
 	appVersion: packageFile.version,
 	// Refer https://lnvusconf.lenovonet.lenovo.local/display/UPE3/Agent+id+for+App
 	upeClientID: '5d725ff1-8cbf-49a4-8f0f-61bc3d70e88c',
