f8c992645f4aaffe7e660a65aeb69f4fb787e0b7
sjain8 <sjain8@lenovo.com>
2019-08-28 18:06:34 +0530

    Added mock service

--- vtr-ui/src/app/components/widgets/widget-macrokey-settings/widget-macrokey-settings.component.spec.ts
+++ vtr-ui/src/app/components/widgets/widget-macrokey-settings/widget-macrokey-settings.component.spec.ts
@@ -1,15 +1,27 @@
+import { MacrokeyService } from './../../../services/gaming/macrokey/macrokey.service';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { WidgetMacrokeySettingsComponent } from './widget-macrokey-settings.component';
+import { Pipe, NO_ERRORS_SCHEMA } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
-xdescribe('WidgetMacrokeySettingsComponent', () => {
+const macrokeyServiceMock = jasmine.createSpyObj('MacrokeyService', [ 'isMacroKeyAvailable' ]);
+
+fdescribe('WidgetMacrokeySettingsComponent', () => {
 	let component: WidgetMacrokeySettingsComponent;
 	let fixture: ComponentFixture<WidgetMacrokeySettingsComponent>;
+	macrokeyServiceMock.isMacroKeyAvailable.and.returnValue(true);
 
 	beforeEach(
 		async(() => {
 			TestBed.configureTestingModule({
-				declarations: [WidgetMacrokeySettingsComponent]
+				declarations: [
+					WidgetMacrokeySettingsComponent,
+					mockPipe({ name: 'translate' }),
+					mockPipe({ name: 'sanitize' })
+				],
+				schemas: [ NO_ERRORS_SCHEMA ],
+				providers: [ { provide: HttpClient }, { provide: MacrokeyService, useValue: macrokeyServiceMock } ]
 			}).compileComponents();
 		})
 	);
@@ -24,3 +36,16 @@ xdescribe('WidgetMacrokeySettingsComponent', () => {
 		expect(component).toBeTruthy();
 	});
 });
+
+export function mockPipe(options: Pipe): Pipe {
+	const metadata: Pipe = {
+		name: options.name
+	};
+	return Pipe(metadata)(
+		class MockPipe {
+			public transform(query: string, ...args: any[]): any {
+				return query;
+			}
+		}
+	);
+}
