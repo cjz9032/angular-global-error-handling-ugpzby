ede24fdf8a05f83a63a01501f5fef9c4bfdf987d
sjain8 <sjain8@lenovo.com>
2019-08-28 20:13:11 +0530

    added mocks

--- vtr-ui/src/app/components/widgets/widget-macrokey-settings/widget-macrokey-settings.component.spec.ts
+++ vtr-ui/src/app/components/widgets/widget-macrokey-settings/widget-macrokey-settings.component.spec.ts
@@ -4,13 +4,30 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { WidgetMacrokeySettingsComponent } from './widget-macrokey-settings.component';
 import { Pipe, NO_ERRORS_SCHEMA } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { Router } from '@angular/router';
+import { GamingAllCapabilitiesService } from 'src/app/services/gaming/gaming-capabilities/gaming-all-capabilities.service';
 
-const macrokeyServiceMock = jasmine.createSpyObj('MacrokeyService', [ 'isMacroKeyAvailable' ]);
+const macrokeyServiceMock = jasmine.createSpyObj('MacrokeyService', [
+	'isMacroKeyAvailable',
+	'getMacrokeyTypeStatusCache',
+	'getMacrokeyRecordedStatusCache',
+	'getMacrokeyInputChangeCache',
+	'getMacrokeyInitialKeyDataCache',
+	'gamingMacroKeyInitializeEvent'
+]);
+
+const gamingAllCapabilitiesServiceMock = jasmine.createSpyObj('GamingAllCapabilitiesService', [
+	'gamingAllCapabilities',
+	'isShellAvailable',
+	'getCapabilityFromCache'
+]);
 
 fdescribe('WidgetMacrokeySettingsComponent', () => {
 	let component: WidgetMacrokeySettingsComponent;
 	let fixture: ComponentFixture<WidgetMacrokeySettingsComponent>;
 	macrokeyServiceMock.isMacroKeyAvailable.and.returnValue(true);
+	gamingAllCapabilitiesServiceMock.getCapabilityFromCache.and.returnValue(true);
+	macrokeyServiceMock.gamingMacroKeyInitializeEvent.and.returnValue(Promise.resolve(true));
 
 	beforeEach(
 		async(() => {
@@ -21,16 +38,18 @@ fdescribe('WidgetMacrokeySettingsComponent', () => {
 					mockPipe({ name: 'sanitize' })
 				],
 				schemas: [ NO_ERRORS_SCHEMA ],
-				providers: [ { provide: HttpClient }, { provide: MacrokeyService, useValue: macrokeyServiceMock } ]
+				providers: [
+					{ provide: HttpClient },
+					{ provide: Router },
+					{ provide: MacrokeyService, useValue: macrokeyServiceMock },
+					{ provide: GamingAllCapabilitiesService, useValue: gamingAllCapabilitiesServiceMock }
+				]
 			}).compileComponents();
+			fixture = TestBed.createComponent(WidgetMacrokeySettingsComponent);
+			component = fixture.componentInstance;
+			fixture.detectChanges();
 		})
 	);
-
-	beforeEach(() => {
-		fixture = TestBed.createComponent(WidgetMacrokeySettingsComponent);
-		component = fixture.componentInstance;
-		fixture.detectChanges();
-	});
 
 	it('should create', () => {
 		expect(component).toBeTruthy();
