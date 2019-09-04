be01739c978f9a816fe7a249df330b79d2729f56
Kidd Liu <kiddliu@lenovo.com>
2019-09-04 15:22:26 +0800

    remove invalid chars and make the final part lower case

--- vtr-ui/postbuild/nuget.js
+++ vtr-ui/postbuild/nuget.js
@@ -6,14 +6,14 @@ const file = path.resolve('./postbuild/Tangram.Client.Experience.nuspec');
 
 version = `${version}.${process.argv[2]}`;
 exec('git rev-parse --abbrev-ref HEAD', (error, stdout) => {
-	const branchName = stdout.trim();
+	const branchName = stdout.trim().toLowerCase();
 
 	fs.writeFileSync(file,
 		fs.readFileSync(file, 'utf-8')
 			.split('\r\n')
 			.map(line => {
 				if (line.includes('<id>')) {
-					return line.replace('</id>', `_(${branchName})</id>`);
+					return line.replace('</id>', `.${branchName}</id>`);
 				}
 				if (line.includes('<version>')) {
 					return line.replace(/>.+</, `>${version}<`);
