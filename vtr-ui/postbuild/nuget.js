0b7330f5ff07f0db6221c124ef807319055943b1
Kidd Liu <kiddliu@lenovo.com>
2019-09-04 14:26:54 +0800

    update postbuild script for nuget

--- vtr-ui/postbuild/nuget.js
+++ vtr-ui/postbuild/nuget.js
@@ -1,19 +1,25 @@
-let { version } = require('../package.json');
+const { version } = require('../package.json');
+const { exec } = require('child_process');
 const fs = require('fs');
 const path = require('path');
-
-version = `${version}.${process.argv[2]}`;
 const file = path.resolve('./postbuild/Tangram.Client.Experience.nuspec');
 
-fs.writeFileSync(file,
-    fs.readFileSync(file, 'utf-8')
-        .split('\r\n')
-        .map(line => {
-            if (line.includes('<version>')) {
-                return line.replace(/>.+</, `>${version}<`);
-            } else {
-                return line;
-            }
-        })
-        .join('\r\n'),
-    'utf-8');
+exec('git rev-parse --abbrev-ref HEAD', (error, stdout) => {
+	const branchName = stdout.trim();
+
+	fs.writeFileSync(file,
+		fs.readFileSync(file, 'utf-8')
+			.split('\r\n')
+			.map(line => {
+				if (line.includes('<version>')) {
+					return line.replace(/>.+</, `>${version}<`);
+				} else if (line.includes('<id>')) {
+					return line.replace('</id>', ` (${branchName})</id>`);
+				}
+				return line;
+			})
+			.join('\r\n'),
+		'utf-8');
+});
+
+
