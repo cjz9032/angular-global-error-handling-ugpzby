742facc8fc6b906c45895d5a69717da971b82c3f
Kidd Liu <kiddliu@lenovo.com>
2019-09-04 15:02:38 +0800

    add build number for uniqueness

--- vtr-ui/postbuild/nuget.js
+++ vtr-ui/postbuild/nuget.js
@@ -1,9 +1,10 @@
-const { version } = require('../package.json');
+let { version } = require('../package.json');
 const { exec } = require('child_process');
 const fs = require('fs');
 const path = require('path');
 const file = path.resolve('./postbuild/Tangram.Client.Experience.nuspec');
 
+version = `${version}.${process.argv[2]}`;
 exec('git rev-parse --abbrev-ref HEAD', (error, stdout) => {
 	const branchName = stdout.trim();
 
@@ -11,10 +12,11 @@ exec('git rev-parse --abbrev-ref HEAD', (error, stdout) => {
 		fs.readFileSync(file, 'utf-8')
 			.split('\r\n')
 			.map(line => {
+				if (line.includes('<id>')) {
+					return line.replace('</id>', ` (${branchName})</id>`);
+				}
 				if (line.includes('<version>')) {
 					return line.replace(/>.+</, `>${version}<`);
-				} else if (line.includes('<id>')) {
-					return line.replace('</id>', ` (${branchName})</id>`);
 				}
 				return line;
 			})
