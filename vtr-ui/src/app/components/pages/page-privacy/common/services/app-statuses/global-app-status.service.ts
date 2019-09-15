c06ff5d1d87ab412b9dbc27bee905281b44d44f8
Zhara <zhara@zeoalliance.com>
2019-09-15 23:35:06 +0300

    switched to new 'trial expiration' API

--- vtr-ui/src/app/components/pages/page-privacy/common/services/app-statuses/global-app-status.service.ts
+++ vtr-ui/src/app/components/pages/page-privacy/common/services/app-statuses/global-app-status.service.ts
@@ -55,9 +55,9 @@ export class GlobalAppStatusService {
 		let appStatus = AppStatuses.figLeafInstalled;
 		const isTrialLicense = this.figleafStatus && this.figleafStatus.licenseType === this.licenseTypes.Trial;
 		const isSubscriptionLicense = this.figleafStatus && this.figleafStatus.licenseType === this.licenseTypes.Subscription;
-		const daysToNotifyTrialExpired = (this.figleafStatus && this.figleafStatus.daysToNotifyTrialExpired) ? this.figleafStatus.daysToNotifyTrialExpired : 1;
+		const daysToNotifyTrialExpired = (this.figleafStatus && this.figleafStatus.daysToExpiration) ? this.figleafStatus.daysToExpiration : 1;
 		const timeToShowExpiredPitchMs = daysToNotifyTrialExpired * MS_IN_DAY;
-		const isExpiredSoon = this.figleafStatus && this.figleafStatus.expirationDate <= Math.floor((Date.now() + timeToShowExpiredPitchMs) / 1000);
+		const isExpiredSoon = this.figleafStatus && this.figleafStatus.daysToExpiration <= Math.floor((Date.now() + timeToShowExpiredPitchMs) / 1000);
 		const isTrialExpired = this.figleafStatus && this.figleafStatus.licenseType === this.licenseTypes.TrialExpired;
 		const isSubscriptionExpired = this.figleafStatus && this.figleafStatus.licenseType === this.licenseTypes.SubscriptionExpired;
 
