65b7e66a8da810f3674f6c01a500c4d3e3133011
Jack Huang <huangbf1@lenovo.com>
2019-09-19 14:36:45 +0800

    VAN-8545 use navigator.onLine for old shell version

--- vtr-ui/src/app/app.component.ts
+++ vtr-ui/src/app/app.component.ts
@@ -92,11 +92,7 @@ export class AppComponent implements OnInit, OnDestroy {
 		this.metricsClient = this.vantageShellService.getMetrics();
 
 		//#endregion
-		if (win.NetworkListener) {
-			win.NetworkListener.onnetworkchanged = (state) => {
-				this.notifyNetworkState(state);
-			};
-		}
+
 
 		document.addEventListener('visibilitychange', (e) => {
 			if (document.hidden) {
@@ -107,10 +103,34 @@ export class AppComponent implements OnInit, OnDestroy {
 		});
 
 
-		if (win.NetworkListener && win.NetworkListener.isInternetAccess()) {
-			this.notifyNetworkState(NetworkStatus.Available);
+		this.addInternetListener();
+	}
+
+	private addInternetListener() {
+		const win: any = window;
+		if (win.NetworkListener) {
+			win.NetworkListener.onnetworkchanged = (state) => {
+				this.notifyNetworkState(state);
+			};
+
+			if ( win.NetworkListener.isInternetAccess()) {
+				this.notifyNetworkState(NetworkStatus.Available);
+			} else {
+				this.notifyNetworkState(NetworkStatus.Unavailable);
+			}
 		} else {
-			this.notifyNetworkState(NetworkStatus.Unavailable);
+			window.addEventListener('online', (e) => {
+				this.notifyNetworkState(NetworkStatus.Available);
+			}, false);
+			window.addEventListener('offline', (e) => {
+				this.notifyNetworkState(NetworkStatus.Unavailable);
+			}, false);
+
+			if (navigator.onLine) {
+				this.notifyNetworkState(NetworkStatus.Available);
+			} else {
+				this.notifyNetworkState(NetworkStatus.Unavailable);
+			}
 		}
 	}
 
