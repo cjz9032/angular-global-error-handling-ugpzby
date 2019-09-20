7d6f4ae22f1b12c15b7e2a833d50ee7288ce602f
Zhara <zhara@zeoalliance.com>
2019-09-20 13:59:03 +0300

    removed unused import

--- vtr-ui/src/app/components/pages/page-privacy/pages/articles/article-sidebar/article-sidebar.component.ts
+++ vtr-ui/src/app/components/pages/page-privacy/pages/articles/article-sidebar/article-sidebar.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
-import { filter, map, switchMap, takeUntil, tap } from 'rxjs/operators';
+import { filter, map, switchMap, takeUntil } from 'rxjs/operators';
 import { SecureMath } from '@lenovo/tan-client-bridge';
 
 import { instanceDestroyed } from '../../../utils/custom-rxjs-operators/instance-destroyed';
