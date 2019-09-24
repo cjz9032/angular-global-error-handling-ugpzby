4a64d54bec70f1bc0d38788f2274d5cb81010aa3
sjain8 <sjain8@lenovo.com>
2019-09-24 14:50:10 +0530

    VAN-2965 Fixed Flicker issue

--- vtr-ui/src/app/components/ui/ui-brightness-slider/ui-brightness-slider.component.ts
+++ vtr-ui/src/app/components/ui/ui-brightness-slider/ui-brightness-slider.component.ts
@@ -49,20 +49,8 @@ export class UiBrightnessSliderComponent implements OnInit, AfterContentChecked 
 		};
 	}
 
-	/**
-	 * This event is fired when value property is changed programmatically.
-	 * Its two way binding event
-	 * @param $event event data from ng5-slider component
-	 */
-	public onValueChange($event: ChangeContext) {
-		this.setLightingBrightness.emit($event);
-		this.change.emit($event);
-		// setTimeout(() => {
-		// 	const element: HTMLElement = document.getElementById('sliderDetailsEle') as HTMLElement;
-		// 	element.focus();
-		// 	this.triggerFocus.emit(PointerType.Min);
-		// }, 500);
+	public dragEnd($event: any) {
+		this.setLightingBrightness.emit($event.value);
+		this.change.emit($event.value);
 	}
-
-	public onSliderChanged(event: any) {}
 }
