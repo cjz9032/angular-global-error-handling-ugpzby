d2c7b597cf08fb5d5bbd83d75e0808be68cae2b8
sateesh nagarjuna <snagarjuna4@lenovo.com>
2019-10-22 18:50:22 +0530

    ui and dev changes

--- vtr-ui/src/app/components/modal/modal-smart-performance-subscribe/modal-smart-performance-subscribe.component.ts
+++ vtr-ui/src/app/components/modal/modal-smart-performance-subscribe/modal-smart-performance-subscribe.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
 import { CommonService } from 'src/app/services/common/common.service';
 import { LocalStorageKey } from 'src/app/enums/local-storage-key.enum';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'vtr-modal-smart-performance-subscribe',
@@ -10,7 +11,9 @@ import { LocalStorageKey } from 'src/app/enums/local-storage-key.enum';
 })
 export class ModalSmartPerformanceSubscribeComponent implements OnInit {
 
-  constructor(public activeModal: NgbActiveModal,private commonService: CommonService) { }
+  constructor(public activeModal: NgbActiveModal,private commonService: CommonService,
+	// private router: Router
+	) { }
 
   ngOnInit() {
   }
@@ -18,7 +21,10 @@ export class ModalSmartPerformanceSubscribeComponent implements OnInit {
     this.activeModal.close('close');
 }
 selectBilledMonthly(){
+	alert("You Have Successfully Subscibed");
 	this.commonService.setLocalStorageValue(LocalStorageKey.IsSubscribed,true);
 	console.log(this.commonService.getLocalStorageValue(LocalStorageKey.IsSubscribed));
+	this.closeModal();
+	// this.router.navigate(['device/smart-performance']);
 }
 }
