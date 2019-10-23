93eb27ebb77a894193f431ad0053bed866257c2a
itpeoplegitadmin <ITpeople!@#123>
2019-10-23 15:22:03 +0530

    smart-performance-ui-changes

--- vtr-ui/src/app/components/ui/ui-smart-performance-scan-summary/ui-smart-performance-scan-summary.component.ts
+++ vtr-ui/src/app/components/ui/ui-smart-performance-scan-summary/ui-smart-performance-scan-summary.component.ts
@@ -14,6 +14,13 @@ export class UiSmartPerformanceScanSummaryComponent implements OnInit {
   public today = new Date();
   public items: any = [];
   isSubscribed:any;
+  subscriptionDetails:any;
+  startDate:any;
+  endDate:any;
+  status:any;
+  givenDate:Date
+  
+
   // tslint:disable-next-line:max-line-length
 @Input() isScanning = false;
 @Input() isScanningCompleted = false;
@@ -22,7 +29,20 @@ export class UiSmartPerformanceScanSummaryComponent implements OnInit {
 	this.items = [ { itemValue : '16 fixes', itemExpandValue : 'lorem ipsum', itemstatus : true, itemDate : this.today},
 	{ itemValue : '0 fixes', itemExpandValue : 'lorem ipsum', itemstatus : false, itemDate : this.today},
 	{ itemValue : '8 fixes', itemExpandValue : 'lorem ipsum', itemstatus : true, itemDate : this.today} ];
-	this.isSubscribed=this.commonService.getLocalStorageValue(LocalStorageKey.IsSubscribed);
+  this.isSubscribed=this.commonService.getLocalStorageValue(LocalStorageKey.IsSubscribed);
+  if(this.isSubscribed)
+  {
+    this.subscriptionDetails = this.commonService.getLocalStorageValue(LocalStorageKey.SubscribtionDetails);
+    this.startDate = this.subscriptionDetails[0].StartDate;
+    this.endDate = this.subscriptionDetails[0].EndDate;
+    this.givenDate = new Date(this.subscriptionDetails[0].EndDate);
+    
+    if(this.givenDate > this.today)
+      this.status = "ACTIVE";
+    else 
+      this.status = "INACTIVE";
+  }
+  
   }
   expandRow(value) {
 	if (this.toggleValue === value) {
