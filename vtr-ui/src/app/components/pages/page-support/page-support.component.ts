f735133f928f158b981f5f7502b7ab474056f3b6
Bob LB4 Liu <liulb4@lenovo.com>
2019-11-04 17:07:17 +0800

    VAN-10523 change support page category icon to png.

--- vtr-ui/src/app/components/pages/page-support/page-support.component.ts
+++ vtr-ui/src/app/components/pages/page-support/page-support.component.ts
@@ -103,11 +103,11 @@ export class PageSupportComponent implements OnInit, OnDestroy {
 		'assets/images/support/support-offline-4.jpg',
 	];
 	localCateIcons = [
-		'assets/images/support/design-innovation.svg',
-		'assets/images/support/how-to.svg',
-		'assets/images/support/lifestyle-entertainment.svg',
-		'assets/images/support/software-apps.svg',
-	]
+		'assets/images/support/design-innovation.png',
+		'assets/images/support/how-to.png',
+		'assets/images/support/lifestyle-entertainment.png',
+		'assets/images/support/software-apps.png',
+	];
 
 	cateStartTime: any;
 	contentStartTime: any;
