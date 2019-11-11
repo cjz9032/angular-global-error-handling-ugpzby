f82fdf44cf1bd25974256e6dac9e71630e141754
raghu <rtenneti@lenovo.com>
2019-11-11 17:52:11 +0530

    autoclose commit

--- vtr-ui/src/app/components/pages/page-autoclose/page-autoclose.component.ts
+++ vtr-ui/src/app/components/pages/page-autoclose/page-autoclose.component.ts
@@ -10,11 +10,11 @@ import { CommonService } from 'src/app/services/common/common.service';
 import { AppNotification } from 'src/app/data-models/common/app-notification.model';
 import { NetworkStatus } from 'src/app/enums/network-status.enum';
 import { LangChangeEvent, TranslateService } from '@ngx-translate/core';
-import { LoggerService } from 'src/app/services/logger/logger.service';
-import { UPEService } from 'src/app/services/upe/upe.service';
-import { HypothesisService } from 'src/app/services/hypothesis/hypothesis.service';
-import { DashboardService } from 'src/app/services/dashboard/dashboard.service';
-import { DeviceService } from 'src/app/services/device/device.service';
+// import { LoggerService } from 'src/app/services/logger/logger.service';
+// import { UPEService } from 'src/app/services/upe/upe.service';
+// import { HypothesisService } from 'src/app/services/hypothesis/hypothesis.service';
+// import { DashboardService } from 'src/app/services/dashboard/dashboard.service';
+// import { DeviceService } from 'src/app/services/device/device.service';
 
 @Component({
 	selector: 'vtr-page-autoclose',
@@ -61,19 +61,19 @@ export class PageAutocloseComponent implements OnInit {
 		private gamingAutoCloseService: GamingAutoCloseService,
 		private commonService: CommonService,
 		private titleService: Title,
-		public dashboardService: DashboardService,
-		private upeService: UPEService,
-		private loggerService: LoggerService,
-		private hypService: HypothesisService,
-		private translate: TranslateService,
-		public deviceService: DeviceService
+		// public dashboardService: DashboardService,
+		// private upeService: UPEService,
+		// private loggerService: LoggerService,
+		// private hypService: HypothesisService,
+		// private translate: TranslateService,
+		// public deviceService: DeviceService
 	) {
-		this.setPreviousContent();
-		this.fetchCMSArticles();
+		// this.setPreviousContent();
+		//this.fetchCMSArticles();
 		// VAN-5872, server switch feature on language change
-		this.translate.onLangChange.subscribe((event: LangChangeEvent) => {
-			this.fetchCMSArticles();
-		});
+		// this.translate.onLangChange.subscribe((event: LangChangeEvent) => {
+		// 	this.fetchCMSArticles();
+		// });
 		this.isOnline = this.commonService.isOnline;
 	}
 
@@ -219,136 +219,136 @@ export class PageAutocloseComponent implements OnInit {
 	}
 
 	// Get the CMS content for the container card
-	fetchCMSArticles() {
-		this.upeRequestResult = {
-			positionB: true,
-			positionF: true
-		};
+	// fetchCMSArticles() {
+	// 	this.upeRequestResult = {
+	// 		positionB: true,
+	// 		positionF: true
+	// 	};
 
-		this.cmsRequestResult = {
-			positionB: false,
-			positionF: false
-		};
-		this.isOnline = this.commonService.isOnline;
-		const queryOptions = {
-			Page: 'dashboard'
-		};
-		this.getTileSource().then(() => {
-			this.cmsService.fetchCMSContent(queryOptions).subscribe((response: any) => {
-				const cardContentPositionF = this.cmsService.getOneCMSContent(
-					response,
-					'half-width-top-image-title-link',
-					'position-F'
-				)[0];
-				if (cardContentPositionF) {
-					this.cardContentPositionFCms = cardContentPositionF;
-					this.cardContentPositionFCms.DataSource = 'cms';
-					if (!this.upeRequestResult.positionF || this.tileSource.positionF === 'CMS') {
-						this.cardContentPositionF = this.cardContentPositionFCms;
-						this.gamingAutoCloseService.cardContentPositionF = this.cardContentPositionFCms;
-					}
-				}
+	// 	this.cmsRequestResult = {
+	// 		positionB: false,
+	// 		positionF: false
+	// 	};
+	// 	this.isOnline = this.commonService.isOnline;
+	// 	const queryOptions = {
+	// 		Page: 'dashboard'
+	// 	};
+	// 	this.getTileSource().then(() => {
+	// 		this.cmsService.fetchCMSContent(queryOptions).subscribe((response: any) => {
+	// 			const cardContentPositionF = this.cmsService.getOneCMSContent(
+	// 				response,
+	// 				'half-width-top-image-title-link',
+	// 				'position-F'
+	// 			)[0];
+	// 			if (cardContentPositionF) {
+	// 				this.cardContentPositionFCms = cardContentPositionF;
+	// 				this.cardContentPositionFCms.DataSource = 'cms';
+	// 				if (!this.upeRequestResult.positionF || this.tileSource.positionF === 'CMS') {
+	// 					this.cardContentPositionF = this.cardContentPositionFCms;
+	// 					this.gamingAutoCloseService.cardContentPositionF = this.cardContentPositionFCms;
+	// 				}
+	// 			}
 
-				const cardContentPositionB = this.cmsService.getOneCMSContent(
-					response,
-					'half-width-title-description-link-image',
-					'position-B'
-				)[0];
-				if (cardContentPositionB) {
-					cardContentPositionB.DataSource = 'cms';
-					this.cardContentPositionBCms = cardContentPositionB;
-					if (this.cardContentPositionBCms.BrandName) {
-						this.cardContentPositionBCms.BrandName = this.cardContentPositionBCms.BrandName.split('|')[0];
-					}
-					this.cmsRequestResult.positionB = true;
-					if (!this.upeRequestResult.positionB || this.tileSource.positionB === 'CMS') {
-						this.cardContentPositionB = this.cardContentPositionBCms;
-						this.gamingAutoCloseService.cardContentPositionB = this.cardContentPositionBCms;
-					}
-				}
-			});
+	// 			const cardContentPositionB = this.cmsService.getOneCMSContent(
+	// 				response,
+	// 				'half-width-title-description-link-image',
+	// 				'position-B'
+	// 			)[0];
+	// 			if (cardContentPositionB) {
+	// 				cardContentPositionB.DataSource = 'cms';
+	// 				this.cardContentPositionBCms = cardContentPositionB;
+	// 				if (this.cardContentPositionBCms.BrandName) {
+	// 					this.cardContentPositionBCms.BrandName = this.cardContentPositionBCms.BrandName.split('|')[0];
+	// 				}
+	// 				this.cmsRequestResult.positionB = true;
+	// 				if (!this.upeRequestResult.positionB || this.tileSource.positionB === 'CMS') {
+	// 					this.cardContentPositionB = this.cardContentPositionBCms;
+	// 					this.gamingAutoCloseService.cardContentPositionB = this.cardContentPositionBCms;
+	// 				}
+	// 			}
+	// 		});
 
-			if (this.tileSource.positionB === 'UPE') {
-				const upeParam = { position: 'position-B' };
-				this.upeService.fetchUPEContent(upeParam).subscribe(
-					(upeResp) => {
-						const cardContentPositionB = this.upeService.getOneUPEContent(
-							upeResp,
-							'half-width-title-description-link-image',
-							'position-B'
-						)[0];
-						if (cardContentPositionB) {
-							this.cardContentPositionB = cardContentPositionB;
-							if (this.cardContentPositionB.BrandName) {
-								this.cardContentPositionB.BrandName = this.cardContentPositionB.BrandName.split('|')[0];
-							}
-							this.cardContentPositionB.DataSource = 'upe';
-							this.gamingAutoCloseService.cardContentPositionB = this.cardContentPositionB;
-							this.upeRequestResult.positionB = true;
-						}
-					},
-					(err) => {
-						this.loggerService.info(`Cause by error: ${err}, position-B load CMS content.`);
-						this.upeRequestResult.positionB = false;
-						if (this.cmsRequestResult.positionB) {
-							this.cardContentPositionB = this.cardContentPositionBCms;
-							this.gamingAutoCloseService.cardContentPositionB = this.cardContentPositionBCms;
-						}
-					}
-				);
-			}
+	// 		if (this.tileSource.positionB === 'UPE') {
+	// 			const upeParam = { position: 'position-B' };
+	// 			this.upeService.fetchUPEContent(upeParam).subscribe(
+	// 				(upeResp) => {
+	// 					const cardContentPositionB = this.upeService.getOneUPEContent(
+	// 						upeResp,
+	// 						'half-width-title-description-link-image',
+	// 						'position-B'
+	// 					)[0];
+	// 					if (cardContentPositionB) {
+	// 						this.cardContentPositionB = cardContentPositionB;
+	// 						if (this.cardContentPositionB.BrandName) {
+	// 							this.cardContentPositionB.BrandName = this.cardContentPositionB.BrandName.split('|')[0];
+	// 						}
+	// 						this.cardContentPositionB.DataSource = 'upe';
+	// 						this.gamingAutoCloseService.cardContentPositionB = this.cardContentPositionB;
+	// 						this.upeRequestResult.positionB = true;
+	// 					}
+	// 				},
+	// 				(err) => {
+	// 					this.loggerService.info(`Cause by error: ${err}, position-B load CMS content.`);
+	// 					this.upeRequestResult.positionB = false;
+	// 					if (this.cmsRequestResult.positionB) {
+	// 						this.cardContentPositionB = this.cardContentPositionBCms;
+	// 						this.gamingAutoCloseService.cardContentPositionB = this.cardContentPositionBCms;
+	// 					}
+	// 				}
+	// 			);
+	// 		}
 
-			if (this.tileSource.positionF === 'UPE') {
-				const upeParam = { position: 'position-F' };
-				this.upeService.fetchUPEContent(upeParam).subscribe(
-					(upeResp) => {
-						const cardContentPositionF = this.upeService.getOneUPEContent(
-							upeResp,
-							'half-width-top-image-title-link',
-							'position-F'
-						)[0];
-						if (cardContentPositionF) {
-							this.cardContentPositionF = cardContentPositionF;
-							this.cardContentPositionF.DataSource = 'upe';
-							this.gamingAutoCloseService.cardContentPositionF = this.cardContentPositionF;
-							this.upeRequestResult.positionF = true;
-						}
-					},
-					(err) => {
-						this.loggerService.info(`Cause by error: ${err}, position-F load CMS content.`);
-						this.upeRequestResult.positionF = false;
-						if (this.cmsRequestResult.positionF) {
-							this.cardContentPositionF = this.cardContentPositionFCms;
-							this.gamingAutoCloseService.cardContentPositionF = this.cardContentPositionFCms;
-						}
-					}
-				);
-			}
-		});
-	}
+	// 		if (this.tileSource.positionF === 'UPE') {
+	// 			const upeParam = { position: 'position-F' };
+	// 			this.upeService.fetchUPEContent(upeParam).subscribe(
+	// 				(upeResp) => {
+	// 					const cardContentPositionF = this.upeService.getOneUPEContent(
+	// 						upeResp,
+	// 						'half-width-top-image-title-link',
+	// 						'position-F'
+	// 					)[0];
+	// 					if (cardContentPositionF) {
+	// 						this.cardContentPositionF = cardContentPositionF;
+	// 						this.cardContentPositionF.DataSource = 'upe';
+	// 						this.gamingAutoCloseService.cardContentPositionF = this.cardContentPositionF;
+	// 						this.upeRequestResult.positionF = true;
+	// 					}
+	// 				},
+	// 				(err) => {
+	// 					this.loggerService.info(`Cause by error: ${err}, position-F load CMS content.`);
+	// 					this.upeRequestResult.positionF = false;
+	// 					if (this.cmsRequestResult.positionF) {
+	// 						this.cardContentPositionF = this.cardContentPositionFCms;
+	// 						this.gamingAutoCloseService.cardContentPositionF = this.cardContentPositionFCms;
+	// 					}
+	// 				}
+	// 			);
+	// 		}
+	// 	});
+	// }
 
-	private getTileSource() {
-		return new Promise((resolve) => {
-			this.hypService.getAllSettings().then(
-				(hyp: any) => {
-					if (hyp) {
-						this.tileSource.positionB = hyp.TileBSource === 'UPE' ? 'UPE' : 'CMS';
-						this.tileSource.positionF = hyp.TileFSource === 'UPE' ? 'UPE' : 'CMS';
-					}
-					resolve();
-				},
-				() => {
-					resolve();
-					console.log('get tile source failed.');
-				}
-			);
-		});
-	}
+	// private getTileSource() {
+	// 	return new Promise((resolve) => {
+	// 		this.hypService.getAllSettings().then(
+	// 			(hyp: any) => {
+	// 				if (hyp) {
+	// 					this.tileSource.positionB = hyp.TileBSource === 'UPE' ? 'UPE' : 'CMS';
+	// 					this.tileSource.positionF = hyp.TileFSource === 'UPE' ? 'UPE' : 'CMS';
+	// 				}
+	// 				resolve();
+	// 			},
+	// 			() => {
+	// 				resolve();
+	// 				console.log('get tile source failed.');
+	// 			}
+	// 		);
+	// 	});
+	// }
 
-	private setPreviousContent() {
-		this.cardContentPositionF = this.gamingAutoCloseService.cardContentPositionF;
-		this.cardContentPositionB = this.gamingAutoCloseService.cardContentPositionB;
-	}
+	// private setPreviousContent() {
+	// 	this.cardContentPositionF = this.gamingAutoCloseService.cardContentPositionF;
+	// 	this.cardContentPositionB = this.gamingAutoCloseService.cardContentPositionB;
+	// }
 
 	async getAutoCloseStatus() {
 		try {
