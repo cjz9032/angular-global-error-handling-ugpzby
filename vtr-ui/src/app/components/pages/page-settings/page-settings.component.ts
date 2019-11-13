0fd80ab883bcc15af3d1903d46a57ceba1c36ea9
Ling Zhang <zhangling11@lenovo.com>
2019-11-13 17:23:53 +0800

    VAN-11464 check selection changed for button state

--- vtr-ui/src/app/components/pages/page-settings/page-settings.component.ts
+++ vtr-ui/src/app/components/pages/page-settings/page-settings.component.ts
@@ -32,6 +32,7 @@ export class PageSettingsComponent implements OnInit, OnDestroy {
 	isToggleUsageStatistics = false;
 	isToggleDeviceStatistics = false;
 	usageRadioValue = null;
+	userSelectionChanged = false;
 
 	valueToBoolean = [false, true, false];
 
@@ -97,7 +98,7 @@ export class PageSettingsComponent implements OnInit, OnDestroy {
 
 	private getSelfSelectStatus() {
 		this.selfSelectService.getConfig();
-		this.selfSelectService.userSelectionChanged = false;
+		this.userSelectionChanged = false;
 	}
 
 	private getSegment() {
@@ -296,7 +297,7 @@ export class PageSettingsComponent implements OnInit, OnDestroy {
 
 	saveUsageType(value) {
 		this.selfSelectService.usageType = value;
-		this.selfSelectService.userSelectionChanged = true;
+		this.userSelectionChanged = this.selfSelectService.selectionChanged();
 	}
 
 	onInterestToggle($event, value) {
@@ -305,12 +306,12 @@ export class PageSettingsComponent implements OnInit, OnDestroy {
 		} else {
 			this.selfSelectService.checkedArray.splice(this.selfSelectService.checkedArray.indexOf(value), 1);
 		}
-		this.selfSelectService.userSelectionChanged = true;
+		this.userSelectionChanged = this.selfSelectService.selectionChanged();
 	}
 
 	saveUserProfile() {
 		this.selfSelectService.saveConfig();
-		this.selfSelectService.userSelectionChanged = false;
+		this.userSelectionChanged = this.selfSelectService.selectionChanged();
 		const usageData = {
 			ItemType: 'FeatureClick',
 			ItemName: 'UsageType',
