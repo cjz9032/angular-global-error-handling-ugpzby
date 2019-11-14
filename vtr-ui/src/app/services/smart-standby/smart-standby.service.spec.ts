ba01de6f344fa514eb375d0a20cdec5d71329d11
Santosh udnur <sudnur@lenovo.com>
2019-11-14 16:26:06 +0530

    spec issues with self-select.service.spec.ts ,smart-standby.service.spec.ts is fixed

--- vtr-ui/src/app/services/smart-standby/smart-standby.service.spec.ts
+++ vtr-ui/src/app/services/smart-standby/smart-standby.service.spec.ts
@@ -1,12 +1,17 @@
-// import { TestBed } from '@angular/core/testing';
+import { TestBed } from '@angular/core/testing';
 
-// import { SmartStandbyService } from './smart-standby.service';
+import { SmartStandbyService } from './smart-standby.service';
+import { TranslateService, TranslateStore } from '@ngx-translate/core';
+import { TranslationModule } from 'src/app/modules/translation.module';
 
-// describe('SmartStandbyService', () => {
-//   beforeEach(() => TestBed.configureTestingModule({}));
+describe('SmartStandbyService', () => {
+	beforeEach(() => TestBed.configureTestingModule({
+		providers: [TranslateStore],
+		imports: [TranslationModule.forChild()]
+	}));
 
-//   it('should be created', () => {
-//     const service: SmartStandbyService = TestBed.get(SmartStandbyService);
-//     expect(service).toBeTruthy();
-//   });
-// });
+	it('should be created', () => {
+		const service: SmartStandbyService = TestBed.get(SmartStandbyService);
+		expect(service).toBeTruthy();
+	});
+});
