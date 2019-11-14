5add237173fd5d769cc4c627317dd4a354cbaeac
Bob LB4 Liu <liulb4@lenovo.com>
2019-11-14 17:29:40 +0800

    VAN-11449 fix system update log modal zoom.

--- vtr-ui/src/app/components/modal/modal-update-change-log.component/modal-update-change-log.component.ts
+++ vtr-ui/src/app/components/modal/modal-update-change-log.component/modal-update-change-log.component.ts
@@ -1,29 +1,56 @@
-import { Component, OnInit, Output, OnDestroy } from '@angular/core';
-import { SafeUrl } from '@angular/platform-browser';
+import { Component, OnInit, Output, OnDestroy, AfterViewInit, SecurityContext } from '@angular/core';
+import { SafeUrl, SafeHtml, DomSanitizer } from '@angular/platform-browser';
 import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
 import { VantageShellService } from 'src/app/services/vantage-shell/vantage-shell.service';
+
+declare let Windows: any;
 
 @Component({
 	selector: 'vtr-modal-update-change-log.',
 	templateUrl: './modal-update-change-log.component.html',
 	styleUrls: ['./modal-update-change-log.component.scss']
 })
-export class ModalUpdateChangeLogComponent implements OnInit, OnDestroy {
+export class ModalUpdateChangeLogComponent implements OnInit, AfterViewInit, OnDestroy {
 
 	@Output() url: string;
 	updateModalMetrics: any;
 	metrics: any;
 	iframeInterval: any;
+	articleBody: SafeHtml = '';
 
 	constructor(
 		public activeModal: NgbActiveModal,
+		private sanitizer: DomSanitizer,
 		private shellService: VantageShellService
 	) {
 		this.metrics = this.shellService.getMetrics();
 	}
 
 	ngOnInit() {
-		this.disableWheel()
+		const warrantyUrl = `https://download.lenovo.com/pccbbs/mobiles/n2car17w.txt`;
+		const uri = new Windows.Foundation.Uri(warrantyUrl);
+		const request = new Windows.Web.Http.HttpRequestMessage(Windows.Web.Http.HttpMethod.get, uri);
+		const httpClient = new Windows.Web.Http.HttpClient();
+		(async () => {
+			try {
+				const response = await httpClient.sendRequestAsync(request);
+				const result = await response.content.readAsStringAsync();
+				if (result) {
+					this.articleBody = this.sanitizer.sanitize(SecurityContext.HTML, result);
+					console.log(result);
+					// resolve(Warranty.getWarranty(result));
+				} else {
+					// reject(new Error('can not get warranty information'));
+				}
+			} catch (e) {
+				// reject(new Error('can not get warranty information'));
+			}
+			httpClient.close();
+		})();
+	}
+
+	ngAfterViewInit() {
+		this.disableZoom();
 	}
 
 	ngOnDestroy() {
@@ -46,10 +73,10 @@ export class ModalUpdateChangeLogComponent implements OnInit, OnDestroy {
 		}
 	}
 
-	disableWheel() {
+	disableZoom() {
 		const iframe: any = document.querySelector('#modal-update-change-log-iframe');
 		this.iframeInterval = setInterval(() => {
-			if (iframe.contentWindow) {
+			if (iframe && iframe.contentWindow) {
 				iframe.contentWindow.addEventListener('mousewheel', (event: any) => {
 					if (event.ctrlKey === true || event.metaKey) {
 						event.preventDefault();
