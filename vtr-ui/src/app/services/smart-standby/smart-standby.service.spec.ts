5ec82eea3f1b5516a3e6b0be63adb110a0085dda
Santosh udnur <sudnur@lenovo.com>
2019-11-14 16:50:10 +0530

    updated hypoothesis Service  formatting

--- vtr-ui/src/app/services/smart-standby/smart-standby.service.spec.ts
+++ vtr-ui/src/app/services/smart-standby/smart-standby.service.spec.ts
@@ -3,15 +3,18 @@ import { TestBed } from '@angular/core/testing';
 import { SmartStandbyService } from './smart-standby.service';
 import { TranslateService, TranslateStore } from '@ngx-translate/core';
 import { TranslationModule } from 'src/app/modules/translation.module';
+import { CommonService } from '../common/common.service';
 
-describe('SmartStandbyService', () => {
+fdescribe('SmartStandbyService', () => {
 	beforeEach(() => TestBed.configureTestingModule({
-		providers: [TranslateStore],
+		providers: [TranslateStore, CommonService],
 		imports: [TranslationModule.forChild()]
 	}));
 
 	it('should be created', () => {
-		const service: SmartStandbyService = TestBed.get(SmartStandbyService);
-		expect(service).toBeTruthy();
+		const ssbService: SmartStandbyService = TestBed.get(SmartStandbyService);
+		const commonService: CommonService = TestBed.get(CommonService);
+		expect(ssbService).toBeTruthy();
+		expect(commonService).toBeTruthy();
 	});
 });
