2ea5139e755985c321a1a748b5528a7a0d596a2c
Ling Zhang <zhangling11@lenovo.com>
2019-11-14 13:48:44 +0800

    VAN-11465 change metric to setting update in settings page

--- vtr-ui/src/app/components/pages/page-settings/page-settings.component.ts
+++ vtr-ui/src/app/components/pages/page-settings/page-settings.component.ts
@@ -315,10 +315,10 @@ export class PageSettingsComponent implements OnInit, OnDestroy {
 		this.selfSelectService.saveConfig();
 		this.userSelectionChanged = this.selfSelectService.selectionChanged();
 		const usageData = {
-			ItemType: 'FeatureClick',
-			ItemName: 'UsageType',
-			ItemValue: this.deviceService.isGaming ? 'Gaming' : this.selfSelectService.usageType,
-			ItemParent: 'Page.Settings'
+			ItemType: 'SettingUpdate',
+			SettingName: 'UsageType',
+			SettingValue: this.deviceService.isGaming ? 'Gaming' : this.selfSelectService.usageType,
+			SettingParent: 'Page.Settings'
 		};
 		this.metrics.sendAsync(usageData);
 
@@ -327,10 +327,10 @@ export class PageSettingsComponent implements OnInit, OnDestroy {
 			interestMetricValue[item] = true;
 		});
 		const interestData = {
-			ItemType: 'FeatureClick',
-			ItemName: 'Interest',
-			ItemValue: interestMetricValue,
-			ItemParent: 'Page.Settings'
+			ItemType: 'SettingUpdate',
+			SettingName: 'Interest',
+			SettingValue: interestMetricValue,
+			SettingParent: 'Page.Settings'
 		};
 		this.metrics.sendAsync(interestData);
 		this.segmentTag = this.selfSelectService.usageType;
