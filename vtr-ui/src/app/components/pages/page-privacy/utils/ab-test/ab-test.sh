76b73ed559b5cf86b762fcd105b4807ae00fe4bc
creo <creo@figleaf.com>
2019-11-15 15:51:22 +0200

    privacy/creo/added version for node script

--- vtr-ui/src/app/components/pages/page-privacy/utils/ab-test/ab-test.sh
+++ vtr-ui/src/app/components/pages/page-privacy/utils/ab-test/ab-test.sh
@@ -3,7 +3,7 @@
 currentDirectory=$(dirname $(readlink -f $0))
 
 #!/usr/bin/env node
-node $currentDirectory/index.js
+node $currentDirectory/generateTestsConfig.js --version=15
 
 npx json-schema-to-typescript --no-enableConstEnums $currentDirectory/abTestsConfig.json > $currentDirectory/ab-tests.type.ts
 
