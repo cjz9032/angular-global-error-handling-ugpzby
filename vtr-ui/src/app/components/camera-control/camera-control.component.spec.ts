f4c683ef8c87a6a5ea4be5c54ecd958f9bc26430
Santosh udnur <sudnur@lenovo.com>
2019-11-22 17:36:40 +0530

    Camera-Control component

--- vtr-ui/src/app/components/camera-control/camera-control.component.spec.ts
+++ vtr-ui/src/app/components/camera-control/camera-control.component.spec.ts
@@ -1,14 +1,26 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { CameraControlComponent } from './camera-control.component';
+import { TranslationModule } from 'src/app/modules/translation.module';
+import { TranslateStore } from '@ngx-translate/core';
+import { NO_ERRORS_SCHEMA, NgZone } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+import { UiSwitchOnoffComponent } from '../ui/ui-switch-onoff/ui-switch-onoff.component';
+import { CameraFeedService } from 'src/app/services/camera/camera-feed/camera-feed.service';
+import { BaseCameraDetail } from 'src/app/services/camera/camera-detail/base-camera-detail.service';
+import { VantageShellService } from 'src/app/services/vantage-shell/vantage-shell-mock.service';
+import { LoggerService } from 'src/app/services/logger/logger.service';
 
-xdescribe('CameraControlComponent', () => {
+describe('CameraControlComponent', () => {
 	let component: CameraControlComponent;
 	let fixture: ComponentFixture<CameraControlComponent>;
 
 	beforeEach(async(() => {
 		TestBed.configureTestingModule({
-			declarations: [CameraControlComponent]
+			declarations: [CameraControlComponent, UiSwitchOnoffComponent],
+			schemas: [NO_ERRORS_SCHEMA],
+			imports: [TranslationModule, HttpClientModule],
+			providers: [TranslateStore, UiSwitchOnoffComponent, CameraFeedService, BaseCameraDetail, VantageShellService, LoggerService, NgZone],
 		})
 			.compileComponents();
 	}));
