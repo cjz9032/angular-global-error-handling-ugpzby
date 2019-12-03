1873779f2b081e4dc481f8c0f6582a25ccc20bf4
Santosh udnur <sudnur@lenovo.com>
2019-12-03 19:02:23 +0530

    UT cases issue fixed

--- vtr-ui/src/app/components/pages/page-security/widget/widget-landing-security/widget-landing-security.component.spec.ts
+++ vtr-ui/src/app/components/pages/page-security/widget/widget-landing-security/widget-landing-security.component.spec.ts
@@ -1,6 +1,8 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { WidgetLandingSecurityComponent } from './widget-landing-security.component';
+import { UiButtonModule } from 'src/app/components/ui/ui-button/ui-button.module';
+import { NO_ERRORS_SCHEMA } from '@angular/core';
 
 describe('WidgetLandingSecurityComponent', () => {
 	let component: WidgetLandingSecurityComponent;
@@ -8,7 +10,9 @@ describe('WidgetLandingSecurityComponent', () => {
 
 	beforeEach(async(() => {
 		TestBed.configureTestingModule({
-			declarations: [WidgetLandingSecurityComponent]
+			declarations: [WidgetLandingSecurityComponent],
+			schemas: [NO_ERRORS_SCHEMA],
+			imports: [UiButtonModule]
 		})
 			.compileComponents();
 	}));
--- vtr-ui/src/app/services/warranty/warranty.service.spec.ts
+++ vtr-ui/src/app/services/warranty/warranty.service.spec.ts
@@ -1,9 +1,21 @@
 import { TestBed } from '@angular/core/testing';
 
 import { WarrantyService } from './warranty.service';
+import { TranslationModule } from 'src/app/modules/translation.module';
+import { HttpClientModule } from '@angular/common/http';
+import { TranslateStore } from '@ngx-translate/core';
+import { RouterTestingModule } from '@angular/router/testing';
+import { MetricsDirective } from 'src/app/directives/metrics.directive';
+import { MetricsModule } from 'src/app/directives/metrics.module';
+import { DeviceService } from '../device/device.service';
+import { CommonService } from '../common/common.service';
+import { VantageShellService } from '../vantage-shell/vantage-shell-mock.service';
 
 describe('WarrantyService', () => {
-	beforeEach(() => TestBed.configureTestingModule({}));
+	beforeEach(() => TestBed.configureTestingModule({
+		imports: [RouterTestingModule, TranslationModule, HttpClientModule, MetricsModule],
+		providers: [TranslateStore, CommonService, DeviceService, VantageShellService]
+	}));
 
 	it('should be created', () => {
 		const service: WarrantyService = TestBed.get(WarrantyService);
