70a8c072353ac8fd24f77a3b85b7299f4c803de6
April <ludan2@lenovo.com>
2019-12-05 18:23:22 +0800

    VAN-11237 When location is enabled, the next button in welcome page1 will still show up. merge

--- vtr-ui/src/app/components/pages/page-connected-home-security/component/home-security-content/home-security-content.component.ts
+++ vtr-ui/src/app/components/pages/page-connected-home-security/component/home-security-content/home-security-content.component.ts
@@ -3,7 +3,7 @@ import { DialogService } from 'src/app/services/dialog/dialog.service';
 import { NgbModalRef, NgbModal } from '@ng-bootstrap/ng-bootstrap';
 import { ModalArticleDetailComponent } from 'src/app/components/modal/modal-article-detail/modal-article-detail.component';
 import { CMSService } from 'src/app/services/cms/cms.service';
-import { HomeSecurityLocation } from 'src/app/data-models/home-security/home-security-location.model';
+import { DeviceLocationPermission } from 'src/app/data-models/home-security/device-location-permission.model';
 
 @Component({
 	selector: 'vtr-home-security-content',
@@ -12,7 +12,7 @@ import { HomeSecurityLocation } from 'src/app/data-models/home-security/home-sec
 })
 export class HomeSecurityContentComponent implements OnInit {
 	@Input() page: string;
-	@Input() location: HomeSecurityLocation;
+	@Input() location: DeviceLocationPermission;
 	@Input() permission: any;
 	peaceOfMindArticleId = '988BE19B75554E09B5A914D5F803C3F3';
 	peaceOfMindArticleCategory: string;
