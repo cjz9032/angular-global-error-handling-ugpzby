c0474abd3737c8e0510adb53b1d722d3df7e58bc
raj <rsahu2@lenovo.com>
2019-12-23 14:26:37 +0530

    Revert "refactor(routing): replace deprecated DeprecatedLoadChildren"
    This reverts commit 828306a5f35dc9665f3b2b7160d3e58b86970d63.

--- vtr-ui/src/app/modules/app-routing.module.ts
+++ vtr-ui/src/app/modules/app-routing.module.ts
@@ -4,21 +4,22 @@ import { RouterModule, Routes, PreloadAllModules } from '@angular/router';
 import { PageSettingsComponent } from '../components/pages/page-settings/page-settings.component';
 
 const routes: Routes = [
+
 	{
 		path: 'dashboard',
-		loadChildren: () => import('./hardware-settings/hardware-dashboard.module').then(mod => mod.HardwareDashboardModule)
+		loadChildren: './hardware-settings/hardware-dashboard.module#HardwareDashboardModule'
 	},
 	{
 		path: 'device-gaming',
-		loadChildren: () => import('./gaming-dashboard.module').then(mod => mod.GamingDashboardModule)
+		loadChildren: './gaming-dashboard.module#GamingDashboardModule'
 	},
 	{
 		path: 'gaming',
-		loadChildren: () => import('./gaming.module').then(mod => mod.GamingModule)
+		loadChildren: './gaming.module#GamingModule'
 	},
 	{
 		path: 'device',
-		loadChildren: () => import('./hardware-settings/hardware-settings.module').then(mod => mod.HardwareSettingsModule)
+		loadChildren: './hardware-settings/hardware-settings.module#HardwareSettingsModule'
 	},
 	{
 		path: 'settings',
@@ -29,35 +30,35 @@ const routes: Routes = [
 	},
 	{
 		path: 'home-security',
-		loadChildren: () => import('./connected-home-security/connected-home-security.module').then(mod => mod.ConnectedHomeSecurityModule)
+		loadChildren: './connected-home-security/connected-home-security.module#ConnectedHomeSecurityModule'
 	},
 	{
 		path: 'privacy',
-		loadChildren: () => import('../components/pages/page-privacy/privacy.module').then(mod => mod.PrivacyModule)
+		loadChildren: '../components/pages/page-privacy/privacy.module#PrivacyModule'
 	},
 	{
 		path: 'security',
-		loadChildren: () => import('./security-advisor/security-advisor.module').then(mod => mod.SecurityAdvisorModule)
+		loadChildren: './security-advisor/security-advisor.module#SecurityAdvisorModule'
 	},
 	{
 		path: 'support',
-		loadChildren: () => import('./support/support.module').then(mod => mod.SupportModule)
+		loadChildren: './support/support.module#SupportModule'
 	},
 	{
 		path: 'apps-for-you/:id',
-		loadChildren: () => import('./apps-for-you/apps-for-you.module').then(mod => mod.AppsForYouModule)
+		loadChildren: './apps-for-you/apps-for-you.module#AppsForYouModule'
 	},
 	{
 		path: 'android',
-		loadChildren: () => import('./android/android-dashboard.module').then(mod => mod.AndroidDashboardModule)
+		loadChildren: './android/android-dashboard.module#AndroidDashboardModule'
 	},
 	{
 		path: 'beta',
-		loadChildren: () => import('../beta/beta.module').then(mod => mod.BetaModule)
+		loadChildren: '../beta/beta.module#BetaModule'
 	},
 	// {
 	// 	path: 'device/smart-performance',
-	// 	loadChildren: () => import('./smart-performance/smart-performance.module').then(mod => mod.SmartPerformanceModule)
+	// 	loadChildren: './smart-performance/smart-performance.module#SmartPerformanceModule'
 	// },
 	{
 		path: '',
