31c6e1477c8ff0a7694aa41da38a5bccfc434d2d
Lucas Ma <mayh11@lenovo.com>
2019-12-25 17:54:00 +0800

    merge Vantage3.2 bug VAN-12552 to `Vantage3.1.6`

--- vtr-ui/src/app/components/pages/page-security-antivirus/page-security-antivirus.component.ts
+++ vtr-ui/src/app/components/pages/page-security-antivirus/page-security-antivirus.component.ts
@@ -268,7 +268,7 @@ export class PageSecurityAntivirusComponent implements OnInit, OnDestroy {
 		articleDetailModal.componentInstance.articleId = this.mcafeeArticleId;
 	}
 
-	getMcafeeFeature(mcafee: phoenix.McAfeeInfo, data?) {
+	getMcafeeFeature(mcafee: phoenix.McAfeeInfo, data?: Array<phoenix.McafeeFeature>) {
 		const featureList = [{
 			status: mcafee.registered,
 			title: this.register,
@@ -325,12 +325,30 @@ export class PageSecurityAntivirusComponent implements OnInit, OnDestroy {
 			});
 		} else if (data) {
 			if (data.length < 6) {
-				data.forEach((feature) => {
-					featureList.push({
-						status: feature.value,
-						title: feature.key,
-						installed: feature.installed
-					});
+				featureList.push({
+					status: data[0].value,
+					title: this.virusScan,
+					installed: data[0].installed
+				});
+				featureList.push({
+					status: data[1].value,
+					title: this.fireWall,
+					installed: data[1].installed
+				});
+				featureList.push({
+					status: null,
+					title: this.antiSpam,
+					installed: data[2].installed
+				});
+				featureList.push({
+					status: null,
+					title: this.quickClean,
+					installed: data[3].installed
+				});
+				featureList.push({
+					status: null,
+					title: this.vulnerability,
+					installed: data[4].installed
 				});
 			} else {
 				featureList.push({
@@ -361,12 +379,30 @@ export class PageSecurityAntivirusComponent implements OnInit, OnDestroy {
 			}
 		} else {
 			if (mcafee.features.length < 6) {
-				mcafee.features.forEach((feature) => {
-					featureList.push({
-						status: feature.value,
-						title: feature.key,
-						installed: feature.installed
-					});
+				featureList.push({
+					status: mcafee.status,
+					title: this.virusScan,
+					installed: true
+				});
+				featureList.push({
+					status: mcafee.firewallStatus,
+					title: this.fireWall,
+					installed: true
+				});
+				featureList.push({
+					status: null,
+					title: this.antiSpam,
+					installed: mcafee.features[2].installed
+				});
+				featureList.push({
+					status: null,
+					title: this.quickClean,
+					installed: mcafee.features[3].installed
+				});
+				featureList.push({
+					status: null,
+					title: this.vulnerability,
+					installed: mcafee.features[4].installed
 				});
 			} else {
 				featureList.push({
