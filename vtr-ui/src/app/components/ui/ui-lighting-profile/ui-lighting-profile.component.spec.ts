98d731cfaba3ea89ca94b0a217009927b8db9406
sdhanekula <sdhanekula@lenovo.com>
2020-01-13 12:53:14 +0530

    ui-lighting-profile component coverage updated

--- vtr-ui/src/app/components/ui/ui-lighting-profile/ui-lighting-profile.component.spec.ts
+++ vtr-ui/src/app/components/ui/ui-lighting-profile/ui-lighting-profile.component.spec.ts
@@ -1,7 +1,6 @@
-import { resolve } from 'url';
 import { LightEffectComplexType } from './../../../enums/light-effect-complex-type';
 import { LightEffectRGBFeature } from './../../../enums/light-effect-rgbfeature';
-import {  ComponentFixture, TestBed, fakeAsync, tick } from '@angular/core/testing';
+import { ComponentFixture, TestBed, fakeAsync, tick } from '@angular/core/testing';
 import { UiLightingProfileComponent } from './ui-lighting-profile.component';
 import { GamingLightingService } from './../../../services/gaming/lighting/gaming-lighting.service';
 import { Pipe, NO_ERRORS_SCHEMA } from '@angular/core';
@@ -9,8 +8,8 @@ import { HttpClient } from '@angular/common/http';
 import { Router } from '@angular/router';
 import { DeviceService } from 'src/app/services/device/device.service';
 
-const gamingLightingServiceMock = jasmine.createSpyObj('GamingLightingService', ['setLightingProfileId', 'setLightingProfileBrightness',
- 'isShellAvailable', 'getLightingCapabilities', 'optionChangedRGBTop', 'optionChangedRGBSide', 'setLightingDefaultProfileById', 'setLightingProfileEffectColor']);
+const gamingLightingServiceMock = jasmine.createSpyObj('GamingLightingService', ['getLightingProfileById', 'setLightingProfileId', 'setLightingProfileBrightness',
+	'isShellAvailable', 'getLightingCapabilities', 'optionChangedRGBTop', 'optionChangedRGBSide', 'setLightingDefaultProfileById', 'setLightingProfileEffectColor']);
 const deviceServiceMock = jasmine.createSpyObj('DeviceService', ['isShellAvailable', 'getMachineInfo']);
 const singleColorResponse = { LightPanelType: [1], LedType_Complex: [0], LedType_simple: [1, 2, 3, 4], BrightAdjustLevel: 0, RGBfeature: 1 };
 const multipleColorResponse = { LightPanelType: [32, 64], LedType_Complex: [268435456, 1, 2, 4, 8, 32, 64, 128], LedType_simple: [0], BrightAdjustLevel: 4, RGBfeature: 255 };
@@ -118,6 +117,27 @@ describe('UiLightingProfileComponent', () => {
 		tick(10);
 		expect(component.profileBrightness).toEqual(3);
 		tick(10);
+		gamingLightingServiceMock.setLightingDefaultProfileById.and.returnValue(Promise.resolve(
+			{
+				didSuccess: false,
+				profileId: 1,
+				brightness: 3,
+				lightInfo: [
+					{
+						lightPanelType: 32,
+						lightEffectType: 4,
+						lightColor: 'FFFFFF'
+					},
+					{
+						lightPanelType: 64,
+						lightEffectType: 2,
+						lightColor: 'FF0000'
+					}
+				]
+			}
+		));
+		component.setDefaultProfile(component.currentProfileId);
+		tick(10);
 	}));
 
 	it('SHould get the cache details', () => {
@@ -158,54 +178,96 @@ describe('UiLightingProfileComponent', () => {
 		const res = component.optionChangedRGBTop(event, {});
 		expect(res).toBe(undefined);
 		tick(5);
-		gamingLightingServiceMock.isShellAvailable.and.returnValue(true);
-		getLightingProfileById.didSuccess = false;
+		gamingLightingServiceMock.isShellAvailable = true;
 		gamingLightingServiceMock.setLightingProfileEffectColor.and
-			.returnValue(Promise.resolve(getLightingProfileById));
+			.returnValue(Promise.resolve({...getLightingProfileById, didSuccess: false}));
 		event.value = LightEffectComplexType.Breath;
 		component.simpleOrComplex = LightEffectRGBFeature.Complex;
 		const res1 = component.optionChangedRGBTop(event, {});
 		expect(res1).toBe(undefined);
 	}));
 
+	it('Should call the optionChangedRGBSide', fakeAsync(() => {
+		const event = { value: LightEffectComplexType.CPU_frequency };
+		component.lightingCapabilities = {
+			RGBfeature: LightEffectRGBFeature.Complex,
+			LightPanelType: [{}],
+			LedType_Complex: [{}],
+			lightInfo: [
+				{ lightPanelType: 32, lightEffectType: 2, lightColor: '55943D' },
+				{ lightPanelType: 64, lightEffectType: 2, lightColor: '4A9325' }
+			]
+		};
+		const res = component.optionChangedRGBSide(event, {});
+		expect(res).toBe(undefined);
+		tick(5);
+		gamingLightingServiceMock.isShellAvailable = true;
+		gamingLightingServiceMock.setLightingProfileEffectColor.and
+			.returnValue(Promise.resolve({ ...getLightingProfileById, didSuccess: false}));
+		event.value = LightEffectComplexType.Breath;
+		component.simpleOrComplex = LightEffectRGBFeature.Complex;
+		const res1 = component.optionChangedRGBSide(event, {});
+		expect(res1).toBe(undefined);
+	}));
+
 	it('should call the colorEffectChangedSide', fakeAsync(() => {
 		component.lightingCapabilities = { LightPanelType: [] };
-		getLightingProfileById.didSuccess = true;
 		gamingLightingServiceMock.setLightingProfileEffectColor.and
-			.returnValue(Promise.resolve(getLightingProfileById));
+			.returnValue(Promise.resolve({ ...getLightingProfileById, didSuccess: true}));
 		const res = component.colorEffectChangedSide('#fffff');
 		expect(res).toBe(undefined);
 	}));
 
 	it('should call the colorEffectChangedSide', fakeAsync(() => {
 		component.lightingCapabilities = { LightPanelType: [] };
-		getLightingProfileById.didSuccess = true;
 		gamingLightingServiceMock.setLightingProfileEffectColor.and
-			.returnValue(Promise.resolve(getLightingProfileById));
+			.returnValue(Promise.resolve({ ...getLightingProfileById, didSuccess: true}));
 		const res = component.colorEffectChangedFront('#fffff');
 		expect(res).toBe(undefined);
 	}));
 
 	it('should call the setLightingProfileId', fakeAsync(() => {
 		component.lightingCapabilities = { LightPanelType: [], LedType_Complex: [268435456, 1, 2, 4, 8, 32, 64, 128] };
-		getLightingProfileById.didSuccess = true;
 		gamingLightingServiceMock.setLightingProfileEffectColor.and
-			.returnValue(Promise.resolve(getLightingProfileById));
-		gamingLightingServiceMock.setLightingProfileId.and.returnValue(Promise.resolve({ profileId : 1, didSuccess: false}));
-		const res = component.setLightingProfileId({target: {value: 3}});
+			.returnValue(Promise.resolve({...getLightingProfileById, didSuccess: true}));
+		gamingLightingServiceMock.setLightingProfileId.and.returnValue(Promise.resolve({ profileId: 1, didSuccess: false }));
+		const res = component.setLightingProfileId({ target: { value: 3 } });
 		expect(res).toBe(undefined);
 	}));
 
 	it('should call the setLightingBrightness', fakeAsync(() => {
-		gamingLightingServiceMock.setLightingProfileBrightness.and.returnValue(Promise.resolve({didSuccess: true}));
+		gamingLightingServiceMock.setLightingProfileBrightness.and.returnValue(Promise.resolve({ didSuccess: true }));
 		const res = component.setLightingBrightness({ target: { value: 3 } });
 		expect(res).toBe(undefined);
 	}));
 
 	it('should call the changeSingleCoorEffect', fakeAsync(() => {
 		component.lightingCapabilities = { LightPanelType: [1], LedType_Complex: [268435456, 1, 2, 4, 8, 32, 64, 128] };
-
 		const res = component.changeSingleCoorEffect({});
+		expect(res).toBe(undefined);
+	}));
+
+	it('should call the getLightingProfileById with available shell', fakeAsync(() => {
+		component.lightingCapabilities = {
+			LightPanelType: [1],
+			RGBfeature: LightEffectRGBFeature.Simple, LedType_Complex: [268435456, 1, 2, 4, 8, 32, 64, 128]
+		};
+		gamingLightingServiceMock.getLightingProfileById.and.returnValue(Promise.resolve(getLightingProfileById));
+		gamingLightingServiceMock.isShellAvailable = true;
+		tick(5);
+		const res = component.getLightingProfileById(1);
+		expect(res).toBe(undefined);
+	}));
+
+	it('should call the getLightingProfileById with no shell available', fakeAsync(() => {
+		component.lightingCapabilities = {
+			LightPanelType: [1],
+			RGBfeature: LightEffectRGBFeature.Simple, LedType_Complex: [268435456, 1, 2, 4, 8, 32, 64, 128]
+		};
+		gamingLightingServiceMock.getLightingProfileById.and.returnValue(Promise.resolve({ ...getLightingProfileById, didSuccess: false }));
+		gamingLightingServiceMock.isShellAvailable = true;
+		tick(5);
+		const res = component.getLightingProfileById(1);
 		expect(res).toBe(undefined);
 	}));
 });
