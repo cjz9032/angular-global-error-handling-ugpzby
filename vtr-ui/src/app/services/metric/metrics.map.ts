6e6c0ae38a859269d2eafc01c060d0264ee32eb1
Mark Tao <taoxf1@lenovo.com>
2020-02-23 21:58:37 +0800

    VAN-14562 ItemParent is incorrect when click on the GiveFeedback button

--- vtr-ui/src/app/services/metric/metrics.map.ts
+++ vtr-ui/src/app/services/metric/metrics.map.ts
@@ -6,12 +6,19 @@ import { FeatureClick, ArticleClick, SettingUpdate } from './metrics.model';
 // Please add your eventId and metric type in the interface so that the static analysis tool can help you to check if you missing anything
 interface MetricsMap {
 	closeFeedbackEvent: FeatureClick;
+	giveFeedbacEvent: FeatureClick;
 }
 
+// if ItemParent is not provided, it would be filled with the pageName of the route
 export const  metricsMap: MetricsMap = {
 	closeFeedbackEvent: {
 		ItemType: EventName.FeatureClick,
 		ItemName: 'btn.close',
 		ItemParent: 'Dialog.Feedback'
+	},
+	giveFeedbacEvent: {
+		ItemType: EventName.FeatureClick,
+		ItemName: 'btn.givefeedback',
+		ItemParent: ''
 	}
 };
