f73bac6be1cd1aad2dfb9b53849e14a1960a0346
dongwenquan <dongwq2@lenovo.com>
2020-02-25 15:49:24 +0800

    bugfix(VAN-13035) using getMachineInfo instead of getDeviceInfo for performance improving.

--- vtr-ui/src/app/components/pages/page-device-settings/children/subpage-device-settings-display/subpage-device-settings-display.component.ts
+++ vtr-ui/src/app/components/pages/page-device-settings/children/subpage-device-settings-display/subpage-device-settings-display.component.ts
@@ -369,8 +369,8 @@ export class SubpageDeviceSettingsDisplayComponent implements OnInit, OnDestroy 
 			'671f1454b4101503f00ff4f786d44fa0',
 			'479a92a1dccaf9467d168ede8848faba',
 		];
-		return this.deviceService.getDeviceInfo()
-			.then(res => whitelist.includes(Md5.hashStr(res.bios.substr(0, 5)) as string));
+		return this.deviceService.getMachineInfo()
+			.then(res => whitelist.includes(Md5.hashStr(res.biosVersion.substr(0, 5)) as string));
 	}
 
 	async initCameraSection() {
