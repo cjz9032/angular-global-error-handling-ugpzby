13a9fac7715553b60f43ccfe27a2d3de5bb27668
Leo Liu <liuhy24@lenovo.com>
2020-02-25 15:56:27 +0800

    bugfix: dpm power tab was removed after switching to other tabs

--- vtr-ui/src/app/components/pages/page-device-settings/children/subpage-device-settings-power-dpm/subpage-device-settings-power-dpm.component.ts
+++ vtr-ui/src/app/components/pages/page-device-settings/children/subpage-device-settings-power-dpm/subpage-device-settings-power-dpm.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
 import { PowerDpmService } from 'src/app/services/power-dpm/power-dpm.service';
 import { Subscription } from 'rxjs';
+import { CommonService } from 'src/app/services/common/common.service';
+import { LocalStorageKey } from 'src/app/enums/local-storage-key.enum';
 
 @Component({
 	selector: 'vtr-subpage-device-settings-power-dpm',
@@ -12,10 +14,14 @@ export class SubpageDeviceSettingsPowerDpmComponent implements OnInit,OnDestroy 
 	allPowerPlansSubscription: Subscription;
 	public isLoading: boolean;
 
-	constructor(public dpmService: PowerDpmService) { }
+	constructor(
+		public dpmService: PowerDpmService,
+		public commonService:CommonService
+		) { }
 
 	ngOnInit() {
 		this.isLoading=true;
+		this.commonService.setLocalStorageValue(LocalStorageKey.IsPowerPageAvailable, true);
 		this.allPowerPlansSubscription = this.dpmService.getAllPowerPlansObs().subscribe(
 			v => {
 				if (v) {
