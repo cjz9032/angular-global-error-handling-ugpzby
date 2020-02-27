75e5582e31a6042e0d48af55c5912fcf619e8286
Daniel Swaroop <dannu@lenovo.com>
2020-02-27 17:06:51 +0530

    fix for VAN-13321, VAN-14746, VAN-15084

--- vtr-ui/src/app/data-models/camera/eyeCareMode.model.ts
+++ vtr-ui/src/app/data-models/camera/eyeCareMode.model.ts
@@ -8,7 +8,7 @@ export class EyeCareMode {
 export class SunsetToSunriseStatus {
 	constructor(
 		public available: boolean,
-		public status = false,
+		public status: boolean,
 		public permission: boolean,
 		public sunriseTime: string,
 		public sunsetTime: string
