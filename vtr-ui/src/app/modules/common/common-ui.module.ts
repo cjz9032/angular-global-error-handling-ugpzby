e9638cb430672bb0e0e0ddc6e9e70598cebf8c09
fuss5 <fuss5@lenovo.com>
2020-03-03 20:04:39 +0800

    dpm energy control

--- vtr-ui/src/app/modules/common/common-ui.module.ts
+++ vtr-ui/src/app/modules/common/common-ui.module.ts
@@ -40,6 +40,7 @@ import { faChevronDown as falChevronDown } from '@fortawesome/pro-light-svg-icon
 import { faChevronUp as falChevronUp } from '@fortawesome/pro-light-svg-icons/faChevronUp';
 import { UiDpmDropdownComponent } from 'src/app/components/ui/ui-dpm-dropdown/ui-dpm-dropdown.component';
 import { SpinnerComponent } from 'src/app/components/common/spinner/spinner.component';
+import { UiDpmEnergyComponent } from 'src/app/components/ui/ui-dpm-energy/ui-dpm-energy.component';
 
 
 @NgModule({
@@ -70,6 +71,7 @@ import { SpinnerComponent } from 'src/app/components/common/spinner/spinner.comp
 		UiToggleComponent,
 		ContainerCollapsibleComponent,
 		UiDpmDropdownComponent,
+		UiDpmEnergyComponent,
 		UiBrightnessSliderComponent,
 		UiAddReduceButtonComponent
 	],
@@ -101,6 +103,7 @@ import { SpinnerComponent } from 'src/app/components/common/spinner/spinner.comp
 		UiToggleComponent,
 		ContainerCollapsibleComponent,
 		UiDpmDropdownComponent,
+		UiDpmEnergyComponent,
 		UiBrightnessSliderComponent,
 		UiAddReduceButtonComponent
 	],
