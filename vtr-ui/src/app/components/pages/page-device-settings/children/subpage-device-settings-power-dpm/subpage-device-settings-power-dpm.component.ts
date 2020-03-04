867a96764561be99ea37000835193e4b25b4b901
Leo Liu <liuhy24@lenovo.com>
2020-03-04 16:00:22 +0800

    DPM remove loading circle, use default value

--- vtr-ui/src/app/components/pages/page-device-settings/children/subpage-device-settings-power-dpm/subpage-device-settings-power-dpm.component.ts
+++ vtr-ui/src/app/components/pages/page-device-settings/children/subpage-device-settings-power-dpm/subpage-device-settings-power-dpm.component.ts
@@ -11,30 +11,16 @@ import { LocalStorageKey } from 'src/app/enums/local-storage-key.enum';
 })
 export class SubpageDeviceSettingsPowerDpmComponent implements OnInit,OnDestroy {
 
-	allPowerPlansSubscription: Subscription;
-	public isLoading: boolean;
 
 	constructor(
-		public dpmService: PowerDpmService,
 		public commonService:CommonService
 		) { }
 
 	ngOnInit() {
-		this.isLoading=true;
 		this.commonService.setLocalStorageValue(LocalStorageKey.IsPowerPageAvailable, true);
-		this.allPowerPlansSubscription = this.dpmService.getAllPowerPlansObs().subscribe(
-			v => {
-				if (v) {
-					this.isLoading=false;
-					this.allPowerPlansSubscription.unsubscribe();
-				}
-			}
-		);
 	}
 
 	ngOnDestroy(): void {
-		if (this.allPowerPlansSubscription) {
-			this.allPowerPlansSubscription.unsubscribe();
-		}
+
 	}
 }
