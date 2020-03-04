e116483a51e6db2895fdad716dddf176320c7160
Henry.Wei <weizm1@lenovo.com>
2020-03-04 17:16:50 +0800

    code review action

--- vtr-ui/prebuild/detectConsole.js
+++ vtr-ui/prebuild/detectConsole.js
@@ -1,5 +1,5 @@
 const { exec } = require('child_process');
-const cmd = 'node node_modules/jscodeshift/bin/jscodeshift.js -t prebuild/removeConsole.js --extensions=ts --parser=ts src -d --ignore-pattern=src/**/logger.service.ts';
+const cmd = 'npm run remove-console -- -d';
 exec(cmd, function(error, stdout, stderr) {
     if (stderr || error) {
         console.log(stderr);
