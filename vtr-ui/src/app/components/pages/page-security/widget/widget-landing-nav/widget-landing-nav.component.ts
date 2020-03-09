dc211641ad7ee5d42513a43aae5c0d4304d6ef12
Gemma Wu <wuxia8@lenovo.com>
2020-03-09 15:48:22 +0800

    VAN-15516: fix only show basic security tab on Think station P920 machine

--- vtr-ui/src/app/components/pages/page-security/widget/widget-landing-nav/widget-landing-nav.component.ts
+++ vtr-ui/src/app/components/pages/page-security/widget/widget-landing-nav/widget-landing-nav.component.ts
@@ -1,14 +1,11 @@
-import { Component, OnInit, Input, Output, EventEmitter, OnChanges } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 
 @Component({
 	selector: 'vtr-widget-landing-nav',
 	templateUrl: './widget-landing-nav.component.html',
 	styleUrls: ['./widget-landing-nav.component.scss']
 })
-export class WidgetLandingNavComponent implements OnInit, OnChanges {
-	@Input() baseItems;
-	@Input() intermediateItems;
-	@Input() advancedItems;
+export class WidgetLandingNavComponent implements OnInit {
 	@Output() haveOwnChecked = new EventEmitter<any>();
 	@Output() retryClick = new EventEmitter<any>();
 	currentPage = 'base-security';
@@ -16,31 +13,60 @@ export class WidgetLandingNavComponent implements OnInit, OnChanges {
 		{
 			title: 'security.landing.basicSecurity',
 			page: 'base-security',
-			item: this.baseItems,
-			display: true
+			item: this.baseItems
 		},
 		{
 			title: 'security.landing.intermediateSecurity',
 			page: 'intermediate-security',
-			item: this.intermediateItems,
-			display: true
+			item: this.intermediateItems
 		},
 		{
 			title: 'security.landing.advancedSecurity',
 			page: 'advanced-security',
-			item: this.advancedItems,
-			display: true
+			item: this.advancedItems
 		},
 	];
+
+	private _baseItems;
+	private _intermediateItems;
+	private _advancedItems;
+
 	constructor() { }
 
 	ngOnInit() {
 	}
 
-	ngOnChanges(): void {
-		this.featurePagesConfig[0].item = this.baseItems;
-		this.featurePagesConfig[1].item = this.intermediateItems;
-		this.featurePagesConfig[2].item = this.advancedItems;
+	@Input() set baseItems(itemValue: any) {
+		if (itemValue) {
+			this.featurePagesConfig[0].item = itemValue;
+			this._baseItems = itemValue;
+		}
+	}
+
+	get baseItems() {
+		return this._baseItems;
+	}
+
+	@Input() set intermediateItems(itemValue: any) {
+		if (itemValue) {
+			this.featurePagesConfig[1].item = itemValue;
+			this._intermediateItems = itemValue;
+		}
+	}
+
+	get intermediateItems() {
+		return this._intermediateItems;
+	}
+
+	@Input() set advancedItems(itemValue: any) {
+		if (itemValue) {
+			this.featurePagesConfig[2].item = itemValue;
+			this._advancedItems = itemValue;
+		}
+	}
+
+	get advancedItems() {
+		return this._advancedItems;
 	}
 
 	changePage(page) {
