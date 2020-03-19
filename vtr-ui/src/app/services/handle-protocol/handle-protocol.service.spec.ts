b9b940768f5def8dcc870c875df2cff327e95c2a
Henry.Wei <weizm1@lenovo.com>
2020-03-19 15:12:46 +0800

    finish code to convert the protocol

--- vtr-ui/src/app/services/handle-protocol/handle-protocol.service.spec.ts
+++ vtr-ui/src/app/services/handle-protocol/handle-protocol.service.spec.ts
@@ -14,6 +14,48 @@ describe('HandleProtocolService', () => {
     expect(service).toBeTruthy();
   });
 
+  it('initializeUrl launch by protocol', () => {
+	window.location.href = 'https://vantage.csw.lenovo.com/#/?protocol=bGVub3ZvLXZhbnRhZ2UzOmluc3RhbGxmaWdsZWFm';
+	spyOn(service, 'processUrl').and.returnValue('https://vantage.csw.lenovo.com/#/security/wifi-security');
+	service.initializeUrl();
+    expect(window.location.href).toEqual('https://vantage.csw.lenovo.com/#/security/wifi-security');
+  });
+
+  it('initializeUrl launch by normal url', () => {
+	window.location.href = 'https://vantage.csw.lenovo.com/#/dashboard';
+	spyOn(service, 'processUrl').and.returnValue('https://vantage.csw.lenovo.com/#/dashboard');
+	service.initializeUrl();
+    expect(window.location.href).toEqual('https://vantage.csw.lenovo.com/#/dashboard');
+  });
+
+  it('processUrl with invalid url', () => {
+	const url = 'this-is-a-invalid-url';
+	expect(service.processUrl(url)).toEqual('this-is-a-invalid-url');
+  });
+
+  it('processUrl with non-protocol url', () => {
+	const url = 'https://vantage.csw.lenovo.com/#/security/wifi-security';
+	expect(service.processUrl(url)).toEqual('https://vantage.csw.lenovo.com/#/security/wifi-security');
+  });
+
+  it('processUrl with invalid base64', () => {
+	const url = 'https://vantage.csw.lenovo.com/#/?protocol=1';
+	expect(service.processUrl(url)).toEqual('https://vantage.csw.lenovo.com/#/?protocol=1');
+  });
+
+  it('processUrl with old protocol', () => {
+	const url = 'https://vantage.csw.lenovo.com/#/?protocol=bGVub3ZvLWNvbXBhbmlvbjo/ZmVhdHVyZUlkPTZGNDg2Q0Y1LTVENTEtNEFFOC1BQkE5LTA4OUI1Q0I5NjQyMA==';
+	spyOn(service, 'convertToUrlAssumeProtocolIs3x').and.returnValue('');
+	spyOn(service, 'convertToUrlAssumeProtocolIs2x').and.returnValue('security/wifi-security');
+	expect(service.processUrl(url)).toEqual('https://vantage.csw.lenovo.com/#/security/wifi-security');
+  });
+
+  it('processUrl with new protocol', () => {
+	const url = 'https://vantage.csw.lenovo.com/#/?protocol=bGVub3ZvLXZhbnRhZ2UzOmRldmljZS1zZXR0aW5ncw==';
+	spyOn(service, 'convertToUrlAssumeProtocolIs3x').and.returnValue('lenovo-vantage3:device-settings');
+	expect(service.processUrl(url)).toEqual('https://vantage.csw.lenovo.com/#/device/device-settings/power');
+  });
+
   it('decode base64 string short', () => {
 	const base64str = 'bGVub3ZvLXZhbnRhZ2UzOnBhc3N3b3JkLXByb3RlY3Rpb24=';
     expect(service.decodeBase64String(base64str)).toEqual('lenovo-vantage3:password-protection');
@@ -24,43 +66,58 @@ describe('HandleProtocolService', () => {
     expect(service.decodeBase64String(base64str)).toEqual('lenovo-companion:PARAM?featureId=6F486CF5-5D51-4AE8-ABA9-089B5CB96420&amp;notificationId=C702B2E1-34BA-4604-92F7-88DC0D1954A7&amp;buttonClicked=LearnMore&amp;plugin=LenovoWiFiSecurityPlugin&amp;choice=open&amp;msgName=Companion.WiFiSecurity.PromoteToEnable2');
   });
 
-  it('Is valid 3.x protocol', () => {
+  it('convertToUrlAssumeProtocolIs3x valid 3.x protocol', () => {
 	const protocol = 'lenovo-vantage3:password-protection';
-    expect(service.isValidVantage3xProtocol(protocol)).toBeTruthy();
+    expect(service.convertToUrlAssumeProtocolIs3x(protocol)).toEqual('security/password-protection');
   });
 
-  it('Is not valid 3.x protocol', () => {
-	const protocol = 'lenovo-vantage3:password-protection-not-exist';
-    expect(service.isValidVantage3xProtocol(protocol)).toBeFalsy();
+  it('convertToUrlAssumeProtocolIs3x invalid 3.x like protocol', () => {
+	const protocol = 'lenovo-vantage3:not-exist';
+    expect(service.convertToUrlAssumeProtocolIs3x(protocol)).toEqual('');
   });
 
-  it('Is not valid 3.x protocol', () => {
-	const protocol = 'lenovo-metro-companion:password-protection';
-    expect(service.isValidVantage3xProtocol(protocol)).toBeFalsy();
+  it('convertToUrlAssumeProtocolIs3x empty args', () => {
+	const protocol = '';
+    expect(service.convertToUrlAssumeProtocolIs3x(protocol)).toEqual('');
   });
 
-  it('Is not valid 3.x protocol', () => {
-	const protocol = 'lenovo-metro-companion:?featureId=6F486CF5-5D51-4AE8-ABA9-089B5CB96420';
-    expect(service.isValidVantage3xProtocol(protocol)).toBeFalsy();
+  it('convertToUrlAssumeProtocolIs3x valid 2.x protocol', () => {
+	const protocol = 'lenovo-companion:PARAM?section=input';
+    expect(service.convertToUrlAssumeProtocolIs3x(protocol)).toEqual('');
   });
 
-  it('Is valid old protocol', () => {
-	const protocol = 'lenovo-metro-companion:?featureId=6F486CF5-5D51-4AE8-ABA9-089B5CB96420';
-    expect(service.isValidVantage2xProtocol(protocol)).toBeTruthy();
+  it('convertToUrlAssumeProtocolIs3x non-url string', () => {
+	const protocol = 'general-string';
+    expect(service.convertToUrlAssumeProtocolIs3x(protocol)).toEqual('');
   });
 
-  it('Is not valid old protocol', () => {
+  it('convertToUrlAssumeProtocolIs2x valid 3.x protocol', () => {
 	const protocol = 'lenovo-vantage3:password-protection';
-    expect(service.isValidVantage2xProtocol(protocol)).toBeFalsy();
+    expect(service.convertToUrlAssumeProtocolIs2x(protocol)).toEqual('');
   });
 
-  it('Is not valid old protocol', () => {
-	const protocol = 'lenovo-vantage3:?featureId=6F486CF5-5D51-4AE8-ABA9-089B5CB96420';
-    expect(service.isValidVantage2xProtocol(protocol)).toBeFalsy();
+  it('convertToUrlAssumeProtocolIs2x invalid 3.x like protocol', () => {
+	const protocol = 'lenovo-vantage3:not-exist';
+    expect(service.convertToUrlAssumeProtocolIs2x(protocol)).toEqual('');
   });
 
-  it('Is not valid old protocol', () => {
-	const protocol = 'lenovo-vantage3:password-protection';
-    expect(service.isValidVantage2xProtocol(protocol)).toBeFalsy();
+  it('convertToUrlAssumeProtocolIs2x empty args', () => {
+	const protocol = '';
+    expect(service.convertToUrlAssumeProtocolIs2x(protocol)).toEqual('');
+  });
+
+  it('convertToUrlAssumeProtocolIs2x valid 2.x protocol', () => {
+	const protocol = 'lenovo-companion:PARAM?section=input';
+    expect(service.convertToUrlAssumeProtocolIs2x(protocol)).toEqual('device/device-settings/input-accessories');
+  });
+
+  it('convertToUrlAssumeProtocolIs2x invalid 2.x protocol', () => {
+	const protocol = 'lenovo-companion:PARAM?section=not-exist';
+    expect(service.convertToUrlAssumeProtocolIs2x(protocol)).toEqual('');
+  });
+
+  it('convertToUrlAssumeProtocolIs2x non-url string', () => {
+	const protocol = 'general-string';
+    expect(service.convertToUrlAssumeProtocolIs2x(protocol)).toEqual('');
   });
 });
--- vtr-ui/src/app/services/handle-protocol/handle-protocol.service.ts
+++ vtr-ui/src/app/services/handle-protocol/handle-protocol.service.ts
@@ -1,28 +1,164 @@
-import { Injectable, Injector } from '@angular/core';
-import { URL } from 'url';
-import { Router } from '@angular/router';
+import { Injectable } from '@angular/core';
 
+@Injectable({
+  providedIn: 'root'
+})
 export class HandleProtocolService {
+  vantage3xSchema = 'lenovo-vantage3:';
+  semanticToPath: { [semantic: string]: string } = {
+	'dashboard': '',
+	'device': 'device',
+	'device-settings': 'device/device-settings/power',
+	'system-updates': 'device/system-updates',
+	'security': 'security',
+	'anti-virus': 'security/anti-virus',
+	'wifi-security': 'security/wifi-security',
+	'password-protection': 'security/password-protection',
+	'internet-protection': 'security/internet-protection',
+	'windows-hello': 'security/windows-hello',
+	'support': 'support',
+	'power': 'device/device-settings/power',
+	'display-camera': 'device/device-settings/display-camera',
+	'audio': 'device/device-settings/audio',
+	'input': 'device/device-settings/input-accessories',
+	'smart-settings': 'device/smart-assist',
+	'home-security': 'home-security',
+	'modern-preload': '?action=modernpreload',
+	'toolbar': '',
+	'preference': 'settings',
+	'gaming-autoclose': 'gaming/autoclose',
+	'gaming-networkboost': 'gaming/networkboost',
+	'gaming-macrokey': 'gaming/macrokey',
+	'gaming-lighting0': 'gaming/lightingcustomize/0',
+	'gaming-lighting1': 'gaming/lightingcustomize/1',
+	'gaming-lighting2': 'gaming/lightingcustomize/2',
+	'gaming-lighting3': 'gaming/lightingcustomize/3'
+  }
+
+  backwardCompatibilitySchemas = [
+	'lenovo-metro-discovery:',
+	'lenovo-metro-companion:',
+	'lenovo-companion:'
+  ];
+
+  sectionToSemantic: { [section: string]: string } = {
+	'powersection': 'power',
+	'multimedia': 'display-camera',
+	'input': 'input',
+	'multimodesection': 'smart-settings'
+  }
+
+  featureIdToSemantic: { [featureId: string]: string } = {
+	'5fbdca5f-02ca-4159-8f1c-725703e31473': 'power',
+	'4efe8c3d-db66-4f91-87fc-31e9aa1cbadf': 'display-camera',
+	'd0ff2f49-ca94-4dd7-a30f-d3d950d5e720': 'input',
+	'9c37804e-6043-4f81-80bb-c4ffbb61ab12': 'smart-settings',
+	'6f486cf5-5d51-4ae8-aba9-089b5cb96420': 'wifi-security',
+	'2885591f-f5a8-477a-9744-d1b9f30b5b79': 'wifi-security',
+	'a191bf9f-60be-4843-b4ba-441dd0aeb12e': 'support',
+	'883b56c4-1348-4478-ab2e-a0909dd121c8': 'support',
+	'e40b12ce-c5dd-4571-bbc6-7ea5879a8472': 'system-updates',
+	'cd300e9b-e1b9-44eb-9634-a91bb32faf79': 'display-camera',
+	'19841a14-32b9-4f67-9d3a-605ee6cef187': 'wifi-security',
+	'5e5800d8-e4ea-4cb0-b628-68d1bdae8622': 'dashboard',
+	'18e12fc0-eacb-43cb-8231-87d9c09ee0df': 'support',
+	'f45a1a5c-44eb-42c3-b361-025ed702dd7c': 'modern-preload',
+  }
 
   constructor() { }
 
   public initializeUrl() {
+	const url = this.processUrl(window.location.href);
+	if (url !== window.location.href) {
+		window.location.href = url;
+	}
   }
 
   public decodeBase64String(args: string) {
-	return atob(args);
+	try {
+		return atob(args);
+	} catch(e) {
+		return '';
+	}
   }
 
-  private parseParam(uri: URL) {
-
+  public constructURL(args: string) : URL | undefined {
+	try {
+		return new URL(args);
+	} catch(e) {
+		return undefined;
+	}
   }
 
-  private isValidVantage3xProtocol() {
+  public processUrl(args: string) : string {
+	let url = this.constructURL(args);
+	if (!url) return args;
 
+	let hash = url.hash;
+	let characteristicCode = '#/?protocol=';
+	if (!hash.startsWith(characteristicCode)) return args;
+
+	let origin = url.origin;
+	let pathname = url.pathname;
+	let homePageUrl = `${origin}${pathname}#/`;
+	let encodedProtocol = hash.slice(hash.indexOf(characteristicCode) + characteristicCode.length);
+
+	let originalProtocol = this.decodeBase64String(encodedProtocol);
+	if (!originalProtocol) return homePageUrl;
+
+	let newUrl = this.convertToUrlAssumeProtocolIs3x(originalProtocol);
+	if (!newUrl) {
+		newUrl = this.convertToUrlAssumeProtocolIs2x(originalProtocol);
+	}
+	if (!newUrl) return homePageUrl;
+
+	return `${homePageUrl}${newUrl}`;
   }
 
-  private isValidVantage2xProtocol() {
+  public convertToUrlAssumeProtocolIs3x(rawData: string) : string {
+	let url = this.constructURL(rawData);
+	if (!url) return '';
+	const schema = url.protocol;
+	const semantic = url.pathname;
+	const query = url.search;
 
+	if (!schema || schema !== this.vantage3xSchema || !semantic) return '';
+
+	const path: string | undefined = this.semanticToPath[semantic];
+	if (!path) return '';
+
+	return `${path}${query}`;
+  }
+
+  public convertToUrlAssumeProtocolIs2x(rawData: string) : string {
+	let url = this.constructURL(rawData);
+	if (!url) return '';
+	const schema = url.protocol;
+	const pathName = url.pathname;
+	const query = url.search;
+	const queryParams = url.searchParams;
+
+	if (!schema || !this.backwardCompatibilitySchemas.includes(schema) || pathName !== 'PARAM') return '';
+
+	const featureId: string | null = queryParams.get('featureId');
+	if (featureId) {
+		const featureSemantic: string | undefined = this.featureIdToSemantic[featureId];
+		if (featureSemantic) {
+			const path: string | undefined = this.semanticToPath[featureSemantic];
+			if (path) return `${path}${query}`;
+		}
+	}
+
+	const section: string | null = queryParams.get('section');
+	if (section) {
+		const sectionSemantic: string | undefined = this.sectionToSemantic[section];
+		if (sectionSemantic) {
+			const path = this.semanticToPath[sectionSemantic];
+			if (path) return `${path}${query}`;
+		}
+	}
+
+	return '';
   }
 }
 
