9dfa1ab1e1c260fe5e732d1dd7ad80ab2759405a
raj <rsahu2@lenovo.com>
2020-04-01 18:34:21 +0530

    fixed build errors

--- vtr-ui/src/app/components/ui/ui-custom-slider/ui-custom-slider.component.ts
+++ vtr-ui/src/app/components/ui/ui-custom-slider/ui-custom-slider.component.ts
@@ -15,15 +15,15 @@ export class UiCustomSliderComponent implements OnInit {
 	@Input() isDisabled = false;
 	@Input() sliderId = 'rangeSlider';
 	@Input() value = 0; // initial slider value
-	@Input() min = 0; // slider minimum end value
-	@Input() max = 10; // slider maximum end value
+	@Input() min = 0; // slider start/minimum value
+	@Input() max = 10; // slider end/maximum value
 	@Input() step = 1; // ticks or steps to change on each slide
 	@Input() minLegend = ''; // label to display at the start of slider
 	@Input() midLegend = ''; // label to display at the center of slider
 	@Input() maxLegend = ''; // label to display at the end of slider
 
-	@Output() sliderChange: any = new EventEmitter();
-	@Output() valueChanged: any = new EventEmitter();
+	@Output() sliderChange: EventEmitter<number> = new EventEmitter();
+	@Output() valueChanged: EventEmitter<number> = new EventEmitter();
 
 	constructor() { }
 
@@ -31,17 +31,17 @@ export class UiCustomSliderComponent implements OnInit {
 
 	/**
 	 * This event is fired after mouse is released after dragging the slider or by keyboard.
-	 * @param value currently selected value
+	 * @param $event currently selected value
 	 */
-	public onValueChange(value: number) {
-		this.valueChanged.emit(value);
+	public onValueChange($event: any) {
+		this.valueChanged.emit($event.target.value);
 	}
 
 	/**
 	 *  This event is fired when user changes slider value by dragging or by keyboard
-	 * @param value currently selected value
+	 * @param $event currently selected value
 	 */
-	public onInputChange(value: number) {
-		this.sliderChange.emit(value);
+	public onInputChange($event: any) {
+		this.sliderChange.emit($event.target.value);
 	}
 }
