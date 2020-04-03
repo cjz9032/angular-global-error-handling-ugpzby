6fd72ad62a712ae3cd68b3afa620f2a9e8473e1b
lixd19 <lixd19@lenovo.com>
2020-04-03 14:47:15 +0800

    VAN-16062: finish UI

--- vtr-ui/src/app/components/modal/modal-store-rating/modal-store-rating.component.ts
+++ vtr-ui/src/app/components/modal/modal-store-rating/modal-store-rating.component.ts
@@ -1,10 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
-import { faSmileBeam } from '@fortawesome/pro-light-svg-icons/faSmileBeam';
-import { faFrown } from '@fortawesome/pro-light-svg-icons/faFrown';
-import { faTimesCircle } from '@fortawesome/pro-light-svg-icons/faTimesCircle';
+import { faTimes } from '@fortawesome/pro-light-svg-icons/faTimes';
 import { LoggerService } from 'src/app/services/logger/logger.service';
-
+import { MetricService } from 'src/app/services/metric/metric.service';
+import { TaskAction } from 'src/app/services/metric/metrics.model';
+import { faHeart } from '@fortawesome/free-solid-svg-icons/faHeart';
+import { faEllipsisH } from '@fortawesome/pro-light-svg-icons/faEllipsisH';
 @Component({
 	selector: 'vtr-modal-store-rating',
 	templateUrl: './modal-store-rating.component.html',
@@ -12,14 +13,18 @@ import { LoggerService } from 'src/app/services/logger/logger.service';
 })
 export class ModalStoreRatingComponent implements OnInit {
 
-	public faSmileBeam = faSmileBeam;
-	public faFrown = faFrown;
-	public faTimesCircle = faTimesCircle;
+	public faTimesCircle = faTimes;
+	public faHeart = faHeart;
+	public faEllipsisH = faEllipsisH;
 	private msStoreUtil: any = null;
+	public redHeartHintVisible = false;
+	public closeHintVisible = false;
+	public elilipsisHintVisible = true;
 
 	constructor(
 		public activeModal: NgbActiveModal,
 		private logger: LoggerService,
+		private metrics: MetricService,
 	) {
 		const win = window as any;
 		if (win.VantageShellExtension) {
@@ -36,29 +41,89 @@ export class ModalStoreRatingComponent implements OnInit {
 
 	public async onBtnLikeClicked($event) {
 		this.activeModal.close(true);
-
-		if (this.msStoreUtil && this.msStoreUtil.isMSStoreRatingSupported()) {
-			this.logger.info('Launch MS rating without leaving Vantage.');
-			const ret = await this.msStoreUtil.launchMSStoreRatingAsync();
-			if (ret) {
-				return;
+		if (this.msStoreUtil) {
+			if (this.msStoreUtil.isMSStoreRatingSupported()) {
+				this.logger.info('Launch MS rating without leaving Vantage.');
+				const ret = await this.msStoreUtil.launchMSStoreRatingAsync();
+				if (ret === 'failed') {
+					this.logger.info(`Launch MS rating failed,Launch ms store to rate Vantage.`);
+					await this.msStoreUtil.launchMSStoreToVantageAsync();
+				}
+				this.sendMSRatingResultMetrics(ret);
+			}
+			else {
+				this.logger.info(`MS store rating not support, launch ms store to rate Vantage.`);
+				await this.msStoreUtil.launchMSStoreToVantageAsync();
 			}
 		}
-		this.logger.info(`Launch ms store to rate Vantage.`);
-		await this.msStoreUtil.launchMSStoreToVantageAsync();
 	}
 
 	public async onBtnDislikeClicked($event) {
 		this.activeModal.close(false);
-
-		if (this.msStoreUtil && this.msStoreUtil.isFeedbackHubSupported()) {
-			const ret = await this.msStoreUtil.launchMSFeedback();
-			if (ret) {
-				return;
+		if (this.msStoreUtil) {
+			if (this.msStoreUtil.isFeedbackHubSupported()) {
+				const ret = await this.msStoreUtil.launchMSFeedback();
+				if (!ret) {
+					this.logger.info(`MS Feedback launch failed,Launch mail to comment Vantage.`);
+					await this.msStoreUtil.emailToLenovo();
+					return;
+				}
+				this.sendMsFeedbackLaunchedMetrics();
+			} else {
+				this.logger.info(`MS Feedback is not supported,Launch mail to comment Vantage.`);
+				await this.msStoreUtil.emailToLenovo();
+				this.sendDoEmailToLenovoMetrics();
 			}
 		}
-		this.logger.info(`Launch mail to comment Vantage.`);
-		await this.msStoreUtil.emailToLenovo();
+	}
+
+	public onBtnMouseEnter(arg) {
+		if (arg === 'like') {
+			this.showRedHeart();
+		} else {
+			this.showCloseHint();
+		}
+	}
+
+	public onBtnMouseLeave() {
+		this.showEllipsis();
+	}
+
+	private sendMSRatingResultMetrics(result: string) {
+		const taskinfo = new TaskAction('StorRating-MSStoreRating', 1, '', result, 0);
+		this.metrics.sendMetrics(taskinfo);
+	}
+
+	private sendMsFeedbackLaunchedMetrics() {
+		const taskinfo = new TaskAction('StorRating-LaunchMSFeedback', 1, '', 'success', 0);
+		this.metrics.sendMetrics(taskinfo);
+	}
+
+	private sendDoEmailToLenovoMetrics() {
+		const taskinfo = new TaskAction('StorRating-LaunchEmailToLenovo', 1, '', 'success', 0);
+		this.metrics.sendMetrics(taskinfo);
+	}
+
+
+	private resetHint() {
+		this.closeHintVisible = false;
+		this.redHeartHintVisible = false;
+		this.elilipsisHintVisible = false;
+	}
+
+	private showRedHeart() {
+		this.resetHint();
+		this.redHeartHintVisible = true;
+	}
+
+	private showCloseHint() {
+		this.resetHint();
+		this.closeHintVisible = true;
+	}
+
+	private showEllipsis() {
+		this.resetHint();
+		this.elilipsisHintVisible = true;
 	}
 
 }
