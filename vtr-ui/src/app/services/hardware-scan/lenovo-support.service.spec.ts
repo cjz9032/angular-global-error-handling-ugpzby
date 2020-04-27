aeb6351945adf3de96d8c5e65fd45096e23abcd6
Valmir Cunha <vcunha@lenovo.com>
2020-04-27 16:12:59 -0300

    VAN-17117: Starting LenovoSupportService implementation

---
+++ vtr-ui/src/app/services/hardware-scan/lenovo-support.service.spec.ts
@@ -0,0 +1,16 @@
+import { TestBed } from '@angular/core/testing';
+
+import { LenovoSupportService } from './lenovo-support.service';
+
+describe('LenovoSupportService', () => {
+  let service: LenovoSupportService;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({});
+    service = TestBed.inject(LenovoSupportService);
+  });
+
+  it('should be created', () => {
+    expect(service).toBeTruthy();
+  });
+});
