fe970b4cc02816f323722f176ffe7ef28a0b7f75
GuoJing <guojing14@lenovo.com>
2020-04-27 10:08:06 +0800

    unit test for gaming-accessory.service

--- vtr-ui/src/app/services/gaming/gaming-accessory/gaming-accessory.service.spec.ts
+++ vtr-ui/src/app/services/gaming/gaming-accessory/gaming-accessory.service.spec.ts
@@ -1,9 +1,10 @@
-import { TestBed } from '@angular/core/testing';
+import { TestBed, async } from '@angular/core/testing';
 import { HttpClientModule } from '@angular/common/http';
 
 import { GamingAccessoryService } from './gaming-accessory.service';
 import { VantageShellService } from '../../vantage-shell/vantage-shell.service';
 import { LoggerService } from '../../logger/logger.service';
+import { WinRT } from '@lenovo/tan-client-bridge';
 
 describe('GamingAccessoryService', () => {
 
@@ -12,9 +13,10 @@ describe('GamingAccessoryService', () => {
 
   describe('isShellAvailable is false', () => {
     beforeEach(() => {
-      let spy = jasmine.createSpyObj('VantageService', ['getGamingAccessory', 'getLogger']);
+      let spy = jasmine.createSpyObj('VantageService', ['getGamingAccessory', 'getRegistryUtil', 'getSystemUpdate', 'getLogger']);
       spy.getGamingAccessory.and.returnValue(undefined);
-
+      spy.getRegistryUtil.and.returnValue(undefined);
+      spy.getSystemUpdate.and.returnValue(undefined);
       TestBed.configureTestingModule({
         imports: [
           HttpClientModule
@@ -22,7 +24,7 @@ describe('GamingAccessoryService', () => {
         providers: [
           GamingAccessoryService,
           LoggerService,
-          { provide: VantageShellService, useValue: spy }
+          { provide: VantageShellService, useValue: spy },
         ]
       });
 
@@ -39,26 +41,42 @@ describe('GamingAccessoryService', () => {
       expect(shellService.getGamingAccessory()).toBeUndefined();
     });
 
+    it('shellService.getRegistryUtil() should return undefined', () => {
+      expect(shellService.getRegistryUtil()).toBeUndefined();
+    });
+
+    it('shellService.getSystemUpdate() should return undefined', () => {
+      expect(shellService.getSystemUpdate()).toBeUndefined();
+    });
+
     it('isShellAvailable is false', () => {
       expect(gamingAccessoryService.isShellAvailable).toBe(false);
     });
 
-    it('launchAccessory should return undefined', () => {
+    it('isLACSupportUriProtocol should return undefinde', () => {
+      expect(gamingAccessoryService.isLACSupportUriProtocol()).toBeUndefined();
+    });
+
+    it('launchAccessory should return undefinde', () => {
       expect(gamingAccessoryService.launchAccessory()).toBeUndefined();
-    })
+    });
+
   });
-  xdescribe('isShellAvailable is true', () => {
-    let launchReturnValue = false;
+  describe('isShellAvailable is true', () => {
+    let stubRes = {
+      keyList: [1, 2]
+    };
     beforeEach(() => {
-      let spy = jasmine.createSpyObj('VantageService', ['getGamingAccessory', 'getLogger']);
-      let stubValue = {
-        setLaunch() {
-            return new Promise( resolve => {
-                resolve(launchReturnValue);
-            })
+      let spy = jasmine.createSpyObj('VantageService', ['getGamingAccessory', 'getRegistryUtil', 'getSystemUpdate', 'getLogger']);
+      let stubRegValue = {
+        queryValue(regPath: string) {
+          return new Promise(resolve => {
+            resolve(stubRes);
+          })
         }
       };
-      spy.getGamingAccessory.and.returnValue(stubValue);
+      spy.getGamingAccessory.and.returnValue(new Promise(resolve => { resolve(true) }));
+      spy.getRegistryUtil.and.returnValue(stubRegValue);
 
       TestBed.configureTestingModule({
         imports: [
@@ -80,22 +98,94 @@ describe('GamingAccessoryService', () => {
       expect(shellService).toBeTruthy('can not inject shellService');
     });
 
-    it('isShellAvailable is false', () => {
+    it('isShellAvailable is true', () => {
       expect(gamingAccessoryService.isShellAvailable).toBe(true);
     });
 
-    it('Accessory installed: launch success and return true', () => {
-      launchReturnValue = true;
-      gamingAccessoryService.launchAccessory().then( res => {
-          expect(res).toBe(true, 'launchAccessory should return true');
-      });
-    })
-
-    it('Accessory uninstalled: launch falil and return false', () => {
-      launchReturnValue = false;
-      gamingAccessoryService.launchAccessory().then( res => {
-          expect(res).toBe(false, 'launchAccessory should return false');
+    it('register existed: isLACSupportUriProtocol should return true', async(() => {
+      stubRes.keyList = [1];
+      gamingAccessoryService.isLACSupportUriProtocol().then(res => {
+        expect(res).toBe(true, 'isLACSupportUriProtocol should return true');
       })
-    })
+    }));
+
+    it('register existed: launch successed, should return false', async(() => {
+      stubRes.keyList = [1];
+      spyOn(WinRT, 'launchUri').and.returnValue(true)
+      gamingAccessoryService.launchAccessory().then(res => {
+        expect(res).toBe(true, 'launchAccessory should return true');
+      })
+    }));
+
+    it('register existed: launch fail, should return false', async(() => {
+      stubRes.keyList = [1];
+      spyOn(WinRT, 'launchUri').and.returnValue(false)
+      gamingAccessoryService.launchAccessory().then(res => {
+        expect(res).toBe(false, 'launchAccessory should return false');
+      })
+    }));
+
+    it('register unexisted: isLACSupportUriProtocolshould should return false', async(() => {
+      stubRes.keyList = [];
+      gamingAccessoryService.isLACSupportUriProtocol().then(res => {
+        expect(res).toBe(false, 'isLACSupportUriProtocol should return false');
+      })
+    }));
+
+    it('register unexisted: stop launch and return false', async(() => {
+      stubRes.keyList = [];
+      gamingAccessoryService.launchAccessory().then(res => {
+        expect(res).toBe(false, 'launchAccessory should return false');
+      })
+    }));
+  });
+  describe('catch error', () => {
+    beforeEach(() => {
+      let spy = jasmine.createSpyObj('VantageService', ['getGamingAccessory', 'getRegistryUtil', 'getSystemUpdate', 'getLogger']);
+      let stubRegValue = {
+        queryValue(regPath: string) {
+          throw new Error('queryValue error');
+        }
+      };
+      spy.getGamingAccessory.and.returnValue(new Promise(resolve => { resolve(true) }));
+      spy.getRegistryUtil.and.returnValue(stubRegValue);
+
+      TestBed.configureTestingModule({
+        imports: [
+          HttpClientModule
+        ],
+        providers: [
+          GamingAccessoryService,
+          LoggerService,
+          { provide: VantageShellService, useValue: spy }
+        ]
+      });
+
+      gamingAccessoryService = TestBed.inject(GamingAccessoryService);
+      shellService = TestBed.get(VantageShellService);
+    });
+
+    it('inject gamingAccessoryService & shellService', () => {
+      expect(gamingAccessoryService).toBeTruthy('can not inject gamingOCService');
+      expect(shellService).toBeTruthy('can not inject shellService');
+    });
+
+    it('isShellAvailable is true', () => {
+      expect(gamingAccessoryService.isShellAvailable).toBe(true);
+    });
+
+    it('isLACSupportUriProtocol should return err', async(() => {
+      gamingAccessoryService.isLACSupportUriProtocol().then().catch(err => {
+        expect(err).toMatch('queryValue error');
+      });
+    }));
+
+    // a problem about async call a async
+    xit('launchAccessory should return err', async(() => {
+      spyOn(WinRT, 'launchUri').and.throwError('WinRT.launchUri error');
+      spyOn(gamingAccessoryService, 'isLACSupportUriProtocol').and.returnValue(new Promise(resolve => { resolve(true) }));
+      gamingAccessoryService.launchAccessory();
+    }));
+
   });
 });
