f65e5ec07ad79df4c85c6b684774a9a49366003c
Weihong WH1 Qian <qianwh1@lenovo.com>
2020-04-29 20:51:37 +0800

    Set default value to @input headerLevel

--- vtr-ui/src/app/components/container-collapsible/container-collapsible.component.ts
+++ vtr-ui/src/app/components/container-collapsible/container-collapsible.component.ts
@@ -16,7 +16,7 @@ export class ContainerCollapsibleComponent extends BaseComponent {
 	@Output() toggle = new EventEmitter();
 	@Input() metricsParent: string;
 	@Input() tabIndex = 0;
-	@Input() headerLevel : number;
+	@Input() headerLevel : number | null = null;
 
 	constructor() {
 		super();
