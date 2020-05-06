0c02db48926cb51074b61385608f6747ed5ca205
Guilherme Baldo <guilherme.baldo@fit-tecnologia.org.br>
2020-05-06 18:02:29 -0300

    VAN-17492: adding translations directly on HTML + adding placeholder for translations

--- vtr-ui/src/app/components/modal/modal-scan-failure/modal-scan-failure.component.ts
+++ vtr-ui/src/app/components/modal/modal-scan-failure/modal-scan-failure.component.ts
@@ -9,20 +9,17 @@ import { Router, ActivatedRoute } from '@angular/router';
   styleUrls: ['./modal-scan-failure.component.scss']
 })
 export class ModalScanFailureComponent implements OnInit {
-  @Input() title = this.translate.instant('hardwareScan.eTicket.header');
-  @Input() problemDetectedMessage = this.translate.instant('hardwareScan.eTicket.problemDetected');
-  @Input() supportRequestQuestionMessage = this.translate.instant('hardwareScan.eTicket.ticketRequestQuestion');
-  @Input() recoverBadSectorsButtonText = "Recover Bad Sectors"; //this.translate.instant('hardwareScan.eTicket.RecoverBadSectorsButton')
-  @Input() failedModules;
-  @Input() rbsDevices;
-  @Input() supportUrl;
-  supportButtonText = "Open Ticket"; //this.translate.instant('hardwareScan.eTicket.supportButtonText')
 
+  @Input() supportUrl: string;
+  @Input() hasFailedRbsDevice: boolean;
+
+  failedModules: any;
+  rbsDevices: any;
   private failedRbsDevices: Array<string>;
 
-  constructor(public activeModal: NgbActiveModal, private translate: TranslateService, 
+  constructor(public activeModal: NgbActiveModal, private translate: TranslateService,
     private route: ActivatedRoute,
-    private router: Router) { 
+    private router: Router) {
       this.failedRbsDevices = [];
   }
 
@@ -33,26 +30,26 @@ export class ModalScanFailureComponent implements OnInit {
   closeModal() {
 		this.activeModal.close('close');
   }
-  
+
   // Checks the any storage device from failedModules has support for RBS (according to rbsDevices list)
   private createListFailedRbsDevices() {
     // First, getting a list of Ids of storage devices with failure
     let failedStorageIds = this.failedModules.find(m => m.moduleId == 'storage')
       .devices.reduce(
         (result, device) => {
-          result.push(device.deviceId); 
+          result.push(device.deviceId);
           return result;
         }, []);
-    
+
     // Second, getting a list of Ids of storage devices that support RBS
-    let rbsDeviceIds = this.rbsDevices.groupList.reduce( 
-      (result, device) => { 
-        result.push(device.id); 
-        return result; 
+    let rbsDeviceIds = this.rbsDevices.groupList.reduce(
+      (result, device) => {
+        result.push(device.id);
+        return result;
       }, []);
 
     // Finally, getting the list of Ids contained in both lists
-    // If any value is returned, it means that a storage device that supports RBS failed, meaning that 
+    // If any value is returned, it means that a storage device that supports RBS failed, meaning that
     // a RBS test will be suggested to the user
     this.failedRbsDevices = failedStorageIds.filter(storageId => rbsDeviceIds.includes(storageId))
   }
