cc3cdef30fdbf80238d948055f575e15f61d7867
Guilherme Baldo <guilherme.baldo@fit-tecnologia.org.br>
2020-05-08 17:01:44 -0300

    VAN-17942: code review fixes (identation + unused variable)

--- vtr-ui/src/app/components/modal/modal-scan-failure/modal-scan-failure.component.ts
+++ vtr-ui/src/app/components/modal/modal-scan-failure/modal-scan-failure.component.ts
@@ -4,69 +4,78 @@ import { TranslateService } from '@ngx-translate/core';
 import { Router, ActivatedRoute } from '@angular/router';
 
 @Component({
-  selector: 'vtr-modal-scan-failure',
-  templateUrl: './modal-scan-failure.component.html',
-  styleUrls: ['./modal-scan-failure.component.scss']
+	selector: 'vtr-modal-scan-failure',
+	templateUrl: './modal-scan-failure.component.html',
+	styleUrls: ['./modal-scan-failure.component.scss']
 })
 export class ModalScanFailureComponent implements OnInit {
 
-  @Input() supportUrl: string;
-  @Input() hasFailedRbsDevice: boolean;
+	@Input() supportUrl: string;
+	@Input() hasFailedRbsDevice: boolean;
 
-  failedModules: any;
-  rbsDevices: any;
-  private failedRbsDevices: Array<string>;
+	failedModules: any;
+	rbsDevices: any;
+	private failedRbsDevices: Array<string>;
 
-  constructor(public activeModal: NgbActiveModal, private translate: TranslateService,
-    private route: ActivatedRoute,
-    private router: Router) {
-      this.failedRbsDevices = [];
-  }
+	constructor(public activeModal: NgbActiveModal, private translate: TranslateService,
+		private route: ActivatedRoute,
+		private router: Router) {
+		this.failedRbsDevices = [];
+	}
 
-  ngOnInit() {
-    this.createListFailedRbsDevices();
-  }
+	ngOnInit() { }
 
-  closeModal() {
+	// closes modal
+	closeModal() {
 		this.activeModal.close('close');
-  }
+	}
 
-  // Checks the any storage device from failedModules has support for RBS (according to rbsDevices list)
-  private createListFailedRbsDevices() {
-    // First, getting a list of Ids of storage devices with failure
-    let failedStorageIds = this.failedModules.find(m => m.moduleId == 'storage')
-      .devices.reduce(
-        (result, device) => {
-          result.push(device.deviceId);
-          return result;
-        }, []);
+	// Opens Lenovo's support page and closes the modal
+	openSupportPage() {
+		window.open(this.supportUrl);
+		this.closeModal();
+	}
 
-    // Second, getting a list of Ids of storage devices that support RBS
-    let rbsDeviceIds = this.rbsDevices.groupList.reduce(
-      (result, device) => {
-        result.push(device.id);
-        return result;
-      }, []);
+	// Sets the lists of failed modules and RBS devices, and then creates the list of RBS devices that failed
+	configureDevicesLists(failedModules, rbsDevices) {
+		this.failedModules = failedModules;
+		this.rbsDevices = rbsDevices;
+		this.failedRbsDevices = this.createListFailedRbsDevices();
+		this.hasFailedRbsDevice = this.failedRbsDevices.length > 0;
+	}
 
-    // Finally, getting the list of Ids contained in both lists
-    // If any value is returned, it means that a storage device that supports RBS failed, meaning that
-    // a RBS test will be suggested to the user
-    this.failedRbsDevices = failedStorageIds.filter(storageId => rbsDeviceIds.includes(storageId))
-  }
+	// Checks the any storage device from failedModules has support for RBS (according to rbsDevices list)
+	private createListFailedRbsDevices() {
+		// First, getting a list of Ids of storage devices with failure
+		let failedStorageIds = this.failedModules.find(m => m.moduleId == 'storage')
+			.devices.reduce(
+				(result, device) => {
+					result.push(device.deviceId);
+					return result;
+				}, []);
 
-  hasFailedRbsDevices(){
-    return this.failedRbsDevices.length > 0;
-  }
+		// Second, getting a list of Ids of storage devices that support RBS
+		let rbsDeviceIds = this.rbsDevices.groupList.reduce(
+			(result, device) => {
+				result.push(device.id);
+				return result;
+			}, []);
 
-  // Goes to RBS page, passing defective device list to be selected when RBS page loads
-  goToRBSPage() {
-    this.router.navigate(['recover-bad-sectors'], {
-      relativeTo: this.route,
-      queryParams: {
-        failedDevices: this.failedRbsDevices
-      },
-      queryParamsHandling: 'merge',
-    });
-    this.closeModal();
-  }
+		// Finally, getting the list of Ids contained in both lists
+		// If any value is returned, it means that a storage device that supports RBS failed, meaning that
+		// a RBS test will be suggested to the user
+		return failedStorageIds.filter(storageId => rbsDeviceIds.includes(storageId))
+	}
+
+	// Goes to RBS page, passing defective device list to be selected when RBS page loads
+	goToRBSPage() {
+		this.router.navigate(['recover-bad-sectors'], {
+			relativeTo: this.route,
+			queryParams: {
+				failedDevices: this.failedRbsDevices
+			},
+			queryParamsHandling: 'merge'
+		});
+		this.closeModal();
+	}
 }
