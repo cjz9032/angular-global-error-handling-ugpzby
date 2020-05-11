62f2ace60dc34c71d09f92f72aae9752051e6ee5
Santosh udnur <sudnur@lenovo.com>
2020-05-11 12:15:44 +0530

    changed the code work as angular way

--- vtr-ui/src/app/components/modal/modal-reboot-confirm/modal-reboot-confirm.component.ts
+++ vtr-ui/src/app/components/modal/modal-reboot-confirm/modal-reboot-confirm.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, HostListener } from '@angular/core';
+import { Component, ElementRef, HostListener, OnInit, ViewChild } from '@angular/core';
 import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
@@ -8,13 +8,18 @@ import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
 })
 export class ModalRebootConfirmComponent implements OnInit {
 	public description: string;
+	@ViewChild('btnClose', { static: false }) set btnClose(element: ElementRef) {
+		if (element) {
+			element.nativeElement.focus();
+		}
+	}
 
 	constructor(public activeModal: NgbActiveModal) { }
 
 	ngOnInit() {
-		const modal = document.querySelector('.close-btn') as HTMLElement;
-		modal.focus();
+		this.btnClose.nativeElement.focus();
 	}
+
 	proceedToReboot() {
 		this.activeModal.close('enable');
 	}
