206504251b13f66d992f2328a6034bdd988392b2
Bob LB4 Liu <liulb4@lenovo.com>
2020-05-11 17:44:18 +0800

    VAN-18099 ensure the warranty date format is consistent with the PC

--- vtr-ui/src/app/pipe/format-utc-date/format-utc-date.pipe.ts
+++ vtr-ui/src/app/pipe/format-utc-date/format-utc-date.pipe.ts
@@ -7,14 +7,7 @@ export class FormatUTCDatePipe implements PipeTransform {
 
 	transform(value: any, ...args: any[]): any {
 		const date = new Date(value);
-		const mm = date.getUTCMonth() + 1; // getMonth() is zero-based
-		const dd = date.getUTCDate();
-
-		return [
-			date.getUTCFullYear(),
-			'/', (mm > 9 ? '' : '0') + mm,
-			'/', (dd > 9 ? '' : '0') + dd
-		].join('');
+		return date.toLocaleDateString();
 	}
 
 }
