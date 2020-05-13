1a95df3e6fa4c58a6db0863b7604955e3edfcef4
lixd19 <lixd19@lenovo.com>
2020-05-13 16:03:03 +0800

    singleton listen for  addin events and  mount event on notification machainism

---
+++ vtr-ui/src/app/services/system-event/system-event.service.ts
@@ -0,0 +1,55 @@
+import { Injectable } from '@angular/core';
+import { VantageShellService } from 'src/app/services/vantage-shell/vantage-shell.service';
+import { SystemEventFeature, EventTypes, SystemEvent } from '@lenovo/tan-client-bridge';
+import { CommonService } from '../common/common.service';
+import { LoggerService } from '../logger/logger.service';
+@Injectable({
+	providedIn: 'root'
+})
+export class SystemEventService {
+
+	private sysEvent: SystemEventFeature;
+
+	constructor(
+		private shellService: VantageShellService,
+		private commonService: CommonService,
+		private logger: LoggerService,
+
+	) {
+		this.sysEvent = this.shellService.getSysEventFeature();
+		this.sysEvent.startListen();
+		this.sysEvent.on(EventTypes.customEvent, (event: SystemEvent) => {
+			this.commonService.sendNotification(event.Name, event.EventArgs);
+		});
+	}
+
+	async registerCustomEvent(eventName: string): Promise<boolean> {
+		try {
+			await this.sysEvent.registerCustomEvent(eventName);
+			return true;
+		} catch{
+			this.logger.error(`registerCustomEvent ${eventName} failed`);
+		}
+		return false;
+	}
+
+	async unRegisterCustomEvent(eventName: string): Promise<boolean> {
+		try {
+			await this.sysEvent.unRegisterCustomEvent(eventName);
+			return false;
+		} catch{
+			this.logger.error(`unRegisterCustomEvent ${eventName} failed`);
+		}
+		return true;
+	}
+
+	cancelListen() {
+		try {
+			this.sysEvent.cancelListen();
+		}
+		catch{
+			this.logger.error(`cancelListen failed`);
+		}
+	}
+
+}
