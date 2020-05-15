c8171c51466d6eac347424fe6cfb2d2e289785db
Santosh udnur <sudnur@lenovo.com>
2020-05-15 16:52:36 +0530

    fix for van-18126 final

--- vtr-ui/src/app/directives/ui-autofocus-directive/autofocus.directive.ts
+++ vtr-ui/src/app/directives/ui-autofocus-directive/autofocus.directive.ts
@@ -1,5 +1,4 @@
-import { Directive, Input, ElementRef, Inject, AfterViewInit, AfterContentInit, Renderer2, OnInit, OnChanges, SimpleChanges } from '@angular/core';
-import { DOCUMENT } from '@angular/common';
+import { AfterContentInit, Directive, ElementRef, Input, OnChanges, SimpleChanges } from '@angular/core';
 
 
 @Directive({
@@ -17,36 +16,37 @@ export class AutofocusDirective implements AfterContentInit, OnChanges {
 			this.element = this.el.nativeElement;
 		}
 		catch (error) {
-			console.log('constructor ' + JSON.stringify(error));
+			// console.log('constructor ' + JSON.stringify(error));
 		}
 
 	}
 	ngOnChanges(changes: SimpleChanges): void {
-		console.log('ngOnChanges ' + this.element.innerHTML + JSON.stringify(changes));
+
 		try {
-			if (changes.focus) {
+			// console.log('ngOnChanges ' + JSON.stringify(changes));
+			if (changes.autofocus.currentValue) {
 				this.focusElement();
 			}
 
 		} catch (error) {
-			console.log('focusElement ' + JSON.stringify(error));
+			// console.log('ngOnChanges error' + JSON.stringify(error));
 		}
 	}
 
 	private focusElement() {
 		try {
-			// console.log('focusElement' + this.element.innerHTML);
-
 			if (this.focus) {
-				// Otherwise Angular throws error: Expression has changed after it was checked.
-				// let activeElement = document.activeElement as HTMLElement;
+
 				if (!this.element.hasAttribute(this.TAB_INDEX)) {
 					this.hadTabIndex = false;
 					this.element.setAttribute(this.TAB_INDEX, '0');
 				}
 
 				window.setTimeout(() => {
-					// activeElement.blur();
+					// Added to reposition narrator focus position during hide and show.
+					let activeElement = document.activeElement as HTMLElement;
+					activeElement.blur();
+
 					this.element.addEventListener('blur', this.handleBur.bind(this));
 					this.element.focus();
 					/* if (this.element.hasAttribute(this.TAB_INDEX)) {
@@ -55,35 +55,40 @@ export class AutofocusDirective implements AfterContentInit, OnChanges {
 						window.scrollBy(0, -100);
 					} */
 
-				}, 500);
+				}, 100);
 			}
 		}
 		catch (error) {
-			console.log('focusElement ' + JSON.stringify(error));
+			// console.log('focusElement error' + JSON.stringify(error));
 		}
 	}
 	private handleBur() {
 		try {
 			// console.log('handleBur this.hadTabIndex' + this.hadTabIndex);
 			if (!this.hadTabIndex) {
+				// console.log('had not hadTabIndex : added remove it');
 				this.element.removeAttribute(this.TAB_INDEX);
 			}
 
 		} catch (error) {
-			console.log('handleBur ' + JSON.stringify(error))
+			// console.log('handleBur error' + JSON.stringify(error));
 		}
 
 
 	}
-	public ngAfterContentInit() {
-		console.log('ngAfterContentInit' + this.element.innerHTML);
-		this.focusElement();
-	}
 
 	@Input() set autofocus(condition: boolean) {
-		console.log('autofocus ' + this.element.innerHTML + typeof (condition));
 		this.focus = condition !== false;
 	}
 
+	public ngAfterContentInit() {
+		try {
+			// console.log('ngAfterContentInit');
+			this.focusElement();
+		} catch (error) {
+			// console.log('ngAfterContentInit error' + JSON.stringify(error))
+		}
+
+	}
 
 }
