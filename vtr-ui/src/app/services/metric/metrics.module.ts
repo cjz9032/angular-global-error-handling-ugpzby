24a7d6a345e1aa15dd883d31fba0905b96ae3b8f
Mark Tao <taoxf1@lenovo.com>
2020-05-28 13:37:01 +0800

    VAN-18254 fix large loading time

--- vtr-ui/src/app/services/metric/metrics.module.ts
+++ vtr-ui/src/app/services/metric/metrics.module.ts
@@ -1,16 +1,13 @@
 import { CommonModule } from '@angular/common';
 import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
 import { MetricsDirective } from 'src/app/services/metric/metrics.directive';
-import { PageLoadedDetectorDirective } from './page-loaded-detector.directive';
 
 @NgModule({
 	declarations: [
-		MetricsDirective,
-		PageLoadedDetectorDirective
+		MetricsDirective
 	],
 	exports: [
-		MetricsDirective,
-		PageLoadedDetectorDirective
+		MetricsDirective
 	],
 	imports: [
 		CommonModule
