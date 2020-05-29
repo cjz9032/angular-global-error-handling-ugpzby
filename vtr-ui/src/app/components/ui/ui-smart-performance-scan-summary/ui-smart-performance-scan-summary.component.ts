e1f7635c8a47a32eff3e5dd09994c2a01f1facf9
zakirgori <zgori@lenovo.com>
2020-05-29 15:48:32 +0530

    Three sixtyfive day for calender.

--- vtr-ui/src/app/components/ui/ui-smart-performance-scan-summary/ui-smart-performance-scan-summary.component.ts
+++ vtr-ui/src/app/components/ui/ui-smart-performance-scan-summary/ui-smart-performance-scan-summary.component.ts
@@ -172,7 +172,7 @@ export class UiSmartPerformanceScanSummaryComponent implements OnInit {
 		this.minDate = {
 			year: this.currentDate.getFullYear() - 1,
 			month: this.currentDate.getMonth() + 1,
-			day: this.currentDate.getDate() 
+			day: this.currentDate.getDate() + 1
 		};
 		this.maxDate = {
 			year: this.currentDate.getFullYear(),
