afd2e91c8c57ca7adf8e95ce6fb20749311b6f1d
rkumar24 <rkumar24@lenovo.com>
2020-06-23 17:51:59 +0530

    VAN-19610 UT coverage for slider component

--- vtr-ui/src/app/components/ui/ui-brightness-slider/ui-brightness-slider.component.spec.ts
+++ vtr-ui/src/app/components/ui/ui-brightness-slider/ui-brightness-slider.component.spec.ts
@@ -1,6 +1,7 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { UiBrightnessSliderComponent } from './ui-brightness-slider.component';
+import { By } from '@angular/platform-browser';
 
 describe('UiBrightnessSliderComponent', () => {
 	let component: UiBrightnessSliderComponent;
@@ -23,16 +24,34 @@ describe('UiBrightnessSliderComponent', () => {
 		expect(component).toBeTruthy();
 	});
 
-	it('should check dragEnd when event true', () => {
-		const event=true;
-		component.dragEnd(event);
-		expect(component).toBeTruthy();
+	it('should check dragEnd when event false', () => {
+		spyOn(component.onSliderChanged, 'emit').and.callThrough();
+		component.onSliderChanged.subscribe((res: any) => {
+			expect(res).toBe(false);
+		});
+		component.dragEnd({value: false});
 	});
 
-	it('should check dragEnd when event false', () => {
-		const event=false;
-		component.dragEnd(event);
-		expect(component).toBeTruthy();
+	it('should check dragEnd when event true', () => {
+		spyOn(component.onSliderChanged, 'emit').and.callThrough();
+		component.onSliderChanged.subscribe((res: any) => {
+			expect(res).toBe(true);
+		});
+		component.dragEnd({value: true});
 	});
+
+	it('set attribute to radiogroup when tab focus', (() => {
+			component.onFocusSlider({ which: 9 });
+			fixture.detectChanges()
+			const elem = fixture.debugElement.query(By.css('.brightness_slider')).nativeElement;
+			expect(elem.role).toBe(undefined);
+		}));
+
+	it('set attribute to slider when mouse hover', (() => {
+			component.onMouseSlider();
+		  fixture.detectChanges();
+			const elem = fixture.debugElement.query(By.css('.brightness_slider'));
+			expect(elem.attributes.role).toContain('slider');
+		}));
 
 });
