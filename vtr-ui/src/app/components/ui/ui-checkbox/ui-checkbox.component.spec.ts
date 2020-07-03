6fb6671c2cb3d1171c5d2670665471c6d3fec7d5
Santosh udnur <sudnur@lenovo.com>
2020-07-03 16:39:56 +0530

    improved coverage for src/app/components/ui/ui-checkbox/ui-checkbox.component.spec.ts

--- vtr-ui/src/app/components/ui/ui-checkbox/ui-checkbox.component.spec.ts
+++ vtr-ui/src/app/components/ui/ui-checkbox/ui-checkbox.component.spec.ts
@@ -1,14 +1,26 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { UiCheckboxComponent } from './ui-checkbox.component';
+import { NO_ERRORS_SCHEMA } from '@angular/core';
+import { CommonMetricsService } from 'src/app/services/common-metrics/common-metrics.service';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { DevService } from 'src/app/services/dev/dev.service';
+import { MetricService } from 'src/app/services/metric/metrics.service';
+import { RouterTestingModule } from '@angular/router/testing';
 
-xdescribe('UiCheckboxComponent', () => {
+describe('UiCheckboxComponent', () => {
 	let component: UiCheckboxComponent;
 	let fixture: ComponentFixture<UiCheckboxComponent>;
 
 	beforeEach(async(() => {
 		TestBed.configureTestingModule({
-			declarations: [UiCheckboxComponent]
+			declarations: [UiCheckboxComponent],
+			schemas: [NO_ERRORS_SCHEMA],
+			imports: [
+				HttpClientTestingModule,
+				RouterTestingModule],
+			providers: [CommonMetricsService, MetricService, DevService]
+
 		})
 			.compileComponents();
 	}));
@@ -16,10 +28,18 @@ xdescribe('UiCheckboxComponent', () => {
 	beforeEach(() => {
 		fixture = TestBed.createComponent(UiCheckboxComponent);
 		component = fixture.componentInstance;
+		component.isMetricsEnabled = true;
 		fixture.detectChanges();
 	});
 
 	it('should create', () => {
 		expect(component).toBeTruthy();
+	});
+
+	it('should test onChange method', () => {
+		let event = { target: { value: true } };
+		spyOn(component.toggle, 'emit').and.callThrough();
+		component.onChange(event);
+		expect(component.toggle.emit).toHaveBeenCalled();
 	});
 });
