ce385c27615b64992a33f3908df9ee247ccc244d
Santosh udnur <sudnur@lenovo.com>
2020-07-03 12:33:26 +0530

    intelligent-media.component.spec.ts ,download-failed-modal.component.spec.ts,voice.component.spec.ts made component creation trustworthy

--- vtr-ui/src/app/components/pages/page-smart-assist/intelligent-media/intelligent-media.component.spec.ts
+++ vtr-ui/src/app/components/pages/page-smart-assist/intelligent-media/intelligent-media.component.spec.ts
@@ -1,77 +1,76 @@
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-import { SmartAssistService } from 'src/app/services/smart-assist/smart-assist.service';
-import { IntelligentMediaComponent } from './intelligent-media.component';
-import { VantageShellService } from 'src/app/services/vantage-shell/vantage-shell.service';
-
-import { LoggerService } from 'src/app/services/logger/logger.service';
+import { RouterTestingModule } from '@angular/router/testing';
+import { TranslateModule, TranslateService } from '@ngx-translate/core';
+import { DevService } from 'src/app/services/dev/dev.service';
 import { DeviceService } from 'src/app/services/device/device.service';
 import { DisplayService } from 'src/app/services/display/display.service';
-import { TranslateService } from '@ngx-translate/core';
+import { LoggerService } from 'src/app/services/logger/logger.service';
 import { MetricService } from 'src/app/services/metric/metrics.service';
-import { HttpClientTestingModule } from '@angular/common/http/testing';
-import { TranslateModule, TranslateLoader } from '@ngx-translate/core';
-import { RouterTestingModule } from "@angular/router/testing";
 import { RouteHandlerService } from 'src/app/services/route-handler/route-handler.service';
-import { DevService } from 'src/app/services/dev/dev.service';
+import { SmartAssistService } from 'src/app/services/smart-assist/smart-assist.service';
+import { VantageShellService } from 'src/app/services/vantage-shell/vantage-shell.service';
+import { IntelligentMediaComponent } from './intelligent-media.component';
+
 
 describe('component: IntelligentMediaComponent', () => {
-  let component: IntelligentMediaComponent;
-  let fixture: ComponentFixture<IntelligentMediaComponent>;
-  let smartAssist: SmartAssistService;
-  let vantageShellService: VantageShellService;
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [IntelligentMediaComponent],
-      imports: [
-        HttpClientTestingModule,
-        TranslateModule.forRoot(),
-        RouterTestingModule
-      ],
-      providers: [
-        SmartAssistService,
-        LoggerService,
-        DeviceService,
-        DevService,
-        RouteHandlerService,
-        DisplayService,
-        TranslateService,
-        MetricService
-      ]
-    })
-      .compileComponents();
-  }));
+	let component: IntelligentMediaComponent;
+	let fixture: ComponentFixture<IntelligentMediaComponent>;
+	let smartAssist: SmartAssistService;
+	let vantageShellService: VantageShellService;
+	beforeEach(async(() => {
+		TestBed.configureTestingModule({
+			declarations: [IntelligentMediaComponent],
+			imports: [
+				HttpClientTestingModule,
+				TranslateModule.forRoot(),
+				RouterTestingModule
+			],
+			providers: [
+				SmartAssistService,
+				LoggerService,
+				DeviceService,
+				DevService,
+				RouteHandlerService,
+				DisplayService,
+				TranslateService,
+				MetricService
+			]
+		})
+			.compileComponents();
+	}));
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(IntelligentMediaComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+	beforeEach(() => {
+		fixture = TestBed.createComponent(IntelligentMediaComponent);
+		component = fixture.componentInstance;
+		fixture.detectChanges();
+	});
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+	it('should create', () => {
+		expect(component).toBeTruthy();
+	});
 
-  it("setVideoPauseResumeStatus", () => {
-    smartAssist = TestBed.get(SmartAssistService);
-    smartAssist.isShellAvailable = true;
-    const spy = spyOn(
-      smartAssist,
-      "setVideoPauseResumeStatus"
-    ).and.returnValue(Promise.resolve(true));
+	it('setVideoPauseResumeStatus', () => {
+		smartAssist = TestBed.get(SmartAssistService);
+		smartAssist.isShellAvailable = true;
+		const spy = spyOn(
+			smartAssist,
+			'setVideoPauseResumeStatus'
+		).and.returnValue(Promise.resolve(true));
 
-    component.setVideoPauseResumeStatus({ switchValue: true });
-    expect(smartAssist.setVideoPauseResumeStatus).toHaveBeenCalled();
-  });
+		component.setVideoPauseResumeStatus({ switchValue: true });
+		expect(smartAssist.setVideoPauseResumeStatus).toHaveBeenCalled();
+	});
 
-  it("setSuperResolutionStatus", () => {
-    smartAssist = TestBed.get(SmartAssistService);
-    smartAssist.isShellAvailable = true;
-    const spy = spyOn(
-      smartAssist,
-      "setSuperResolutionStatus"
-    ).and.returnValue(Promise.resolve(true));
+	it('setSuperResolutionStatus', () => {
+		smartAssist = TestBed.get(SmartAssistService);
+		smartAssist.isShellAvailable = true;
+		const spy = spyOn(
+			smartAssist,
+			'setSuperResolutionStatus'
+		).and.returnValue(Promise.resolve(true));
 
-    component.setSuperResolutionStatus({ switchValue: true });
-    expect(smartAssist.setSuperResolutionStatus).toHaveBeenCalled();
-  });
+		component.setSuperResolutionStatus({ switchValue: true });
+		expect(smartAssist.setSuperResolutionStatus).toHaveBeenCalled();
+	});
 });
--- vtr-ui/src/app/components/pages/page-smart-assist/voice/voice.component.spec.ts
+++ vtr-ui/src/app/components/pages/page-smart-assist/voice/voice.component.spec.ts
@@ -1,25 +1,38 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { VoiceComponent } from './voice.component';
+import { NO_ERRORS_SCHEMA } from '@angular/core';
+import { TranslateModule } from '@ngx-translate/core';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { RouterTestingModule } from '@angular/router/testing';
+import { SmartAssistService } from 'src/app/services/smart-assist/smart-assist.service';
+import { LoggerService } from 'src/app/services/logger/logger.service';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 
 describe('VoiceComponent', () => {
-  // let component: VoiceComponent;
-  // let fixture: ComponentFixture<VoiceComponent>;
+	let component: VoiceComponent;
+	let fixture: ComponentFixture<VoiceComponent>;
 
-  beforeEach(async(() => {
-    // TestBed.configureTestingModule({
-    //   declarations: [ VoiceComponent ]
-    // })
-    // .compileComponents();
-  }));
+	beforeEach(async(() => {
+		TestBed.configureTestingModule({
+			declarations: [VoiceComponent],
+			schemas: [NO_ERRORS_SCHEMA],
+			imports: [
+				TranslateModule.forRoot(),
+				HttpClientTestingModule,
+				RouterTestingModule
+			],
+			providers: [SmartAssistService, LoggerService, NgbModal]
+		})
+			.compileComponents();
+	}));
 
-  beforeEach(() => {
-    // fixture = TestBed.createComponent(VoiceComponent);
-    // component = fixture.componentInstance;
-    // fixture.detectChanges();
-  });
+	beforeEach(() => {
+		fixture = TestBed.createComponent(VoiceComponent);
+		component = fixture.componentInstance;
+		fixture.detectChanges();
+	});
 
-  it('should create', () => {
-    // expect(component).toBeTruthy();
-  });
+	it('should create', () => {
+		expect(component).toBeTruthy();
+	});
 });
