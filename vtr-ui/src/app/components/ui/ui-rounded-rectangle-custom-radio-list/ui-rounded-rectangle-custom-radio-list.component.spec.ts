208c2f9cc02f5d8f466b5657d455d6fc70ad7c99
Santosh udnur <sudnur@lenovo.com>
2020-07-06 17:52:49 +0530

    made ui-rounded-rectangle-custom-radio-list.component.spec.ts compilatable

--- vtr-ui/src/app/components/ui/ui-rounded-rectangle-custom-radio-list/ui-rounded-rectangle-custom-radio-list.component.spec.ts
+++ vtr-ui/src/app/components/ui/ui-rounded-rectangle-custom-radio-list/ui-rounded-rectangle-custom-radio-list.component.spec.ts
@@ -1,14 +1,34 @@
+
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { NO_ERRORS_SCHEMA } from '@angular/core';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
+import { TranslateModule, TranslateService } from '@ngx-translate/core';
+import { DevService } from 'src/app/services/dev/dev.service';
+import { LoggerService } from 'src/app/services/logger/logger.service';
+import { MetricService } from 'src/app/services/metric/metrics.service';
 import { UiRoundedRectangleCustomRadioListComponent } from './ui-rounded-rectangle-custom-radio-list.component';
 
 
-xdescribe('UiRoundedRectangleCustomRadioListComponent', () => {
+fdescribe('UiRoundedRectangleCustomRadioListComponent', () => {
 	let component: UiRoundedRectangleCustomRadioListComponent;
 	let fixture: ComponentFixture<UiRoundedRectangleCustomRadioListComponent>;
 
 	beforeEach(async(() => {
 		TestBed.configureTestingModule({
-			declarations: [UiRoundedRectangleCustomRadioListComponent]
+			declarations: [UiRoundedRectangleCustomRadioListComponent],
+			schemas: [NO_ERRORS_SCHEMA],
+			imports: [
+				TranslateModule.forRoot(),
+				HttpClientTestingModule,
+				RouterTestingModule
+			],
+			providers: [
+				LoggerService,
+				MetricService,
+				DevService,
+				TranslateService
+			]
 		})
 			.compileComponents();
 	}));
