faa429dd605dfba3d97b89462d0516042f6fb95a
Phaneendra <psadhu@lenovo.com>
2020-07-10 14:02:08 +0530

    Test cases

--- vtr-ui/src/app/components/ui/ui-switch-onoff/ui-switch-onoff.component.spec.ts
+++ vtr-ui/src/app/components/ui/ui-switch-onoff/ui-switch-onoff.component.spec.ts
@@ -1,25 +1,49 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { UiSwitchOnoffComponent } from './ui-switch-onoff.component';
+import { RouterTestingModule } from '@angular/router/testing';
+import { TranslateModule, TranslateLoader } from '@ngx-translate/core';
+import { HttpLoaderFactory } from 'src/app/modules/translation.module';
+import { HttpClient } from '@angular/common/http';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 
-xdescribe('UiSwitchOnoffComponent', () => {
-  let component: UiSwitchOnoffComponent;
-  let fixture: ComponentFixture<UiSwitchOnoffComponent>;
+describe('UiSwitchOnoffComponent', () => {
+	let component: UiSwitchOnoffComponent;
+	let fixture: ComponentFixture<UiSwitchOnoffComponent>;
 
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [ UiSwitchOnoffComponent ]
-    })
-    .compileComponents();
-  }));
+	beforeEach(async(() => {
+		TestBed.configureTestingModule({
+			declarations: [UiSwitchOnoffComponent],
+			imports: [RouterTestingModule, TranslateModule.forRoot({
+				loader: {
+					provide: TranslateLoader,
+					useFactory: HttpLoaderFactory,
+					deps: [HttpClient]
+				}
+			}), HttpClientTestingModule],
+		})
+			.compileComponents();
+	}));
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(UiSwitchOnoffComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+	beforeEach(() => {
+		fixture = TestBed.createComponent(UiSwitchOnoffComponent);
+		component = fixture.componentInstance;
+		fixture.detectChanges();
+	});
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+	it('should create', () => {
+		expect(component).toBeTruthy();
+	});
+	it('should call sendChangeEvent', () => {
+		const event = { target: { switchValue: 4 } }
+		component.sendChangeEvent(event)
+	});
+	it('should call stopPropagation', () => {
+		const event = new Event('click');
+		component.stopPropagation(event)
+	});
+	it('should call onChange', () => {
+		const event = { target: { switchValue: 4, disabled: true } }
+		component.onChange(event)
+	});
 });
