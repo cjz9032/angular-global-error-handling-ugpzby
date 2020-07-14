41233604e30ee9c6f2d8f2052b66a53af6fcb315
Santosh udnur <sudnur@lenovo.com>
2020-07-14 15:35:21 +0530

    improvised coverage for active-protection-system.component

--- vtr-ui/src/app/components/pages/page-smart-assist/active-protection-system/active-protection-system.component.spec.ts
+++ vtr-ui/src/app/components/pages/page-smart-assist/active-protection-system/active-protection-system.component.spec.ts
@@ -46,7 +46,28 @@ class MockAPS {
 	setAPSMode() {
 		return Promise.resolve(true);
 	}
+	setAPSSensitivityLevel(value) {
+		let promise = new Promise((resolve, reject) => {
+			resolve(true);
+			reject(false);
+		});
+		return promise;
 
+	}
+	setSnoozeTime(value) {
+		return Promise.resolve(true);
+	}
+
+	sendSnoozeCommand(value) {
+		return Promise.resolve(true);
+	}
+	setAutoDisableSetting(value) {
+		return Promise.resolve(true);
+	}
+
+	public setSnoozeSetting(value: boolean) {
+		return Promise.resolve(true);
+	}
 }
 
 
@@ -88,6 +109,7 @@ describe('ActiveProtectionSystemComponent', () => {
 		debugElement = fixture.debugElement;
 		component = fixture.componentInstance;
 		smartAssist = debugElement.injector.get(SmartAssistService);
+		commonMetricsService = debugElement.injector.get(CommonMetricsService);
 		fixture.detectChanges();
 	}));
 
@@ -107,11 +129,43 @@ describe('ActiveProtectionSystemComponent', () => {
 			expect(component.apsStatus).toBeTruthy();
 		}
 	});
+
+	it('should set setAPSMode , send metrics', () => {
+		fixture.detectChanges();
+		const spyOnMetrics = spyOn(commonMetricsService, 'sendMetrics');
+		component.setAPSMode();
+		expect(spyOnMetrics).toHaveBeenCalled();
+	});
+
 	it('should set APS Sensitivity level', () => {
 		const sensitivity = 0;
 		spyOn(component, 'setAPSSensitivityLevel');
 		component.setAPSSensitivityLevel(sensitivity);
 		expect(component.setAPSSensitivityLevel).toHaveBeenCalled();
+	});
+
+	it('should set APS Sensitivity level API call check with sensitivity 0', () => {
+		const sensitivity = 0;
+		// spyOn(smartAssist.setAPSSensitivityLevel, 'then').and.returnValue(true);
+		const spySetAPSSensitivityLevel = spyOn(smartAssist, 'setAPSSensitivityLevel').and.returnValue(Promise.resolve(true));
+		component.setAPSSensitivityLevel(sensitivity);
+		expect(spySetAPSSensitivityLevel).toHaveBeenCalled();
+	});
+
+	it('should set APS Sensitivity level API call check with sensitivity 50', () => {
+		const sensitivity = 50;
+		// spyOn(smartAssist.setAPSSensitivityLevel, 'then').and.returnValue(true);
+		const spySetAPSSensitivityLevel = spyOn(smartAssist, 'setAPSSensitivityLevel').and.returnValue(Promise.resolve(true));
+		component.setAPSSensitivityLevel(sensitivity);
+		expect(spySetAPSSensitivityLevel).toHaveBeenCalled();
+	});
+
+	it('should set APS Sensitivity level API call check with sensitivity 100', () => {
+		const sensitivity = 100;
+		// spyOn(smartAssist.setAPSSensitivityLevel, 'then').and.returnValue(true);
+		const spySetAPSSensitivityLevel = spyOn(smartAssist, 'setAPSSensitivityLevel').and.returnValue(Promise.resolve(true));
+		component.setAPSSensitivityLevel(sensitivity);
+		expect(spySetAPSSensitivityLevel).toHaveBeenCalled();
 	});
 
 	it('should set Auto Disabiling Settings', () => {
@@ -120,6 +174,15 @@ describe('ActiveProtectionSystemComponent', () => {
 		component.setAutoDisableSetting(flag);
 		expect(component.setAutoDisableSetting).toHaveBeenCalled();
 	});
+
+	it('should set Auto Disabiling Settings , send metrics', () => {
+		// fixture.detectChanges();
+		const flag = 0;
+		const spyOnMetrics = spyOn(commonMetricsService, 'sendMetrics');
+		component.setAutoDisableSetting(flag);
+		expect(spyOnMetrics).toHaveBeenCalled();
+	});
+
 	it('should set Snooze Settings', () => {
 		const flag = component.manualSnooze; // the value that is changing in component
 		spyOn(component, 'setSnoozeSetting');
@@ -130,6 +193,14 @@ describe('ActiveProtectionSystemComponent', () => {
 		} else {
 			expect(flag).toBeTruthy();
 		}
+	});
+
+	it('should set Snooze Settings send metrics', () => {
+		const flag = component.manualSnooze; // the value that is changing in component
+		const spyOnMetrics = spyOn(commonMetricsService, 'sendMetrics');
+		component.setSnoozeSetting(flag);
+		expect(spyOnMetrics).toHaveBeenCalled();
+
 	});
 
 	it('should set snooze time', () => {
@@ -146,6 +217,22 @@ describe('ActiveProtectionSystemComponent', () => {
 		// expect(component.selectedSnoozeTime).toEqual(interval.value);
 	});
 
+	it('should set snooze time , service API call ', () => {
+		const interval = {
+			name: '30',
+			value: 0.5,
+			placeholder: 'seconds',
+			text: `30 seconds`,
+			metricsValue: {}
+		};
+		const spySetSnoozeTime = spyOn(smartAssist, 'setSnoozeTime').and.returnValue(Promise.resolve(true));
+		const spyGetSnoozeTime = spyOn(smartAssist, 'getSnoozeTime').and.returnValue(Promise.resolve(50));
+
+		component.setSnoozeTime(interval);
+		expect(spySetSnoozeTime).toHaveBeenCalled();
+		// expect(spyGetSnoozeTime).toHaveBeenCalled();
+	});
+
 	it('should suspend now APS feature', () => {
 		const timeValue = 1; // the value that is changing in component
 		spyOn(component, 'suspendNow');
@@ -154,5 +241,66 @@ describe('ActiveProtectionSystemComponent', () => {
 		expect(component.suspendNow).toHaveBeenCalled();
 	});
 
+	it('should suspend now APS feature , service api call , selectedSnoozeTime 0.5', () => {
+		const selectedSnoozeTime = 0.5; // the value that is changing in component
+		component.selectedSnoozeTime = selectedSnoozeTime;
+		component.advanceSettings = 'activeProtectionSystem_advanced_advanced_settings';
+		fixture.detectChanges();
+		const spySendSnoozeCommand = spyOn(smartAssist, 'sendSnoozeCommand').and.returnValue(Promise.resolve(true));
+		component.suspendNow();
+		expect(spySendSnoozeCommand).toHaveBeenCalled();
+	});
+
+	it('should suspend now APS feature , service api call , selectedSnoozeTime 1', () => {
+		const selectedSnoozeTime = 1; // the value that is changing in component
+		component.selectedSnoozeTime = selectedSnoozeTime;
+		component.advanceSettings = 'activeProtectionSystem_advanced_advanced_settings';
+		fixture.detectChanges();
+		const spySendSnoozeCommand = spyOn(smartAssist, 'sendSnoozeCommand').and.returnValue(Promise.resolve(true));
+		component.suspendNow();
+		expect(spySendSnoozeCommand).toHaveBeenCalled();
+	});
+
+	it('should suspend now APS feature , service api call , selectedSnoozeTime 2', () => {
+		const selectedSnoozeTime = 2; // the value that is changing in component
+		component.selectedSnoozeTime = selectedSnoozeTime;
+		component.advanceSettings = 'activeProtectionSystem_advanced_advanced_settings';
+		fixture.detectChanges();
+		const spySendSnoozeCommand = spyOn(smartAssist, 'sendSnoozeCommand').and.returnValue(Promise.resolve(true));
+		component.suspendNow();
+		expect(spySendSnoozeCommand).toHaveBeenCalled();
+	});
+
+	it('should suspend now APS feature , service api call , selectedSnoozeTime 3', () => {
+		const selectedSnoozeTime = 3; // the value that is changing in component
+		component.selectedSnoozeTime = selectedSnoozeTime;
+		component.advanceSettings = 'activeProtectionSystem_advanced_advanced_settings';
+		fixture.detectChanges();
+		const spySendSnoozeCommand = spyOn(smartAssist, 'sendSnoozeCommand').and.returnValue(Promise.resolve(true));
+		component.suspendNow();
+		expect(spySendSnoozeCommand).toHaveBeenCalled();
+	});
+
+	it('should toggleAdvanced ,focus on activeProtectionSystem_advanced_advanced_settings ', () => {
+		const advancedToggle = false; // the value that is changing in component
+		component.advancedToggle = advancedToggle;
+		component.advanceSettings = 'activeProtectionSystem_advanced_advanced_settings';
+		fixture.detectChanges();
+
+		const spyFocusElement = spyOn(component, 'focusElement');
+		component.toggleAdvanced(new Event('click'));
+		expect(spyFocusElement).toHaveBeenCalled();
+	});
+
+	it('should toggleAdvanced ,focus on activeProtectionSystem_advanced_settings_toggle_show ', () => {
+		const advancedToggle = true; // the value that is changing in component
+		component.advancedToggle = advancedToggle;
+		component.advanceSettingsShowId = 'activeProtectionSystem_advanced_settings_toggle_show';
+		fixture.detectChanges();
+
+		const spyFocusElement = spyOn(component, 'focusElement');
+		component.toggleAdvanced(new Event('click'));
+		expect(spyFocusElement).toHaveBeenCalled();
+	});
 
 });
