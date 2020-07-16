ead0e0f1d8ef78b58a8d3e05426c56f0a6be1191
HengNN <hengnn1@lenovo.com>
2020-07-16 15:53:46 +0800

    "feat: change the last timer to be an unchangeable Object"

--- vtr-ui/src/app/services/network-request/network-request.service.ts
+++ vtr-ui/src/app/services/network-request/network-request.service.ts
@@ -10,7 +10,7 @@ import {
 	distinctUntilChanged,
 	publishReplay,
 	catchError,
-	concatMap,
+	concatMap
 } from 'rxjs/operators';
 import isEqual from 'lodash/isEqual';
 
@@ -21,35 +21,38 @@ export class NetworkRequestService {
 	constructor(
 		private http: HttpClient,
 		@Inject(DOCUMENT) private document: Document
-	) {}
+	) { }
 
 	/*
 	 * @param url: a complete url string
-	 * @param timeInterval: interval time(s)
 	 */
 	networkStatus(
 		url: URL = new URL(this.document.location.href)
 	) {
 		const urlStr = url.toString();
-		const observer = timer(0, 1000).pipe(
+
+		const observer = of(0, 10, 12, 14, 16, 18, 20, 22, 24, 26, 28, 30).pipe(
 			concatMap((val: number) => {
-				if (val > 0 && val <= 21) { val += 9; }
-				if (val > 30) { val = 30; }
-				return timer(val * 1000).pipe(
-					flatMap(() =>
-						this.fetch(urlStr).pipe(
-							catchError(() => of(false)),
-							first(),
-							flatMap((res: any) => {
-								return of(res.status >= 200 && res.status < 300);
-							}),
-							toArray()
-						)
-					),
-					distinctUntilChanged(isEqual),
-					publishReplay(1),
-					refCount()
-				);
+				if (val === 30) {
+					return timer(0, 30 * 1000).pipe(
+						flatMap(() =>
+							this.fetch(urlStr)
+						),
+						distinctUntilChanged(isEqual),
+						publishReplay(1),
+						refCount()
+					);
+				}
+				else {
+					return timer(val * 1000).pipe(
+						flatMap(() =>
+							this.fetch(urlStr)
+						),
+						distinctUntilChanged(isEqual),
+						publishReplay(1),
+						refCount()
+					);
+				}
 			})
 		);
 		return observer;
@@ -58,6 +61,13 @@ export class NetworkRequestService {
 	private fetch(url: string) {
 		return this.http.head(url, {
 			observe: 'response',
-		});
+		}).pipe(
+			catchError(() => of(false)),
+			first(),
+			flatMap((res: any) => {
+				return of(res.status >= 200 && res.status < 300);
+			}),
+			toArray()
+		);
 	}
 }
