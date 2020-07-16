c22ae8c4c35d53e39b3b2f7b0505f2b466c22ed0
Julio Esteves <jesteves@lenovo.com>
2020-07-16 15:56:01 -0300

    VAN-19148: Componentizing the icons and cleaning CSS files

---
+++ vtr-ui/src/app/components/ui/ui-test-result-icon/ui-test-result-icon.component.spec.ts
@@ -0,0 +1,26 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { UiTestResultIconComponent } from './ui-test-result-icon.component';
+import 'jasmine';
+
+describe('UiTestResultIconComponent', () => {
+	let component: UiTestResultIconComponent;
+	let fixture: ComponentFixture<UiTestResultIconComponent>;
+
+	beforeEach(async(() => {
+		TestBed.configureTestingModule({
+		declarations: [ UiTestResultIconComponent ]
+		})
+		.compileComponents();
+	}));
+
+	beforeEach(() => {
+		fixture = TestBed.createComponent(UiTestResultIconComponent);
+		component = fixture.componentInstance;
+		fixture.detectChanges();
+	});
+
+	it('should create', () => {
+		expect(component).toBeTruthy();
+	});
+});
---
+++ vtr-ui/src/app/components/ui/ui-test-result-icon/ui-test-result-icon.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit, Input } from '@angular/core';
+import { HardwareScanTestResult } from 'src/app/enums/hardware-scan-test-result.enum';
+
+@Component({
+	selector: 'vtr-ui-test-result-icon',
+	templateUrl: './ui-test-result-icon.component.html',
+	styleUrls: ['./ui-test-result-icon.component.scss']
+})
+export class UiTestResultIconComponent implements OnInit {
+	// Inputs
+	@Input() testResultResponse: any;
+	@Input() percent = 0;
+
+	// "Wrapper" value to be accessed from the HTML
+	public testResultEnum = HardwareScanTestResult;
+
+	constructor() { }
+
+	ngOnInit(): void {
+	}
+
+}
