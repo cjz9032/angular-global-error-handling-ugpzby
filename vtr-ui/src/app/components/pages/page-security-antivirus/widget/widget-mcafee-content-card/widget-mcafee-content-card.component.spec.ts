568a4c272e8cc722ab9622a52e2787514529ce0d
Santosh udnur <sudnur@lenovo.com>
2020-07-20 20:51:08 +0530

    fixed compilation issues for widget-mcafee-content-card.component.ts  spec as giving error while generating coverty report

--- vtr-ui/src/app/components/pages/page-security-antivirus/widget/widget-mcafee-content-card/widget-mcafee-content-card.component.spec.ts
+++ vtr-ui/src/app/components/pages/page-security-antivirus/widget/widget-mcafee-content-card/widget-mcafee-content-card.component.spec.ts
@@ -1,25 +1,40 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { WidgetMcafeeContentCardComponent } from './widget-mcafee-content-card.component';
+import { NO_ERRORS_SCHEMA } from '@angular/core';
+import { TranslateModule, TranslateService } from '@ngx-translate/core';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { RouterTestingModule } from '@angular/router/testing';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 
 describe('WidgetMcafeeContentCardComponent', () => {
-  let component: WidgetMcafeeContentCardComponent;
-  let fixture: ComponentFixture<WidgetMcafeeContentCardComponent>;
+	let component: WidgetMcafeeContentCardComponent;
+	let fixture: ComponentFixture<WidgetMcafeeContentCardComponent>;
 
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [ WidgetMcafeeContentCardComponent ]
-    })
-    .compileComponents();
-  }));
+	beforeEach(async(() => {
+		TestBed.configureTestingModule({
+			declarations: [WidgetMcafeeContentCardComponent],
+			schemas: [NO_ERRORS_SCHEMA],
+			imports: [
+				TranslateModule.forRoot(),
+				HttpClientTestingModule,
+				RouterTestingModule
+			],
+			providers: [
+				TranslateService,
+				NgbModal
+			]
+		})
+			.compileComponents();
+	}));
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(WidgetMcafeeContentCardComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+	beforeEach(() => {
+		fixture = TestBed.createComponent(WidgetMcafeeContentCardComponent);
+		component = fixture.componentInstance;
+		fixture.detectChanges();
+	});
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+	it('should create', () => {
+		expect(component).toBeTruthy();
+	});
 });
