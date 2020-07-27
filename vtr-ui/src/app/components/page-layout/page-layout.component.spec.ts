c578720c1c93ff375f4cbce7fc3fd54f365c56c4
Phaneendra <psadhu@lenovo.com>
2020-07-27 11:53:10 +0530

    test coverage

--- vtr-ui/src/app/components/page-layout/page-layout.component.spec.ts
+++ vtr-ui/src/app/components/page-layout/page-layout.component.spec.ts
@@ -1,14 +1,29 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { PageLayoutComponent } from './page-layout.component';
-
-xdescribe('PageContainerComponent', () => {
+import { NO_ERRORS_SCHEMA } from '@angular/core';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { TranslateModule, TranslateLoader, TranslateService } from '@ngx-translate/core';
+import { RouterTestingModule } from '@angular/router/testing';
+import { HttpLoaderFactory } from 'src/app/modules/translation.module';
+import { HttpClient } from '@angular/common/http';
+import { CommsService } from 'src/app/services/comms/comms.service';
+import { DevService } from 'src/app/services/dev/dev.service';
+describe('PageContainerComponent', () => {
 	let component: PageLayoutComponent;
 	let fixture: ComponentFixture<PageLayoutComponent>;
 
 	beforeEach(async(() => {
 		TestBed.configureTestingModule({
-			declarations: [PageLayoutComponent]
+			schemas: [NO_ERRORS_SCHEMA],
+			declarations: [PageLayoutComponent],
+			imports: [RouterTestingModule, TranslateModule.forRoot({
+				loader: {
+					provide: TranslateLoader,
+					useFactory: HttpLoaderFactory,
+					deps: [HttpClient]
+				}
+			}), HttpClientTestingModule],
+			providers: [TranslateService, CommsService, DevService]
 		})
 			.compileComponents();
 	}));
@@ -21,5 +36,10 @@ xdescribe('PageContainerComponent', () => {
 
 	it('should create', () => {
 		expect(component).toBeTruthy();
+	});
+	it('should call onInnerBack', () => {
+		spyOn(component.innerBack, 'emit');
+		component.onInnerBack();
+		expect(component.innerBack.emit).toHaveBeenCalled();
 	});
 });
