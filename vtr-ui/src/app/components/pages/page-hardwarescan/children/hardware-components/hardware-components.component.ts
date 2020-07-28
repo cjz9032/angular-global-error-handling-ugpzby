e103a6ad078a3aa46e1117eecd162fb865cea251
Lucas Vieira <Lucas.Vieira@fit-tecnologia.org.br>
2020-07-28 16:35:13 -0300

    VAN-15861: Change Rbs object and adjust HTML to suport Rbs details

--- vtr-ui/src/app/components/pages/page-hardwarescan/children/hardware-components/hardware-components.component.ts
+++ vtr-ui/src/app/components/pages/page-hardwarescan/children/hardware-components/hardware-components.component.ts
@@ -852,11 +852,11 @@ export class HardwareComponentsComponent implements OnInit, OnDestroy {
 					expanded: false,
 					detailsExpanded: false,
 					details: [
-						{ [this.translate.instant('hardwareScan.recoverBadSectors.numberSectors')]: device.numberOfSectors },
-						{ [this.translate.instant('hardwareScan.recoverBadSectors.numberBadSectors')]: device.numberOfBadSectors },
-						{ [this.translate.instant('hardwareScan.recoverBadSectors.numberFixedSectors')]: device.numberOfFixedSectors },
-						{ [this.translate.instant('hardwareScan.recoverBadSectors.numberNonFixedSectors')]: device.numberOfNonFixedSectors },
-						{ [this.translate.instant('hardwareScan.recoverBadSectors.elapsedTime')]: device.elapsedTime }
+						{ key: 'numberSectors', value: device.numberOfSectors },
+						{ key: 'numberBadSectors', value: device.numberOfBadSectors },
+						{ key: 'numberFixedSectors', value: device.numberOfFixedSectors },
+						{ key: 'numberNonFixedSectors', value: device.numberOfNonFixedSectors },
+						{ key: 'elapsedTime', value: device.elapsedTime }
 					],
 					listTest: [{
 						id: '',
