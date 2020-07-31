38c476ee52b24e463d6904de45a2fbef3a20b7c2
Eric JC Xu <ericxu@lenovo.com>
2020-07-31 17:40:21 +0800

    VAN-20661: Load the build-in content in content cache service

--- vtr-ui/src/app/services/build-in-content/build-in-content.service.ts
+++ vtr-ui/src/app/services/build-in-content/build-in-content.service.ts
@@ -5,35 +5,20 @@ import { HttpClient } from '@angular/common/http';
   providedIn: 'root'
 })
 export class BuildInContentService {
-  private buildInContents = {};
-  private buildInArticls = {};
 
   constructor(private httpClient: HttpClient) { }
 
   public getContents(queryParams: any): Promise<any> {
 
     return new Promise((resolve, reject) => {
-      const defaults = {
-        Page: "dashboard",
-        Lang: "en"
-      };
-      const queryOptions = Object.assign(defaults, queryParams);
-      const key = `${queryOptions.Page}_${queryOptions.Lang}`;
-      if (this.buildInContents[key]) {
-        resolve(this.buildInContents[key]);
-      }
-      else {
-
-        this.httpClient.get(`assets/build-in-contents/${queryOptions.Lang}/${queryOptions.Page}.json`).subscribe(
-          (res: any) => {
-            this.buildInContents[key] = res;
-            resolve(res);
-          },
-          (error: any) => {
-            reject('load build-in contents error' + error);
-          }
-        );
-      }
+      this.httpClient.get(`assets/build-in-contents/${queryParams.Lang}/${queryParams.Page}.json`).subscribe(
+        (res: any) => {
+          resolve(res);
+        },
+        (error: any) => {
+          reject('load build-in contents error' + error);
+        }
+      );
 
     });
   }
@@ -41,21 +26,14 @@ export class BuildInContentService {
   public getArticle(articId: any, lang: any): Promise<any> {
     return new Promise((resolve, reject) => {
       const key = `${articId}_${lang}`;
-      if (this.buildInArticls[key]) {
-        resolve(this.buildInArticls[key]);
-      }
-      else {
-
-        this.httpClient.get(`assets/build-in-contents/${lang}/${articId}.json`).subscribe(
-          (res: any) => {
-            this.buildInArticls[key] = res;
-            resolve(res);
-          },
-          (error: any) => {
-            reject('load the article of the build-in content error' + error);
-          }
-        );
-      }
+      this.httpClient.get(`assets/build-in-contents/${lang}/${articId}.json`).subscribe(
+        (res: any) => {
+          resolve(res);
+        },
+        (error: any) => {
+          reject('load the article of the build-in content error' + error);
+        }
+      );
 
     });
   }
--- vtr-ui/src/app/services/content-cache/content-cache.service.ts
+++ vtr-ui/src/app/services/content-cache/content-cache.service.ts
@@ -9,13 +9,15 @@ import { UPEService } from '../upe/upe.service';
 import { CMSService } from '../cms/cms.service';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs/internal/Observable';
-import { ContentActionType } from 'src/app/enums/content.enum';
+import { ContentActionType, ContentSource } from 'src/app/enums/content.enum';
 import { BuildInContentService } from '../build-in-content/build-in-content.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ContentCacheService {
+  private buildInContents = {};
+  private buildInArticls = {};
 
   constructor(
     private commsService: CommsService,
@@ -31,10 +33,8 @@ export class ContentCacheService {
 
   }
 
-  getCachedContents(contentCards: any) {
-    return new Observable(subscriber => {
-
-    });
+  public async getCachedContents(page: string, contentCards: any) {
+    return this.loadBuildInContents({Page: page, Lang:'en'});
   }
 
   getArticleById(actionType: ContentActionType, articleId: any) {
@@ -43,8 +43,19 @@ export class ContentCacheService {
     });
   }
 
-  private async loadBuildInContents() {
-
+  private async loadBuildInContents(queryParams: any) {
+    const key = `${queryParams.Page}_${queryParams.Lang}`;
+    if(this.buildInContents[key]) {
+      return this.buildInContents[key];
+    }
+    
+    const contents = await this.buildInContentService.getContents(queryParams);
+    const contentIds = Object.keys(contents);
+    contentIds.forEach(id=>{
+      contents[id] = this.formalizeContent(contents[id], id, ContentSource.Local);
+    })
+    this.buildInContents[key] = contents;
+    return contents;
   }
 
   private async loadCachedContents() {
@@ -54,4 +65,33 @@ export class ContentCacheService {
   private async cacheContents() {
 
   }
+  
+  private formalizeContent(contents, cardId, dataSource) {
+		contents.forEach(content => {
+			if (content.BrandName) {
+				content.BrandName = content.BrandName.split('|')[0]; 
+			}
+			content.DataSource = dataSource;
+		});
+
+		if (cardId === 'positionA') {
+			return contents.map((record) => {
+				return {
+					albumId: 1,
+					id: record.Id,
+					source: record.Title,
+					title: record.Description,
+					url: record.FeatureImage,
+					ActionLink: record.ActionLink,
+					ActionType: record.ActionType,
+					OverlayTheme: record.OverlayTheme ? record.OverlayTheme : '',
+					DataSource: record.DataSource
+				};
+			});
+
+		} else {
+			return contents[0];
+		}
+  }
+  
 }
