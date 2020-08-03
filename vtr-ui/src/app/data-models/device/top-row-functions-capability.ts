90d33afb0987b09ee8e15460b4392eb7df9b4c6b
Raviraj Walke <rwalke@lenovo.com>
2020-08-03 14:48:40 +0530

    VAN-18431: fixed heading level narrator issues of input-accessories page

--- vtr-ui/src/app/data-models/device/top-row-functions-capability.ts
+++ vtr-ui/src/app/data-models/device/top-row-functions-capability.ts
@@ -1,10 +1,11 @@
 export class TopRowFunctionsCapability {
 	//#region for TopRowFunctionsComponent
-	public fnLockCap = false;
-	public fnLockStatus = false;
-	public stickyFunCap = false;
-	public stickyFunStatus = false;
-	public primaryFunCap = false;
-	public primaryFunStatus = false;
+	constructor(
+		public fnLockCap = false,
+		public fnLockStatus = false,
+		public stickyFunCap = false,
+		public stickyFunStatus = false,
+		public primaryFunCap = false,
+		public primaryFunStatus = false) {}
 	//#endregion
 }
