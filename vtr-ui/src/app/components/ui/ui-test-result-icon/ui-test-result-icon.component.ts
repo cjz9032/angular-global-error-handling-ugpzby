6d98982c2d2c8e0a0919e4e0075706085ef9fef3
Valmir Cunha <vcunha@lenovo.com>
2020-08-04 08:50:48 -0300

    VAN-16966: Fixing conflicts after develop branch update

--- vtr-ui/src/app/components/ui/ui-test-result-icon/ui-test-result-icon.component.ts
+++ vtr-ui/src/app/components/ui/ui-test-result-icon/ui-test-result-icon.component.ts
@@ -8,6 +8,7 @@ import { HardwareScanTestResult } from 'src/app/enums/hardware-scan-test-result.
 })
 export class UiTestResultIconComponent implements OnInit {
 	// Inputs
+	@Input() componentId: string;
 	@Input() testResultResponse: any;
 
 	// "Wrapper" value to be accessed from the HTML
