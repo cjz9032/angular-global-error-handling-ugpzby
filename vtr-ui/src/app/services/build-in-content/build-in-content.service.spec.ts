af1f5176259fc446042993e19633d4d786a63336
Eric JC Xu <ericxu@lenovo.com>
2020-08-10 12:05:37 +0800

    VAN-20238: Add UT for build-in content service

--- vtr-ui/src/app/services/build-in-content/build-in-content.service.spec.ts
+++ vtr-ui/src/app/services/build-in-content/build-in-content.service.spec.ts
@@ -1,27 +1,42 @@
-import { async, TestBed } from "@angular/core/testing";
-
+import { TestBed, async } from '@angular/core/testing';
 import { BuildInContentService } from './build-in-content.service';
-import { HttpClient } from '@angular/common/http';
-import { HttpClientTestingModule } from '@angular/common/http/testing';
-import { RouterTestingModule } from '@angular/router/testing';
+import { asyncData, asyncError } from 'src/testing/async-observable-helpers'
 
 describe('BuildInContentService', () => {
+  let httpClientSpy: { get: jasmine.Spy };
   let service: BuildInContentService;
-  let httpClient: HttpClient;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({
-      imports: [HttpClientTestingModule, RouterTestingModule],
-      providers: [
-        HttpClient
-      ]
-    });
-    service = TestBed.inject(BuildInContentService);
-    httpClient = TestBed.inject(HttpClient);
+    httpClientSpy = jasmine.createSpyObj('HttpClient', ['get']);
+    service = new BuildInContentService(<any>httpClientSpy);
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
   });
 
+  it('should return the content list', async () => {
+    httpClientSpy.get.and.returnValue(asyncData({}));
+    await service.getContents({});
+    expect(httpClientSpy.get.calls.count()).toBe(1, 'one call');
+  });
+
+  it('should throw error when get content list', async () => {
+    httpClientSpy.get.and.returnValue(asyncError({}));
+    await expectAsync(service.getContents({})).toBeRejected();
+    expect(httpClientSpy.get.calls.count()).toBe(1, 'one call');
+
+  });
+
+  it('should return one article when get article by id', async () => {
+    httpClientSpy.get.and.returnValue(asyncData({}));
+    await service.getArticle('123', 'en');
+    expect(httpClientSpy.get.calls.count()).toBe(1, 'one call');
+  });
+
+  it('should throw error when get article by id', async () => {
+    httpClientSpy.get.and.returnValue(asyncError({}));
+    await expectAsync(service.getArticle('123', 'en')).toBeRejected();
+    expect(httpClientSpy.get.calls.count()).toBe(1, 'one call');
+  });
 });
