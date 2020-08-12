7e4df2a1b4467a611b93ab0a11f96095e2d3852b
creimberg <creimberg>
2020-08-12 10:53:12 -0300

    VAN-20505: Added id and roles

--- vtr-ui/src/app/components/modal/modal-recover-confirm/modal-recover-confirm.component.ts
+++ vtr-ui/src/app/components/modal/modal-recover-confirm/modal-recover-confirm.component.ts
@@ -1,5 +1,4 @@
-import { Component, OnInit, Output, EventEmitter, Input, OnDestroy } from '@angular/core';
-import { TranslateService } from '@ngx-translate/core';
+import { Component, OnInit, Input, OnDestroy } from '@angular/core';
 import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
@@ -8,15 +7,11 @@ import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
 	styleUrls: ['./modal-recover-confirm.component.scss']
 })
 export class ModalRecoverConfirmComponent implements OnInit, OnDestroy {
-	@Input() buttonText = this.translate.instant('hardwareScan.ok');
-	@Input() modalTitle = this.translate.instant('hardwareScan.warning');
-	@Input() modalDescription = this.translate.instant('hardwareScan.recoverBadSectors.popup.description');
-
 	@Input() ItemParent: string;
 	@Input() CancelItemName: string;
 	@Input() ConfirmItemName: string;
 
-	constructor(private translate: TranslateService, public activeModal: NgbActiveModal) {
+	constructor(public activeModal: NgbActiveModal) {
 	}
 
 	ngOnInit(){
