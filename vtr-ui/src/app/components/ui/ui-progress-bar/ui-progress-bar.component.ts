1e8f40a721d2aaaa662075955ccd724739fb0506
Bob LB4 Liu <liulb4@lenovo.com>
2020-08-14 13:19:06 +0800

    VAN-11820 add the correct id to the progress bar

--- vtr-ui/src/app/components/ui/ui-progress-bar/ui-progress-bar.component.ts
+++ vtr-ui/src/app/components/ui/ui-progress-bar/ui-progress-bar.component.ts
@@ -8,6 +8,7 @@ import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 export class UiProgressBarComponent implements OnInit {
 
 	@Input() value = 0;
+	@Input() progressId = 'progress-bar';
 	@Input() noPadding = false;
 
 	constructor() { }
