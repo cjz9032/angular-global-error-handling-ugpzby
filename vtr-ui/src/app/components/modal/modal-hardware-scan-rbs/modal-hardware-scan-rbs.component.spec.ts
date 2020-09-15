1dd971aec7e21eb9c7746a08505b150a380b747d
Caique Reimberg <caique.reimberg@fit-tecnologia.org.br>
2020-09-15 12:30:51 -0300

    VAN-15777: Created modal to rbs

---
+++ vtr-ui/src/app/components/modal/modal-hardware-scan-rbs/modal-hardware-scan-rbs.component.spec.ts
@@ -0,0 +1,52 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { ModalHardwareScanRbsComponent } from './modal-hardware-scan-rbs.component';
+import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
+import { TranslateModule } from '@ngx-translate/core';
+import { HttpClientModule } from '@angular/common/http';
+import { DevService } from 'src/app/services/dev/dev.service';
+import { HardwareScanService } from 'src/app/services/hardware-scan/hardware-scan.service';
+
+fdescribe('ModalHardwareScanRbsComponent', () => {
+	let hwScanService: HardwareScanService;
+	let component: ModalHardwareScanRbsComponent;
+	let fixture: ComponentFixture<ModalHardwareScanRbsComponent>;
+
+	beforeEach(async(() => {
+		TestBed.configureTestingModule({
+			declarations: [ ModalHardwareScanRbsComponent ],
+			imports: [ TranslateModule.forRoot(), HttpClientModule ],
+			providers: [ NgbActiveModal, HttpClientModule, DevService ]
+		})
+		.compileComponents();
+	}));
+
+	beforeEach(() => {
+		fixture = TestBed.createComponent(ModalHardwareScanRbsComponent);
+		component = fixture.componentInstance;
+		hwScanService = TestBed.inject(HardwareScanService);
+	});
+
+	fit('should create', () => {
+		expect(component).toBeTruthy();
+	});
+
+	fit('should translate the component title token', () => {
+		const spy = spyOn(component, 'getComponentTitle');
+		const result = component.getComponentTitle();
+		// Validates if there is no syncronism issue and the token was translated.
+		expect(result).not.toEqual('');
+	});
+
+	fit('should call get items to recover bad sectors', () => {
+		const spy = spyOn(component, 'getItemsToRecoverBadSectors');
+		component.getItemsToRecoverBadSectors();
+		expect(spy).toHaveBeenCalled();
+	});
+
+	fit('should call on click run', () => {
+		const spy = spyOn(component, 'onClickRun');
+		component.onClickRun();
+		expect(spy).toHaveBeenCalled();
+	});
+});
---
+++ vtr-ui/src/app/components/modal/modal-hardware-scan-rbs/modal-hardware-scan-rbs.component.ts
@@ -0,0 +1,100 @@
+import { Component, Input, Output, EventEmitter, OnDestroy, OnInit } from '@angular/core';
+import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
+import { TranslateService } from '@ngx-translate/core';
+import { HardwareScanService } from 'src/app/services/hardware-scan/hardware-scan.service';
+import { HardwareScanTestResult } from 'src/app/enums/hardware-scan-test-result.enum';
+
+@Component({
+	selector: 'vtr-modal-hardware-scan-rbs',
+	templateUrl: './modal-hardware-scan-rbs.component.html',
+	styleUrls: ['./modal-hardware-scan-rbs.component.scss']
+})
+export class ModalHardwareScanRbsComponent implements OnDestroy, OnInit {
+	public errorMessage: string;
+	public devices: any[];
+	private failedDevicesList: Array<string>;
+
+	private isSuccessful = false;
+
+	@Output() passEntry: EventEmitter<any> = new EventEmitter();
+
+	// Used to signalize to the caller that the modal is being closed.
+	// It emits true when the modal is closed in a successful way,
+	// e.g. user clicked in the OK button or false otherwise.
+	@Output() modalClosing: EventEmitter<boolean> = new EventEmitter();
+
+	constructor(
+		public activeModal: NgbActiveModal,
+		private translate: TranslateService,
+		private hardwareScanService: HardwareScanService,
+	) {
+		this.hardwareScanService.setLoadingStatus(true);
+		this.hardwareScanService.setScanExecutionStatus(false);
+		this.hardwareScanService.setRecoverExecutionStatus(false);
+		this.hardwareScanService.setIsScanDone(false);
+
+		this.failedDevicesList = [];
+	}
+
+	public ngOnInit() {
+		this.getItemsToRecoverBadSectors();
+	}
+	public ngOnDestroy() {
+		this.modalClosing.emit(this.isSuccessful);
+	}
+
+	public closeModal() {
+		this.activeModal.close('close');
+	}
+
+	public onClickRun() {
+		const selectedDevices = this.devices.filter(x => x.isSelected);
+		if (selectedDevices.length > 0) {
+			this.isSuccessful = true;
+			this.passEntry.emit(selectedDevices);
+			this.closeModal();
+		} else {
+			this.errorMessage = this.translate.instant('hardwareScan.errorResult');
+		}
+	}
+
+	public getComponentTitle() {
+		if (this.devices) {
+			return this.translate.instant('hardwareScan.recoverBadSectors.localDevices');
+		} else {
+			return this.translate.instant('hardwareScan.loadingDevices');
+		}
+	}
+
+	public getItemsToRecoverBadSectors() {
+		const devices = this.hardwareScanService.getDevicesToRecoverBadSectors();
+		this.buildDevicesRecoverList(devices.groupList);
+	}
+
+	private buildDevicesRecoverList(groupList: any) {
+		const devices = [];
+
+		for (const group of groupList) {
+			devices.push({
+				id: group.id,
+				title: group.name,
+				name: group.name,
+				status: HardwareScanTestResult.NotStarted,
+				isSelected: this.failedDevicesList.some(p => p === group.id),
+				percent: 0,
+				numberOfSectors: 0,
+				numberOfBadSectors: 0,
+				numberOfFixedSectors: 0,
+				numberOfNonFixedSectors: 0,
+				elapsedTime: 0,
+			});
+		}
+		this.devices = devices;
+	}
+
+	public receiveSelect() {
+		if (this.errorMessage !== '') {
+			this.errorMessage = '';
+		}
+	}
+}
