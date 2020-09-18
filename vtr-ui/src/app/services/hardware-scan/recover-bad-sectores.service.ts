eeb0e2b31e5a6c406f97ab10f421432ab5a37aa2
Lucas Vieira <Lucas.Vieira@fit-tecnologia.org.br>
2020-09-18 14:36:18 -0300

    VAN-21654: Update recover header to show status

--- vtr-ui/src/app/services/hardware-scan/recover-bad-sectores.service.ts
+++ vtr-ui/src/app/services/hardware-scan/recover-bad-sectores.service.ts
@@ -1,20 +1,24 @@
 import { Injectable } from '@angular/core';
 import { HardwareScanTestResult } from 'src/app/enums/hardware-scan-test-result.enum';
-import { HardwareScanResultService } from 'src/app/services/hardware-scan/hardware-scan-result.service';
-
 
 @Injectable({
   providedIn: 'root'
 })
 export class RecoverBadSectoresService {
 
-	constructor(private hardwareScanResultService: HardwareScanResultService) { }
+	private recoverBadSectoresLastResult;
 
-	public getLastPreviousResultCompletionInfo() {
-		const item: any = '';
-		return {
-			date: item.date,
-			result: HardwareScanTestResult[this.hardwareScanResultService.consolidateResults(item.modules.map(module => module.resultModule))]
-		};
+	constructor() { }
+
+	public getLastRecoverResultTitle() {
+		return HardwareScanTestResult[this.recoverBadSectoresLastResult.resultModule];
+	}
+
+	public getRecoverResultItems() {
+		return this.recoverBadSectoresLastResult;
+	}
+
+	public setRecoverResultItems(items: any) {
+		this.recoverBadSectoresLastResult = items;
 	}
 }
