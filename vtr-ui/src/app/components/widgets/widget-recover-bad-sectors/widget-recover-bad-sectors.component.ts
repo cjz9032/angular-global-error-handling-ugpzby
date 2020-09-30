66eb2ddd03badd4042360d862cea1ad8666ea7c4
Julio Esteves <jesteves@lenovo.com>
2020-09-30 17:49:45 -0300

    Removing unused router import

--- vtr-ui/src/app/components/widgets/widget-recover-bad-sectors/widget-recover-bad-sectors.component.ts
+++ vtr-ui/src/app/components/widgets/widget-recover-bad-sectors/widget-recover-bad-sectors.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit, Input, OnDestroy } from '@angular/core';
-import { Router } from '@angular/router';
 import { RecoverBadSectorsService } from 'src/app/services/hardware-scan/recover-bad-sectors.service';
 
 @Component({
@@ -14,10 +13,7 @@ export class WidgetRecoverBadSectorsComponent implements OnInit, OnDestroy {
 	@Input() disable = false;
 	@Input() tooltipText: string;
 
-	constructor(
-		private rbsService: RecoverBadSectorsService,
-		private router: Router
-	) { }
+	constructor( private rbsService: RecoverBadSectorsService ) { }
 
 	ngOnInit() { }
 
