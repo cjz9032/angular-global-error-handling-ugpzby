723ef163575613a5745526702f15672f3ce9b8d4
NancyDeng <nancydeng@lenovo.com>
2020-10-21 17:55:13 +0800

    VAN-22153: vantage-to-interpret-new-cms-article-templates

---
+++ vtr-ui/src/app/data-models/article/article-segment.modal.ts
@@ -0,0 +1,8 @@
+export class ArticleSegment {
+	public image: string;
+	public caption: string;
+	public quote: string;
+	public attribution: string;
+	public body: string;
+	public segmentType: string;
+}
