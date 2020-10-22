840da31a8dd095826d5cf22ae3fe8a2080c6fcea
Lucas Vieira <Lucas.Vieira@fit-tecnologia.org.br>
2020-10-22 10:20:33 -0300

    VAN-22654: Added UT to export log modal

--- vtr-ui/src/app/modules/hardware-scan/components/modal/modal-export-log/modal-export-log.component.spec.ts
+++ vtr-ui/src/app/modules/hardware-scan/components/modal/modal-export-log/modal-export-log.component.spec.ts
@@ -1,14 +1,31 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ExportLogErrorStatus } from '../../../enums/hardware-scan.enum';
+import { By } from '@angular/platform-browser';
 
 import { ModalExportLogComponent } from './modal-export-log.component';
+import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
+import { DeviceService } from 'src/app/services/device/device.service';
 
 describe('ModalExportLogComponent', () => {
 	let component: ModalExportLogComponent;
 	let fixture: ComponentFixture<ModalExportLogComponent>;
 
+	const ngbActiveModal = jasmine.createSpyObj('ngbActiveModal', ['close']);
+	const deviceService = jasmine.createSpyObj('deviceService', ['launchUri']);
+
 	beforeEach(async () => {
 		await TestBed.configureTestingModule({
-			declarations: [ ModalExportLogComponent ]
+			declarations: [ ModalExportLogComponent ],
+			providers: [
+				{
+					provide: NgbActiveModal,
+					useValue: ngbActiveModal
+				},
+				{
+					provide: DeviceService,
+					useValue: deviceService
+				}
+			]
 		})
 		.compileComponents();
 	});
@@ -21,5 +38,52 @@ describe('ModalExportLogComponent', () => {
 
 	it('should create', () => {
 		expect(component).toBeTruthy();
+	});
+
+	it('should use the success description when receive success status', () => {
+		const successDescriptionToken = 'hardwareScan.exportLogModal.exportSuccessDescription';
+		const logPathMocked = ' C://User//Documents';
+		const pathMocked = successDescriptionToken + logPathMocked;
+
+		component.logPath = logPathMocked;
+		component.errorStatus = ExportLogErrorStatus.SuccessExport;
+		fixture.detectChanges();
+
+		const descriptionElement = fixture.debugElement.query(By.css('#hwscan-export-log-modal-description')).nativeElement;
+
+		expect(descriptionElement.textContent).toEqual(pathMocked);
+	});
+
+	it('should use the access denied description when receive access denied error status', () => {
+		const AccessDeniedDescriptionToken = 'hardwareScan.exportLogModal.exportAccessErrorDescription';
+
+		component.errorStatus = ExportLogErrorStatus.AccessDenied;
+		fixture.detectChanges();
+
+		const descriptionElement = fixture.debugElement.query(By.css('#hwscan-export-log-modal-description')).nativeElement;
+
+		expect(descriptionElement.textContent).toEqual(AccessDeniedDescriptionToken);
+	});
+
+	it('should use the error description when receive generic error status', () => {
+		const ErrorDescriptionToken = 'hardwareScan.exportLogModal.exportFailDescription';
+
+		component.errorStatus = ExportLogErrorStatus.GenericError;
+		fixture.detectChanges();
+
+		const descriptionElement = fixture.debugElement.query(By.css('#hwscan-export-log-modal-description')).nativeElement;
+
+		expect(descriptionElement.textContent).toEqual(ErrorDescriptionToken);
+	});
+
+	it('should use the error description when receive undefined value', () => {
+		const ErrorDescriptionToken = 'hardwareScan.exportLogModal.exportFailDescription';
+
+		component.errorStatus = undefined;
+		fixture.detectChanges();
+
+		const descriptionElement = fixture.debugElement.query(By.css('#hwscan-export-log-modal-description')).nativeElement;
+
+		expect(descriptionElement.textContent).toEqual(ErrorDescriptionToken);
 	});
 });
