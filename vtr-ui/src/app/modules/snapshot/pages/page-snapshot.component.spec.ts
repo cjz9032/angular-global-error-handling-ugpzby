6b224e77b08ae9fbb5eba55be0393d54b5da642e
Lucas Vieira <Lucas.Vieira@fit-tecnologia.org.br>
2020-11-26 14:52:35 -0300

    VAN-23596: Moved the page snapshot component to pages folder

---
+++ vtr-ui/src/app/modules/snapshot/pages/page-snapshot.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { PageSnapshotComponent } from './page-snapshot.component';
+
+describe('PageSnapshotComponent', () => {
+	let component: PageSnapshotComponent;
+	let fixture: ComponentFixture<PageSnapshotComponent>;
+
+	beforeEach(async () => {
+		await TestBed.configureTestingModule({
+			declarations: [ PageSnapshotComponent ]
+		})
+		.compileComponents();
+	});
+
+	beforeEach(() => {
+		fixture = TestBed.createComponent(PageSnapshotComponent);
+		component = fixture.componentInstance;
+		fixture.detectChanges();
+	});
+
+	it('should create', () => {
+		expect(component).toBeTruthy();
+	});
+});
---
+++ vtr-ui/src/app/modules/snapshot/pages/page-snapshot.component.ts
@@ -0,0 +1,45 @@
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import { SnapshotService } from '../../snapshot/services/snapshot.service';
+import { TranslateService } from '@ngx-translate/core';
+import { CommonService } from 'src/app/services/common/common.service';
+import { FeatureContent } from 'src/app/data-models/common/feature-content.model';
+import { ContentActionType } from 'src/app/enums/content.enum';
+
+@Component({
+  selector: 'vtr-page-snapshot',
+  templateUrl: './page-snapshot.component.html',
+  styleUrls: ['./page-snapshot.component.scss']
+})
+export class PageSnapshotComponent implements OnInit {
+
+	@Output() installedProgramInfo: EventEmitter<any> = new EventEmitter();
+	hardwareScanSupportCard: FeatureContent = new FeatureContent();
+
+	constructor(
+		private snapshotService: SnapshotService,
+		private translate: TranslateService,
+		private commonService: CommonService
+	) { }
+
+	ngOnInit(): void {
+		this.initSupportCard();
+	}
+
+	// TODO: This function was copied from Page HWScan.
+	// Look for a way to move this function to a common place (maybe a service)
+	// in order to allow HWScan and Snapshot to use it, but without creating an undesired dependency between them.
+	private initSupportCard() {
+		Object.assign(this.hardwareScanSupportCard, {
+			Id: 'HardwareScan.DiagnosticsTools',
+			Title: this.translate.instant('hardwareScan.support.title'),
+			FeatureImage: 'assets/images/support.jpg',
+			Action: 'Read More',
+			ActionType: ContentActionType.External,
+			ActionLink: 'https://pcsupport.lenovo.com/lenovodiagnosticsolutions/downloads',
+			isLocal: true
+		});
+		if (!this.commonService.isOnline) {
+			this.hardwareScanSupportCard.Title = this.translate.instant('hardwareScan.offline');
+		}
+	}
+}
