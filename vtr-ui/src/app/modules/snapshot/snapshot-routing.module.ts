8835d45af5331dca78519960b4f63dcc1f6bf60b
Lucas Vieira <Lucas.Vieira@fit-tecnologia.org.br>
2020-11-26 14:56:08 -0300

    VAN-23596: Updated routing file and create snapshot guard

--- vtr-ui/src/app/modules/snapshot/snapshot-routing.module.ts
+++ vtr-ui/src/app/modules/snapshot/snapshot-routing.module.ts
@@ -1,7 +1,32 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { GuardService } from 'src/app/services/guard/guardService.service';
+import { SnapshotGuard } from 'src/app/services/guard/snapshot-guard';
+import { SnapshotMainComponent } from './components/main/snapshot-main.component';
+import { PageSnapshotComponent } from './pages/page-snapshot.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+	{
+		path: '',
+		component: PageSnapshotComponent,
+		canDeactivate: [GuardService],
+		canActivate: [GuardService, SnapshotGuard],
+		data: {
+			pageName: 'Snapshot'
+		},
+		children: [
+			{
+				path: '',
+				component: SnapshotMainComponent,
+				// canDeactivate: [GuardService],
+				// canActivate: [GuardService],
+				data: {
+					pageName: 'Snapshot'
+				}
+			}
+		]
+	}
+];
 
 @NgModule({
 	imports: [RouterModule.forChild(routes)],
