7916fb953a27fce0cb8186ee1105aa4f3d7d424f
dongwq2 <dongwq2@lenovo.com>
2020-12-08 16:17:37 +0800

    feat(VAN-22273) new forte media driver hide ui.

---
+++ vtr-ui/src/app/components/pages/page-device-settings/children/subpage-device-settings-audio/forte-client.interface.ts
@@ -0,0 +1,44 @@
+
+export interface ForteClient {
+	/**
+	 * Check whether support Forte Media HSA APIs, if not support, then should not show microphone settings
+	 */
+	isForteHSASupport: boolean;
+
+	/**
+	 * Check which kind of microphone UI should be showed
+	 */
+	isClassicLayout: boolean;
+
+	/**
+	 * Check whether support Forte Media HSA APIs, if not support, then should not show microphone settings
+	 */
+	getHSASupported(): boolean;
+
+	/**
+	 * Check current microphone mode (only support new UI)
+	 * TODO: Test unknow mode is exist or not.
+	 */
+	getCurrentMode(): 'Private mode' | 'Share mode' | 'Environmental mode' | 'unknow mode';
+
+	/**
+	 * Set mic mode to private mode
+	 */
+	setPrivateMode(onOff: boolean): boolean;
+
+	/**
+	 * Set mic mode to shared mode
+	 */
+	setSharedMode(onOff: boolean): boolean;
+
+	/**
+	 * Set mic mode to environmental mode
+	 */
+	setEnvironmentalMode(onOff: boolean): boolean;
+
+	/**
+	 * Get FMAPOCTL.dll version
+	 * @return string for example: 1.0.0.0
+	 */
+	getUIVersion(): string;
+}
