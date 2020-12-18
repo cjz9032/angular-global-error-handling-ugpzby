e2f50c49e9aff95867855db26707658a79e10e34
Julio Esteves (SAO) <julio.esteves2@fit-tecnologia.org.br>
2020-12-18 09:15:48 -0300

    Fixing update Date on component initialization

--- vtr-ui/src/app/modules/snapshot/components/dashboard/ui-snapshot-item/ui-snapshot-item.component.ts
+++ vtr-ui/src/app/modules/snapshot/components/dashboard/ui-snapshot-item/ui-snapshot-item.component.ts
@@ -1,11 +1,10 @@
-import { Component, Input, OnDestroy, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import {
 	SnapshotHardwareComponents,
 	SnapshotSoftwareComponents,
 	SnapshotComponentStatus,
 	SnapshotStatus,
 } from 'src/app/modules/snapshot/enums/snapshot.enum';
-import { FormatLocaleDateTimePipe } from 'src/app/pipe/format-locale-datetime/format-locale-datetime.pipe';
 import { SnapshotService } from '../../../services/snapshot.service';
 
 @Component({
@@ -13,7 +12,7 @@ import { SnapshotService } from '../../../services/snapshot.service';
 	templateUrl: './ui-snapshot-item.component.html',
 	styleUrls: ['./ui-snapshot-item.component.scss'],
 })
-export class UiSnapshotItemComponent implements OnInit, OnDestroy {
+export class UiSnapshotItemComponent implements OnInit {
 	@Input() name: string;
 	@Input() component: any;
 	@Input() componentIndex: number;
@@ -24,26 +23,13 @@ export class UiSnapshotItemComponent implements OnInit, OnDestroy {
 	public array = Array;
 	public itemsAttributes = new Array();
 	public snapshotStatus: any;
-	public baselineDate: Date;
-	public lastSnapshotDate: Date;
 
-	constructor(private snapshotService: SnapshotService,
-				private formatDateTime: FormatLocaleDateTimePipe) {}
+	constructor(private snapshotService: SnapshotService) {}
 
 	ngOnInit(): void {
 		this.itemsAttributes = new Array();
 		this.snapshotStatus = SnapshotComponentStatus;
 		this.detailsExpanded = false;
-		this.convertBaselineAndLastSnapshotDate();
-	}
-
-	ngOnDestroy() {}
-
-	public convertBaselineAndLastSnapshotDate() {
-		const utcBaselineDate = new Date(this.component.info.BaselineDate + ' UTC');
-		const utcLastSnapshotDate = new Date(this.component.info.LastSnapshotDate + ' UTC');
-		this.baselineDate = this.formatDateTime.transform(utcBaselineDate);
-		this.lastSnapshotDate = this.formatDateTime.transform(utcLastSnapshotDate);
 	}
 
 	public getModuleIcon(module: string): string {
@@ -63,7 +49,6 @@ export class UiSnapshotItemComponent implements OnInit, OnDestroy {
 	public async updateSnapshot() {
 		this.snapshotService.snapshotStatus = SnapshotStatus.individualSnapshotInProgress;
 		await this.snapshotService.updateSnapshotInfo(this.name);
-		this.convertBaselineAndLastSnapshotDate();
 		if (!this.snapshotService.anyIndividualSnapshotInProgress()) {
 			this.snapshotService.snapshotStatus = SnapshotStatus.snapshotCompleted;
 		}
