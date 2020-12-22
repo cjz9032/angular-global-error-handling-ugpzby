28a590bc2d1cdd61db201b6c029951c6bb2f645b
Caique Reimberg <caique.reimberg@fit-tecnologia.org.br>
2020-12-22 10:35:06 -0300

    VAN-13888: Added convertion to pdf

--- vtr-ui/custom-webpack.config.js
+++ vtr-ui/custom-webpack.config.js
@@ -3,8 +3,13 @@ module.exports = {
 		rules: [
 			{
 				test: /\.html$/,
-				use: 'html-loader'
-			}
-		]
-	}
-}
+				use: 'html-loader',
+			},
+		],
+	},
+	externals: {
+		canvg: 'canvg',
+		html2canvas: 'html2canvas',
+		dompurify: 'dompurify',
+	},
+};
