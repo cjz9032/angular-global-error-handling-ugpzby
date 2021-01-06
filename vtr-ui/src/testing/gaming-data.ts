b19403317abbfa953ba657a8df877abd8f233af9
DESKTOP-602FJP4\henry <weizm1@lenovo.com>
2021-01-06 16:09:57 +0800

    fix(Overall): remove internal url from prod code

--- vtr-ui/src/testing/gaming-data.ts
+++ vtr-ui/src/testing/gaming-data.ts
@@ -19,8 +19,7 @@ export const GAMING_DATA = {
 				Title: 'Header Image DCC',
 				ShortTitle: '',
 				Description: '',
-				FeatureImage:
-					'https://qa.csw.lenovo.com/-/media/Lenovo/Vantage/Features/DCC_top_image.jpg?v=5cf8a0151ea84c4ca43e906339c3c3b2',
+				FeatureImage: '',
 				Action: '',
 				ActionType: null,
 				ActionLink: null,
@@ -44,8 +43,7 @@ export const GAMING_DATA = {
 				Title: 'Default Header Image',
 				ShortTitle: '',
 				Description: '',
-				FeatureImage:
-					'https://qa.csw.lenovo.com/-/media/Lenovo/Vantage/Features/Header-Image-Default.jpg?v=5d0bf7fd0065478c977ed284fecac45d',
+				FeatureImage: '',
 				Action: '',
 				ActionType: null,
 				ActionLink: null,
@@ -87,36 +85,36 @@ export const GAMING_DATA = {
 		MemoryBrightLevel: 4,
 		MemoryPanelType: [40961, 40962, 40963, 40964],
 		MemoryUnifySetList: [0],
-    },
-    singleColorResponse: {
-        LightPanelType: [1],
-        LedType_Complex: [0],
-        LedType_simple: [1, 2, 3, 4],
-        BrightAdjustLevel: 0,
-        RGBfeature: 1,
-    },
-    multipleColorResponse: {
-        LightPanelType: [32, 64],
-        LedType_Complex: [268435456, 1, 2, 4, 8, 32, 64, 128],
-        LedType_simple: [0],
-        BrightAdjustLevel: 4,
-        RGBfeature: 255,
-    },
-    lightEffectComplexTypeMock: {
-        Static: 1, // Same as On
-        Flicker: 2,
-        Breath: 4,
-        Wave: 8,
-        Music: 16,
-        Smooth: 32, ///change spectrum to smooth
-        CPU_thermal: 64,
-        CPU_frequency: 128,
-        Response: 256,
-        Ripple: 512,
-        Off: 268435456, ///same Off
-    },
-    panelImageData: [
-        { panelType: 1, colorRGB: 1, panelImage: 'C530@2x.png' },
-        { panelType: 4, colorRGB: 1, panelImage: 'T730Front@2x.png' },
-    ]
+	},
+	singleColorResponse: {
+		LightPanelType: [1],
+		LedType_Complex: [0],
+		LedType_simple: [1, 2, 3, 4],
+		BrightAdjustLevel: 0,
+		RGBfeature: 1,
+	},
+	multipleColorResponse: {
+		LightPanelType: [32, 64],
+		LedType_Complex: [268435456, 1, 2, 4, 8, 32, 64, 128],
+		LedType_simple: [0],
+		BrightAdjustLevel: 4,
+		RGBfeature: 255,
+	},
+	lightEffectComplexTypeMock: {
+		Static: 1, // Same as On
+		Flicker: 2,
+		Breath: 4,
+		Wave: 8,
+		Music: 16,
+		Smooth: 32, ///change spectrum to smooth
+		CPU_thermal: 64,
+		CPU_frequency: 128,
+		Response: 256,
+		Ripple: 512,
+		Off: 268435456, ///same Off
+	},
+	panelImageData: [
+		{ panelType: 1, colorRGB: 1, panelImage: 'C530@2x.png' },
+		{ panelType: 4, colorRGB: 1, panelImage: 'T730Front@2x.png' },
+	],
 };
