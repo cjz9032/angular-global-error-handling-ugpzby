3e21545f16cd46e43404ca6e309933b8ff368afe
Gemma Wu <wuxia8@lenovo.com>
2021-01-20 13:50:43 +0800

    fix(menu): update description

--- vtr-ui/src/app/material/menu/material-menu.component.ts
+++ vtr-ui/src/app/material/menu/material-menu.component.ts
@@ -143,11 +143,13 @@ export class MaterialMenuComponent implements OnInit, OnDestroy {
 		});
 	}
 
-	/* This is a temporary solution for an issue.
-	Issue is: open vanatge then hover on menu item, the dropdown menu will show.
-	Then click the tab key, the two sub menus will show at the same time.
-	The issue is caused by when first click the tab key,
-	focus will be lost from the current element, and appear on the body element*/
+	/**
+	 * This is a temporary solution for an issue.
+	 * Issue is: Open vanatge then hover on menu item, the dropdown menu will show.
+	 * Then press the tab key, the two sub menus will show at the same time.
+	 * The issue is caused by when first press the tab key,
+	 * focus will be lost from the current element, and appear on the body element
+	 */
 	@HostListener('document:keydown.tab')
 	onTabHandler() {
 		if (this.firstTab) {
