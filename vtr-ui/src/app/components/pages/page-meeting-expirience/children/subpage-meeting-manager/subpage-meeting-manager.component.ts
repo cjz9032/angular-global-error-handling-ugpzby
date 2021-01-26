ab5b2c62a96a2f83a04daa9698164b529cc9eb19
Jiaxi JX2 Xiao <xiaojx2@lenovo.com>
2021-01-26 10:47:13 +0800

    meeting page

--- vtr-ui/src/app/components/pages/page-meeting-expirience/children/subpage-meeting-manager/subpage-meeting-manager.component.ts
+++ vtr-ui/src/app/components/pages/page-meeting-expirience/children/subpage-meeting-manager/subpage-meeting-manager.component.ts
@@ -8,6 +8,26 @@ import { Component, OnInit } from '@angular/core';
 export class SubpageMeetingManagerComponent implements OnInit {
 
   constructor() { }
+  headerMenuItems = [
+    {
+				title: 'smb.meetingExperience.meetingManager.aiMeetingManager.title',
+				path: 'meetingManager',
+				metricsItem: 'PowerSmartSettings',
+				order: 1,
+			},
+    {
+				title: 'smb.meetingExperience.meetingManager.microPhone.title',
+				path: 'microPhone',
+				metricsItem: 'PowerSmartSettings',
+				order: 2,
+      },
+      {
+				title: 'smb.meetingExperience.meetingManager.smartAppearance.title',
+				path: 'smartAppearance',
+				metricsItem: 'PowerSmartSettings',
+				order: 3,
+			},
+  ];
 
   ngOnInit(): void {
   }
--- vtr-ui/src/app/components/pages/page-meeting-expirience/page-meeting-expirience.component.ts
+++ vtr-ui/src/app/components/pages/page-meeting-expirience/page-meeting-expirience.component.ts
@@ -1,5 +1,7 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { NavigationEnd, Router } from '@angular/router';
+import { TranslateService } from '@ngx-translate/core';
+import { Subscription } from 'rxjs';
 @Component({
   selector: 'vtr-page-meeting-expirience',
   templateUrl: './page-meeting-expirience.component.html',
@@ -7,9 +9,48 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PageMeetingExpirienceComponent implements OnInit {
 
-  constructor() { }
+  @ViewChild('hsRouterOutlet', { static: false }) hsRouterOutlet: ElementRef;
+
+  constructor(private translate: TranslateService
+    ) {this.menuItems.forEach((m) => {
+			m.label = this.translate.instant(m.label);
+		});
+  }
+
+  routerSubscription: Subscription;
+  activeElement: HTMLElement;
+  title = 'Meeting Expirience';
+  back = 'BACK';
+  backarrow = '< ';
+  parentPath = 'smb/meeting-experience';
+  private router: Router;
+  menuItems = [
+		{
+			id: 'meeting-expirience',
+			label: 'smb.meetingExperience.meetingManager.title',
+			path: 'meeting-manager',
+			subitems: [],
+			active: true,
+		},
+	];
 
   ngOnInit(): void {
+    this.routerSubscription = this.router.events.subscribe((evt) => {
+			if (!(evt instanceof NavigationEnd)) {
+				return;
+			}
+			// focus same active link element after route change , content loaded.
+			/* if ((evt instanceof NavigationEnd)) {
+				if (this.activeElement) {
+					this.activeElement.focus();
+				}
+			} */
+		});
   }
+
+  onRouteActivate($event, hsRouterOutlet: HTMLElement) {
+		// On route change , change foucs to immediate next below first tabindex on route change response
+		this.activeElement = document.activeElement as HTMLElement;
+	}
 
 }
