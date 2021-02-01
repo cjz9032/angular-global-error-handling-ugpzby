270a816ec8e390eedcf5346b49eef847b29c1aab
Jiaxi JX2 Xiao <xiaojx2@lenovo.com>
2021-02-01 13:22:31 +0800

    machine type identification for creator centre page

--- vtr-ui/src/app/components/pages/page-creator-centre/page-creator-centre.component.ts
+++ vtr-ui/src/app/components/pages/page-creator-centre/page-creator-centre.component.ts
@@ -4,6 +4,8 @@ import { TranslateService } from '@ngx-translate/core';
 import { Subscription } from 'rxjs';
 import { GuardService } from 'src/app/services/guard/guardService.service';
 import { NonArmGuard } from 'src/app/services/guard/non-arm-guard';
+import { CommonService } from 'src/app/services/common/common.service';
+import { DeviceService } from 'src/app/services/device/device.service';
 
 @Component({
   selector: 'vtr-page-creator-centre',
@@ -14,7 +16,10 @@ export class PageCreatorCentreComponent implements OnInit {
 
   @ViewChild('hsRouterOutlet', { static: false }) hsRouterOutlet: ElementRef;
 
-  constructor(private translate: TranslateService
+  constructor(
+	  private translate: TranslateService,
+	  private commonService: CommonService,
+	  private deviceService: DeviceService
   ) {this.menuItems.forEach((m) => {
 			m.label = this.translate.instant(m.label);
 		});
@@ -52,6 +57,7 @@ export class PageCreatorCentreComponent implements OnInit {
 	];
 
   ngOnInit(): void {
+	this.getMachineInfo();
     this.routerSubscription = this.router.events.subscribe((evt) => {
 			if (!(evt instanceof NavigationEnd)) {
 				return;
@@ -63,6 +69,23 @@ export class PageCreatorCentreComponent implements OnInit {
 				}
 			} */
 		});
+  }
+
+  async getMachineInfo(){
+	  await this.deviceService.getMachineInfo().then(()=>{
+		  	if(!this.deviceService.supportCreatorSettings){
+				this.menuItems = this.commonService.removeObjFrom(this.menuItems,'creator-settings');
+			}
+			if(!this.deviceService.supportEasyRendering){
+			  this.menuItems = this.commonService.removeObjFrom(this.menuItems,'easy-rendering');
+			}
+			if(!this.deviceService.supportColorCalibration){
+				this.menuItems = this.commonService.removeObjFrom(this.menuItems,'color-calibration');
+			}
+			if(this.menuItems){
+				this.menuItems[0].active=true;
+			}
+	  });
   }
 
   onRouteActivate($event, hsRouterOutlet: HTMLElement) {
