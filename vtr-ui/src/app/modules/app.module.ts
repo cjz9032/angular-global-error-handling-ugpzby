f083baf04bdb12214508a92adccb548ae4b7f115
HengNN <hengnn1@lenovo.com>
2021-03-10 13:40:10 +0800

    Revert "fix: add cdkTrapFocus and dialog restore focus config"
    This reverts commit b621e84284039728057a018629774fc530e07748.

--- vtr-ui/src/app/modules/app.module.ts
+++ vtr-ui/src/app/modules/app.module.ts
@@ -45,7 +45,6 @@ import { faTrashAlt } from '@fortawesome/free-regular-svg-icons';
 import { faTimesCircle } from '@fortawesome/free-solid-svg-icons/faTimesCircle';
 import { FontAwesomeModule, FaIconLibrary } from '@fortawesome/angular-fontawesome';
 
-import { A11yModule } from '@angular/cdk/a11y';
 import { MatButtonModule } from '@lenovo/material/button';
 import { MAT_RIPPLE_GLOBAL_OPTIONS } from '@lenovo/material/core';
 import { MatTooltipModule, MAT_TOOLTIP_DEFAULT_OPTIONS } from '@lenovo/material/tooltip';
@@ -151,7 +150,6 @@ const initializerFactory = (initializerService: InitializerService) => () =>
 		MatIconModule,
 		MatButtonModule,
 		MaterialModule,
-		A11yModule,
 	],
 	exports: [
 		NavbarModule,
@@ -187,7 +185,7 @@ const initializerFactory = (initializerService: InitializerService) => () =>
 		},
 		{
 			provide: MAT_DIALOG_DEFAULT_OPTIONS,
-			useValue: { autoFocus: true, restoreFocus: true },
+			useValue: { autoFocus: true },
 		},
 	],
 	entryComponents: [
