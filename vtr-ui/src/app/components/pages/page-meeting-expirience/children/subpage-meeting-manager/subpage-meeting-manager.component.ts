edcf40e4eddac4d748c28c28fae360c7c6e791ac
dongwq2 <dongwq2@Lenovo.com>
2021-03-17 16:28:54 +0800

    feat(VAN-27247) smb microphone automationid

--- vtr-ui/src/app/components/pages/page-meeting-expirience/children/subpage-meeting-manager/subpage-meeting-manager.component.ts
+++ vtr-ui/src/app/components/pages/page-meeting-expirience/children/subpage-meeting-manager/subpage-meeting-manager.component.ts
@@ -382,7 +382,7 @@ export class SubpageMeetingManagerComponent implements OnInit, OnDestroy {
 			this.microphoneModesUIModel = [];
 			response.modes.forEach((micMode) => {
 				this.microphoneModesUIModel.push({
-					componentId: `radioMicrophone${micMode}`.replace(/\s/g, ''),
+					componentId: `smb-radioMicrophone${micMode}`.replace(/\s/g, ''),
 					label: `device.deviceSettings.audio.microphone.optimize.options.${micMode}`,
 					value: micMode,
 					isChecked: micMode === response.current,
