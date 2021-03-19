4939f526633ff9df540f7401ca7f963bad087542
Lucas Vieira <Lucas.Vieira@fit-tecnologia.org.br>
2021-03-19 14:55:34 -0300

    VAN-24339: Added UT to ExportSnapshot service and adjust describe into UiQuestionMarkButtonComponent

--- vtr-ui/src/app/modules/snapshot/services/export-snapshot-results.service.spec.ts
+++ vtr-ui/src/app/modules/snapshot/services/export-snapshot-results.service.spec.ts
@@ -1,16 +1,62 @@
+import { HttpClient } from '@angular/common/http';
 import { TestBed } from '@angular/core/testing';
+import { ExportLogErrorStatus } from 'src/app/enums/export-log.enum';
+import { TranslateDefaultValueIfNotFoundPipe } from 'src/app/pipe/translate-default-value-if-not-found/translate-default-value-if-not-found.pipe';
+import { LoggerService } from 'src/app/services/logger/logger.service';
 
 import { ExportSnapshotResultsService } from './export-snapshot-results.service';
+import { SnapshotService } from './snapshot.service';
 
 describe('ExportSnapshotResultsService', () => {
-  let service: ExportSnapshotResultsService;
+	const loggerServiceSpy = jasmine.createSpyObj('LoggerService', ['error']);
+	const httpServiceSpy = jasmine.createSpyObj('HttpClient', ['get']);
+	const translateSpy = jasmine.createSpyObj('TranslateDefaultValueIfNotFoundPipe', ['transform']);
+	const snapshotServiceSpy = jasmine.createSpyObj('SnapshotService', [
+		'getSoftwareComponentsList',
+		'addinVersion',
+		'snapshotInfo',
+	]);
 
-  beforeEach(() => {
-    TestBed.configureTestingModule({});
-    service = TestBed.inject(ExportSnapshotResultsService);
-  });
+	let service: ExportSnapshotResultsService;
 
-  it('should be created', () => {
-    expect(service).toBeTruthy();
-  });
+	beforeEach(() => {
+		TestBed.configureTestingModule({
+			providers: [
+				ExportSnapshotResultsService,
+				{ provide: TranslateDefaultValueIfNotFoundPipe, useValue: translateSpy },
+				{ provide: LoggerService, useValue: loggerServiceSpy },
+				{ provide: HttpClient, useValue: httpServiceSpy },
+				{ provide: SnapshotService, useValue: snapshotServiceSpy },
+			],
+		});
+		service = TestBed.inject(ExportSnapshotResultsService);
+	});
+
+	it('should be created', () => {
+		expect(service).toBeTruthy();
+	});
+
+	it('should throw an exception when prepareDataFromScanLog return error', async () => {
+		const error = new Error('Generic Error');
+		spyOn<any>(service, 'prepareDataFromScanLog').and.throwError(error);
+
+		await expectAsync(service.exportSnapshotResults()).toBeRejectedWith(ExportLogErrorStatus.GenericError);
+		expect(loggerServiceSpy.error).toHaveBeenCalledWith('Could not get scan log', error);
+	});
+
+	it('should throw an exception when generateHtmlReport return error', async () => {
+		const error = new Error('Generic Error');
+		spyOn<any>(service, 'generateHtmlReport').and.throwError(error);
+
+		await expectAsync(service.exportSnapshotResults()).toBeRejectedWith(ExportLogErrorStatus.GenericError);
+		expect(loggerServiceSpy.error).toHaveBeenCalledWith('Could not get scan log', error);
+	});
+
+	it('should throw an exception when exportReportToFile return error', async () => {
+		const error = new Error('Generic Error');
+		spyOn<any>(service, 'exportReportToFile').and.throwError(error);
+
+		await expectAsync(service.exportSnapshotResults()).toBeRejectedWith(ExportLogErrorStatus.GenericError);
+		expect(loggerServiceSpy.error).toHaveBeenCalledWith('Could not get scan log', error);
+	});
 });
