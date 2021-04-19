20088a0f5b3dfe7654b2036adde9ee7366716a5b
HengNN <hengnn1@lenovo.com>
2021-04-19 10:50:04 +0800

    fix: use Render2 to add class for dialog container

--- vtr-ui/src/app/app.component.ts
+++ vtr-ui/src/app/app.component.ts
@@ -9,6 +9,7 @@ import {
 	AfterViewInit,
 	ElementRef,
 	ViewChild,
+	Renderer2,
 } from '@angular/core';
 import { Router, ActivatedRoute } from '@angular/router';
 import { Overlay } from '@angular/cdk/overlay';
@@ -105,7 +106,8 @@ export class AppComponent implements OnInit, OnDestroy, AfterViewInit {
 		private localCacheService: LocalCacheService,
 		private warrantyService: WarrantyService,
 		@Inject(DOCUMENT) public document: Document,
-		private snackBar: MatSnackBar
+		private snackBar: MatSnackBar,
+		private renderer: Renderer2
 	) {
 		this.ngbTooltipConfig.triggers = 'hover';
 		// to check web and js bridge version in browser console
@@ -244,7 +246,8 @@ export class AppComponent implements OnInit, OnDestroy, AfterViewInit {
 				}
 			}
 			setTimeout(() => {
-				document.querySelector('.mat-dialog-container').classList.add('mat-customize');
+				const container = document.querySelector('.mat-dialog-container');
+				self.renderer.addClass(container, 'mat-customize');
 			}, 0);
 
 			return original.call(this, template, config);
