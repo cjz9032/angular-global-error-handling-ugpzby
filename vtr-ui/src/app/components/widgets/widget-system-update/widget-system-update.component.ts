41b0e8726900900ce229999892e48b2d1c64d58f
Bob LB4 Liu <liulb4@lenovo.com>
2021-04-25 09:58:44 +0800

    fix(dashboard): VAN-28245 use pipe instead of instance in widget su ngOninit.

--- vtr-ui/src/app/components/widgets/widget-system-update/widget-system-update.component.ts
+++ vtr-ui/src/app/components/widgets/widget-system-update/widget-system-update.component.ts
@@ -1,22 +1,14 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { Router } from '@angular/router';
-import { TranslateService } from '@ngx-translate/core';
 
 @Component({
 	selector: 'vtr-widget-system-update',
 	templateUrl: './widget-system-update.component.html',
 	styleUrls: ['./widget-system-update.component.scss'],
 })
-export class WidgetSystemUpdateComponent implements OnInit {
-	public title: any;
-	public subtitle: any;
+export class WidgetSystemUpdateComponent {
 
-	constructor(private router: Router, private translate: TranslateService) {}
-
-	ngOnInit(): void {
-		this.title = this.translate.instant('systemUpdates.title');
-		this.subtitle = this.translate.instant('dashboard.systemUpdate.subtitle');
-	}
+	constructor(private router: Router) { }
 
 	gotoAndCheckSystemUpdate() {
 		this.router.navigate(['/device/system-updates'], { queryParams: { action: 'start' } });
