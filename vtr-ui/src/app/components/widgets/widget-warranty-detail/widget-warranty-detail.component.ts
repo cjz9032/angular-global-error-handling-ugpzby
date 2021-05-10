a817bffdaf6be7725b8cf3d21aa38f49b4dbe535
Bob LB4 Liu <liulb4@lenovo.com>
2021-05-10 14:06:19 +0800

    fix(dashboard): VAN-28840 open browser instead of goto support on arm machine

--- vtr-ui/src/app/components/widgets/widget-warranty-detail/widget-warranty-detail.component.ts
+++ vtr-ui/src/app/components/widgets/widget-warranty-detail/widget-warranty-detail.component.ts
@@ -1,46 +1,57 @@
 import { OnInit, Input, Component } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { WarrantyCodeEnum, WarrantyData, WarrantyStatusEnum } from 'src/app/data-models/warranty/warranty.model';
+import { DeviceService } from 'src/app/services/device/device.service';
 
 @Component({
-  selector: 'vtr-widget-warranty-detail',
-  templateUrl: './widget-warranty-detail.component.html',
-  styleUrls: ['./widget-warranty-detail.component.scss']
+	selector: 'vtr-widget-warranty-detail',
+	templateUrl: './widget-warranty-detail.component.html',
+	styleUrls: ['./widget-warranty-detail.component.scss']
 })
 export class WidgetWarrantyDetailComponent implements OnInit {
 
-  @Input() isOnline: boolean;
-  @Input() isLargeWidget = false;
-  @Input() warrantyUrl = '';
-  @Input() warrantyData: WarrantyData;
+	@Input() isOnline: boolean;
+	@Input() isLargeWidget = false;
+	@Input() warrantyUrl = '';
+	@Input() warrantyData: WarrantyData;
 
-  WarrantyStatusEnum = WarrantyStatusEnum;
-  WarrantyCodeEnum = WarrantyCodeEnum;
+	WarrantyStatusEnum = WarrantyStatusEnum;
+	WarrantyCodeEnum = WarrantyCodeEnum;
 
-  warrantyRounds = [];
+	warrantyRounds = [];
 
-  public pageParent: string;
-  status = {
-    title: [
-      'support.warranty.titleInWarranty',
-      'support.warranty.titleWarrantyExpired',
-      'support.warranty.titleWarrantyNotFound',
-    ],
-    detail: [
-      'support.warranty.statusInWarranty',
-      'support.warranty.statusWarrantyExpired',
-      'support.warranty.statusWarrantyNotFound',
-    ],
-  };
+	public pageParent: string;
+	status = {
+		title: [
+			'support.warranty.titleInWarranty',
+			'support.warranty.titleWarrantyExpired',
+			'support.warranty.titleWarrantyNotFound',
+		],
+		detail: [
+			'support.warranty.statusInWarranty',
+			'support.warranty.statusWarrantyExpired',
+			'support.warranty.statusWarrantyNotFound',
+		],
+	};
 
-  constructor(
-    private activatedRoute: ActivatedRoute
-  ) { }
+	constructor(
+		private activatedRoute: ActivatedRoute,
+		private deviceService: DeviceService,
+		private router: Router,
+	) { }
 
-  ngOnInit() {
-    try {
-      this.pageParent = this.activatedRoute.snapshot.data.pageName;
-    } catch (ex) { }
-  }
+	ngOnInit() {
+		try {
+			this.pageParent = this.activatedRoute.snapshot.data.pageName;
+		} catch (ex) { }
+	}
+
+	exploreOptionsClicked() {
+		if (this.deviceService.isArm) {
+			window.open(this.warrantyUrl, '_blank');
+		} else {
+			this.router.navigate(['/support']);
+		}
+	}
 
 }
